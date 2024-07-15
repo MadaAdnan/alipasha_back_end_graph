0d34e5e9220eaab641c72550f527d06253e43d2e
ADNAN_DEV <mh.shamey@gmail.com>
2024-07-15 11:58:17 +0300

    add color to categories table

--- database/migrations/2024_07_01_162548_create_categories_table.php
+++ database/migrations/2024_07_01_162548_create_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
+            $table->string('color')->nullable();
             //$table->foreignId('category_id')->nullable()->constrained()->cascadeOnDelete();
             $table->boolean('is_active')->nullable();
             $table->boolean('has_color')->nullable()->default(false);
