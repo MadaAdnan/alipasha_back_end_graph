6203d2763e1f6402fa4e25e25d1669e113ddcc16
ADNAN_DEV <mh.shamey@gmail.com>
2024-07-22 09:39:18 +0300

    add myProducts graph

--- app/GraphQL/Queries/MyProducts.php
+++ app/GraphQL/Queries/MyProducts.php
@@ -11,6 +11,6 @@ final class MyProducts
     public function __invoke($_, array $args)
     {
         return \App\Models\Product::where('user_id', auth()->id())
-            ->where(fn($query) => $query->where('name', 'LIKE', "%{$args['search']}%")->orWhere('expert', 'LIKE', "%{$args['search']}%"));
+           ->when(isset($args['search']) && !empty($args['search']),fn($query)=>$query ->where(fn($query) => $query->where('name', 'LIKE', "%{$args['search']}%")->orWhere('expert', 'LIKE', "%{$args['search']}%")));
     }
 }
