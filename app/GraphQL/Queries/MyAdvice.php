83d79a66ef97d08910d0269ff0822a38287c6f00
ADNAN_DEV <mh.shamey@gmail.com>
2024-07-22 13:13:00 +0300

    add views advice

--- app/GraphQL/Queries/MyAdvice.php
+++ app/GraphQL/Queries/MyAdvice.php
@@ -3,6 +3,7 @@
 namespace App\GraphQL\Queries;
 
 use App\Models\Advice;
+use App\Models\Slider;
 
 final class MyAdvice
 {
@@ -13,15 +14,16 @@ final class MyAdvice
     public function __invoke($_, array $args)
     {
         $advices = Advice::where('user_id', auth()->id())->get();
-
+        $sliderCount=Slider::where('user_id',auth()->id())->count();
+        $viewsCount=$advices->sum('views_count');
         return [
             'advices' => $advices,
             'advice_count' => $advices->count(),
-            'my_balance'=>190.5,
-            'my_point'=>100,
-            'my_wins'=>20.8,
-            'views'=>auth()->id(),
-            'slider_count'=>3,
+            'my_balance'=>45.5,
+            'my_point'=>20,
+            'my_wins'=>10.7,
+            'views'=>$viewsCount,
+            'slider_count'=>$sliderCount,
         ];
     }
 }
