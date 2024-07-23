f29ad4760c010600125588c9358718d6222b17ff
ADNAN_DEV <mh.shamey@gmail.com>
2024-07-23 12:13:14 +0300

    add shipping pricing

---
+++ database/migrations/2024_07_23_125959_create_shipping_prices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipping_prices', function (Blueprint $table) {
+            $table->id();
+            $table->double('weight')->nullable()->default(1);
+            $table->double('size')->nullable()->default(1);
+            $table->double('internal_price')->nullable()->default(1);
+            $table->double('external_price')->nullable()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipping_prices');
+    }
+};
