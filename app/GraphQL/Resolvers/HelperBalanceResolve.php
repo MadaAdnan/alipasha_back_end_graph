4ce5f8ee33a4cda57962959d0b28957e300048de
ADNAN_DEV <mh.shamey@gmail.com>
2024-07-23 16:22:03 +0300

    add pricing api

---
+++ app/GraphQL/Resolvers/HelperBalanceResolve.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\GraphQL\Resolvers;
+
+final class HelperBalanceResolve
+{
+
+    public function getTotalBalance($root)
+    {
+        return \DB::table('balances')->where('user_id', $root->id)->selectRaw('SUM(credit) - SUM(debit) as total')->first()?->total ?? 0;
+    }
+
+    public function getTotalPoint($root)
+    {
+        return \DB::table('balances')->where('user_id', $root->id)->selectRaw('SUM(credit) - SUM(debit) as total')->first()?->total ?? 0;
+    }
+
+}
