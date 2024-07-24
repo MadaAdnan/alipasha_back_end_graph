2d29bb78fee9ea33ac39383d6550f19de093442b
ADNAN_DEV <mh.shamey@gmail.com>
2024-07-24 11:19:05 +0300

    add createOrderShipping

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function from(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'from_id');
+    }
+
+    public function to(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'to_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
