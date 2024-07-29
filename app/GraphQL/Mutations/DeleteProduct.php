5b63df8ebbc1be202c1440af2478d9467fc76133
ADNAN_DEV <mh.shamey@gmail.com>
2024-07-29 15:32:56 +0300

    add isAvailable Product

---
+++ app/GraphQL/Mutations/DeleteProduct.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Product;
+
+final class DeleteProduct
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $id = $args['id'];
+        $product = Product::where(['id' => $id, 'user_id' => auth()->id()])->first();
+        if (!$product) {
+            throw new \Exception('لم يتم العثور على المنتج');
+        }
+        $product->delete();
+        return $product;
+    }
+}
