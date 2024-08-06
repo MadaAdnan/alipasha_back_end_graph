089e64b08fca5cd29eac6cb9b7738d932f3c4c8f
ADNAN_DEV <mh.shamey@gmail.com>
2024-08-06 09:51:26 +0300

    add turkeyPrice Resolve

--- app/GraphQL/Queries/Product.php
+++ app/GraphQL/Queries/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Enums\ProductActiveEnum;
 use App\Models\ProductView;
 use Carbon\Carbon;
 
@@ -16,6 +17,48 @@ final class Product
         $proudct = \App\Models\Product::findOrFail($args['id']);
         ProductView::updateOrCreate(['view_at' => Carbon::today(), 'product_id' => $proudct->id], // الشروط التي يجب أن تتطابق
             ['count' => \DB::raw('count + 1')]);
-        return $proudct;
+
+        $products= \App\Models\Product::query()->where('active', ProductActiveEnum::ACTIVE->value)
+            ->where('category_id',$proudct->category_id)
+            ->where('sub1_id',$proudct->sub1_id)->inRandomOrder()->latest()->limit(6)->get();
+        $ids = $products->pluck('id')->toArray();
+        $today = today();
+        \DB::transaction(function () use ($ids, $today) {
+
+            // تحديث السجلات الموجودة
+            \DB::table('product_views')
+                ->whereIn('product_id', $ids)
+                ->whereDate('view_at', $today)
+                ->update(['count' => \DB::raw('count + 1')]);
+
+            // إدخال السجلات الجديدة
+            $existingIds = \DB::table('product_views')
+                ->whereIn('product_id', $ids)
+                ->whereDate('view_at', $today)
+                ->pluck('product_id')
+                ->toArray();
+
+            $newIds = array_diff($ids, $existingIds);
+
+            if (!empty($newIds)) {
+                $inserts = array_map(function ($id) use ($today) {
+                    return [
+                        'product_id' => $id,
+                        'view_at' => $today,
+                        'count' => 1,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ];
+                }, $newIds);
+
+                \DB::table('product_views')->insert($inserts);
+            }
+        });
+
+
+        return [
+            "product"=>$proudct,
+            "products"=>$products
+        ];
     }
 }
