5263884e624b9abdf4c110e2a93882e5ac096062
ADNAN_DEV <mh.shamey@gmail.com>
2024-09-03 11:47:21 +0300

    fix create product

--- app/GraphQL/Mutations/CreateProduct.php
+++ app/GraphQL/Mutations/CreateProduct.php
@@ -14,40 +14,41 @@ final class CreateProduct
     public function __invoke($_, array $args)
     {
         $data = $args['input'];
-
         $user = auth()->user();
         try {
             $product = \App\Models\Product::create([
+                'city_id' => $user->city_id,
                 'name' => $data['name'] ?? '',
+                'video' => $data['video'] ?? '',
                 'info' => $data['info'] ?? '',
                 'tags' => $data['tags'] ?? null,
+                'is_available' => $data['is_available'] ?? false,
+                'price' => $data['price'] ?? 0,
+                'discount' => $data['discount'] ?? null,
+                'is_discount' => (double)$data['discount'] > 0,
                 'category_id' => $data['category_id'] ?? null,
                 'sub1_id' => $data['sub1_id'] ?? null,
                 'sub2_id' => $data['sub2_id'] ?? null,
                 'sub3_id' => $data['sub3_id'] ?? null,
                 'sub4_id' => $data['sub4_id'] ?? null,
-                'is_discount' => $data['is_discount'] ?? false,
-                'discount' => $data['discount'] ?? null,
-                'is_available' => $data['is_available'] ?? false,
-                'price' => $data['price'] ?? 0,
+
                 'active' => ProductActiveEnum::PENDING->value,
                 'user_id' => $user->id,
-                'city_id' => $data['city_id'] ?? null,
-                'video' => $data['video'] ?? '',
+
                 'type' => 'product',
                 'expert' => \Str::words($data['info'], 10),
-                'end_date' => $data['end_date'] ?? null,
+                'end_date' => $data['period'] != null ? now()->addDays($data['period']) : null,
                 'is_delivery' => $data['is_delivery'] ?? false,
                 'latitude' => $data['latitude'] ?? null,
                 'longitude' => $data['longitude'] ?? null,
 
             ]);
 
-            if(isset($data['image']) && $data['image']!==null){
+            if (isset($data['image']) && $data['image'] !== null) {
                 $product->addMedia($data['image'])->toMediaCollection('image');
             }
 
-            if(isset($data['images']) && $data['images'] !==null){
+            if (isset($data['images']) && $data['images'] !== null) {
                 foreach ($data['images'] as $image) {
                     $product->addMedia($image)->toMediaCollection('images');
                 }
