0b07c280dd73ab59e66c15d841177798086c70ca
ADNAN_DEV <mh.shamey@gmail.com>
2024-09-10 14:06:02 +0300

    fix update tender

--- app/GraphQL/Mutations/UpdateProduct.php
+++ app/GraphQL/Mutations/UpdateProduct.php
@@ -21,15 +21,15 @@ final class UpdateProduct
                 throw new \Exception('المنتج رقم ' . $productId . ' غير موجود');
             }
             $product->update([
-                'name' => $data["name"] ?? null,
-                'info' => array_key_exists('info', $data) ? $data['info'] : null,
+                'name' =>\Str::words($data['info'], 10),
+                'info' =>$data['info'] ??null,
                 'tags' => $data['tags'] ?? null,
                 'category_id' => $data['category_id'] ?? null,
                 'sub1_id' => $data['sub1_id'] ?? null,
                 'sub2_id' => $data['sub2_id'] ?? null,
                 'sub3_id' => $data['sub3_id'] ?? null,
                 'sub4_id' => $data['sub4_id'] ?? null,
-                'is_discount' => $data['is_discount'] ?? false,
+                'is_discount' =>isset($data['discount']) &&   $data['discount']>0,
                 'discount' => $data['discount'] ?? null,
                 'is_available' => $data['is_available'] ?? false,
                 'price' => $data['price'] ?? 0,
@@ -38,8 +38,8 @@ final class UpdateProduct
                 'expert' => \Str::words($data['info'], 10),
                 'end_date' => $data['end_date'] ?? null,
                 'is_delivery' => $data['is_delivery'] ?? false,
-                'latitude' => $data['latitude'] ?? null,
-                'longitude' => $data['longitude'] ?? null,
+               // 'latitude' => $data['latitude'] ?? null,
+               // 'longitude' => $data['longitude'] ?? null,
 
             ]);
 
