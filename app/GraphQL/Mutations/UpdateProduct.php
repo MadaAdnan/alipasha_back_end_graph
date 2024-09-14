b0099cd918e4a0c6436ca2aed64f5ba4a295ee62
ADNAN_DEV <mh.shamey@gmail.com>
2024-09-14 11:54:07 +0300

    fix update tender

--- app/GraphQL/Mutations/UpdateProduct.php
+++ app/GraphQL/Mutations/UpdateProduct.php
@@ -21,31 +21,31 @@ final class UpdateProduct
                 throw new \Exception('المنتج رقم ' . $productId . ' غير موجود');
             }
             $product->update([
-                'name' =>\Str::words($data['info'], 10),
-                'info' =>$data['info'] ??null,
+                'name' => $data['name'] ?? \Str::words($data['info'], 10),
+                'info' => $data['info'] ?? null,
                 'tags' => $data['tags'] ?? null,
                 'category_id' => $data['category_id'] ?? null,
                 'sub1_id' => $data['sub1_id'] ?? null,
                 'sub2_id' => $data['sub2_id'] ?? null,
                 'sub3_id' => $data['sub3_id'] ?? null,
                 'sub4_id' => $data['sub4_id'] ?? null,
-                'is_discount' =>isset($data['discount']) &&   $data['discount']>0,
+                'is_discount' => isset($data['discount']) && $data['discount'] > 0,
                 'discount' => $data['discount'] ?? null,
                 'is_available' => $data['is_available'] ?? false,
                 'price' => $data['price'] ?? 0,
-                'city_id' => $data['city_id']  ??$product->city_id,
+                'city_id' => $data['city_id'] ?? $product->city_id,
                 'video' => $data['video'] ?? '',
                 'expert' => \Str::words($data['info'], 10),
                 'end_date' => $data['end_date'] ?? null,
                 'is_delivery' => $data['is_delivery'] ?? false,
-               // 'latitude' => $data['latitude'] ?? null,
-               // 'longitude' => $data['longitude'] ?? null,
+                // 'latitude' => $data['latitude'] ?? null,
+                // 'longitude' => $data['longitude'] ?? null,
 
             ]);
-            $product->colors()->sync($data['colors']??[]);
+            $product->colors()->sync($data['colors'] ?? []);
             if (isset($data['images']) && $data['images'] !== null) {
-                foreach ($data['images'] as $key=>$image) {
-                        $product->addMedia($image)->toMediaCollection('images');
+                foreach ($data['images'] as $key => $image) {
+                    $product->addMedia($image)->toMediaCollection('images');
                 }
             }
         } catch (\Exception | \Error $e) {
