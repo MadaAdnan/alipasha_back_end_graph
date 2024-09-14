ae59347e76ed48753f55caa4d14d246c351412dd
ADNAN_DEV <mh.shamey@gmail.com>
2024-09-14 15:49:40 +0300

    recommended product

--- app/GraphQL/Queries/RecomandedProduct.php
+++ app/GraphQL/Queries/RecomandedProduct.php
@@ -11,8 +11,9 @@ final class RecomandedProduct
      * @param  null  $_
      * @param  array{}  $args
      */
-    public function __invoke($_, array $args)
+    public function __invoke($root, array $args, $context, $info)
     {
+        // التحقق من وجود المستخدم
         if (auth()->user()) {
             $userId = auth()->id();
             $mostVisitedCategoryId = Interaction::where('user_id', $userId)
@@ -25,22 +26,30 @@ final class RecomandedProduct
             $followedSellerProductsQuery = Product::whereIn('user_id', $followedSellers);
         } else {
             // في حالة عدم وجود المستخدم، يتم جلب المنتجات المميزة فقط
-            $mostVisitedProductsQuery =Product::whereRaw('1=0'); // يجب أن يكون الاستعلام فارغًا
+            $mostVisitedProductsQuery = Product::whereRaw('1=0'); // يجب أن يكون الاستعلام فارغًا
             $followedSellerProductsQuery = Product::whereRaw('1=0'); // يجب أن يكون الاستعلام فارغًا
         }
 
-// جلب المنتجات المميزة
+        // جلب المنتجات المميزة
         $featuredProductsQuery = Product::where('level', LevelProductEnum::SPECIAL->value)->take(10);
 
-// دمج النتائج وترتيبها وتقسيمها إلى صفحات
-        $products = Product::fromSub(function ($query) use ($featuredProductsQuery, $mostVisitedProductsQuery, $followedSellerProductsQuery) {
+        // دمج النتائج وترتيبها وتقسيمها إلى صفحات
+        $productsQuery = Product::fromSub(function ($query) use ($featuredProductsQuery, $mostVisitedProductsQuery, $followedSellerProductsQuery) {
             $query->select('*')
                 ->fromSub($featuredProductsQuery, 'featured')
                 ->union($mostVisitedProductsQuery)
                 ->union($followedSellerProductsQuery);
         }, 'combined')
-            ->orderBy('level', 'desc')
-            ->orderBy('sub1_id', 'desc')
-            ->orderBy('user_id', 'desc');
+            ->orderByRaw("
+                CASE
+                    WHEN level = '".LevelProductEnum::SPECIAL->value."' THEN 0
+                    WHEN sub1_id = $mostVisitedCategoryId THEN 1
+                    WHEN user_id IN (".implode(',', $followedSellers).") THEN 2
+                    ELSE 3
+                END
+            ")
+            ->orderBy('created_at', 'desc');
+
+        return $productsQuery;
     }
 }
