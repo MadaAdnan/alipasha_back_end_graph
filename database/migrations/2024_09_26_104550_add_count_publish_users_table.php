e33aa689e4c43e10dfbbae995ce7c205f0ee9768
ADNAN_DEV <mh.shamey@gmail.com>
2024-09-26 10:52:34 +0300

    add is_verified for seller

---
+++ database/migrations/2024_09_26_104550_add_count_publish_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('product_count')->nullable()->default(0);// عدد المنتجات في الخطة الحالية
+            $table->boolean('is_verified')->nullable()->default(false);// توثيق الحساب
+            $table->string('id_color')->nullable()->default('#FF0000');//  لون الهوية
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['product_count','is_verified','id_color']);
+        });
+    }
+};
