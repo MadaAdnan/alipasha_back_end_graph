d3f86248b28ee45f26a8a62b5b654ac99ab30ea0
ADNAN_DEV <mh.shamey@gmail.com>
2024-09-28 16:07:21 +0300

    fix Update User

--- app/Models/Community.php
+++ app/Models/Community.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use AnourValar\EloquentSerialize\Tests\Models\User;
+
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
