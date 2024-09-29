23c5e913745c9b9f0353ab1b8abe368d951c9a3c
ADNAN_DEV <mh.shamey@gmail.com>
2024-09-29 14:44:20 +0300

    fix Update User

--- app/GraphQL/Queries/GetMyCommunity.php
+++ app/GraphQL/Queries/GetMyCommunity.php
@@ -14,19 +14,6 @@ final class GetMyCommunity
     public function __invoke($_, array $args)
     {
         $search = $args['search'] ?? '';
-      /*  $communities = Community::whereHas('users', function ($query) {
-            $query->where('users.id', Auth::id());
-        })
-            ->with(['users' => function ($query) {
-                $query->select('users.*')
-                    ->join('community_user as t', 'users.id', '=', 't.user_id')
-                    ->where('users.id', '!=', Auth::id())
-                    ->take(3);
-            }])
-            ->when(!empty($search), function ($query) use ($search) {
-                $query->where('name', 'like', "%$search%");
-            })
-            ->latest('last_update');*/
         $communities = Community::whereHas('allUsers', function ($query) {
             $query->where('users.id', Auth::id());  // جلب المجتمعات التي يشارك فيها المستخدم الحالي
         })
