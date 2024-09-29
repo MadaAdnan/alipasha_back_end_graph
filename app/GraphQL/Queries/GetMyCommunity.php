ed754755f5a540a9c0c9b3aa39bd31b6a6c37aba
ADNAN_DEV <mh.shamey@gmail.com>
2024-09-29 14:31:27 +0300

    fix Update User

--- app/GraphQL/Queries/GetMyCommunity.php
+++ app/GraphQL/Queries/GetMyCommunity.php
@@ -14,7 +14,7 @@ final class GetMyCommunity
     public function __invoke($_, array $args)
     {
         $search = $args['search'] ?? '';
-        $communities = auth()->user()?->communities()->latest('last_update');
+        $communities = auth()->user()?->communities()->latest('last_update')->get();
 
 // اجلب 3 مستخدمين فقط لكل مجتمع
         $communities->each(function($community) {
