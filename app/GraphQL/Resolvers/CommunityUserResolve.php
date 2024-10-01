c14e817c5d34d61640afca974d30c691162610ee
ADNAN_DEV <mh.shamey@gmail.com>
2024-10-01 14:42:17 +0300

    fix create message

--- app/GraphQL/Resolvers/CommunityUserResolve.php
+++ app/GraphQL/Resolvers/CommunityUserResolve.php
@@ -5,14 +5,15 @@ namespace App\GraphQL\Resolvers;
 use App\Models\Community;
 use App\Models\Product;
 use App\Models\Rate;
+use Illuminate\Database\Eloquent\Collection;
 
 class CommunityUserResolve
 {
     /**
      * @param $root Community
-     * @return float
+     * @return Collection
      */
-    public static function users($root): float
+    public static function users($root):Collection
     {
         return $root->users->take(3);
 
