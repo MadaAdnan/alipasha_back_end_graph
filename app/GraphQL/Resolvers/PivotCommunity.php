369755163d6716eac2b175990d332beb358ba72e
ADNAN_DEV <mh.shamey@gmail.com>
2024-10-16 15:31:09 +0300

    fix create community

---
+++ app/GraphQL/Resolvers/PivotCommunity.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\GraphQL\Resolvers;
+
+use App\Models\Product;
+use App\Models\Rate;
+
+class PivotCommunity
+{
+    /**
+     * @param $root Product
+     * @return float
+     */
+    public static function getPivot($root): null | array
+    {
+        $community= auth()->user()->communities()->find($root->id);
+            if($community){
+                return [
+                    'is_manager'=>$community->pivot->is_manager,
+                    'notify'=>$community->pivot->notify,
+                ];
+            }
+            return null;
+
+    }
+
+
+
+}
