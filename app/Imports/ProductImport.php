f95915a448bfe3dffa0035380bc57dc1c1e92e5c
ADNAN_DEV <mh.shamey@gmail.com>
2024-10-19 16:34:16 +0300

    add myOrderShipping Query

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 
 use App\Enums\LevelProductEnum;
 use App\Enums\ProductActiveEnum;
+use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use Rap2hpoutre\FastExcel\FastExcel;
 
@@ -25,7 +26,33 @@ class ProductImport
 
             foreach ($chunk as $key => $row) {
 
-                $data[] = [
+                Product::create([
+                    'id' => $row['Column1'],
+                    'name' => $row['Column2'],
+                    'info' => $row['Column3'],
+                    'expert' => $row['Column4'],
+                    'user_id' => $row['Column5'],
+                    'phone' => $row['Column6'],
+                    'email' => $row['Column7'],
+                    'type' => $row['Column8'],
+                    'price' => !empty($row['Column9']) ? $row['Column9'] : 0,
+                    'discount' => !empty($row['Column10']) ? $row['Column10'] : 0,
+                    'active' => ($row['Column11'] === 'active') ? ProductActiveEnum::ACTIVE->value : (($row['Column11'] === 'pending') ? ProductActiveEnum::PENDING->value : ProductActiveEnum::BLOCK->value),
+                    'is_available' => $row['Column12'] ?? false,
+                    'is_discount' => $row['Column13'] ?? false,
+                    'level' => $row['Column14'] === 'special' ? LevelProductEnum::SPECIAL->value : LevelProductEnum::NORMAL->value,
+                    'url' => $row['Column15'],
+                    'start_date' => !empty($row['Column16']) ? $row['Column16'] : null,
+                    'end_date' => !empty($row['Column17']) ? $row['Column17'] : null,
+                    // 'video' => $row['Column18'],
+                    'code' => !empty($row['Column21']) ? $row['Column21'] : null,
+                    'created_at' => $row['Column22'],
+                    'deleted_at' => !empty($row['Column23']) ? $row['Column23'] : null,
+                    'city_id' => !empty($row['Column24']) ? $row['Column24'] : null,
+                    'tags'=>!empty($row['Column27']) ? $row['Column27'] : null,
+                    // أضف المزيد من الأعمدة كما هو مطلوب
+                ]);
+               /* $data[] = [
                     'id' => $row['Column1'],
                     'name' => $row['Column2'],
                     'info' => $row['Column3'],
@@ -50,12 +77,12 @@ class ProductImport
                     'city_id' => !empty($row['Column24']) ? $row['Column24'] : null,
                     'tags'=>!empty($row['Column27']) ? $row['Column27'] : null,
                     // أضف المزيد من الأعمدة كما هو مطلوب
-                ];
+                ];*/
                 // افترض أن لديك جدول يسمى `your_table` وأن العمود الأول هو `column1` والعمود الثاني هو `column2`
 
             }
 
-            DB::table('products')->insert($data);
+            //DB::table('products')->insert($data);
         }
 
     }
