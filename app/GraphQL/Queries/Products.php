28590b4c92b2b3ca7d6567d4cb054010fb5c4d12
ADNAN_DEV <mh.shamey@gmail.com>
2024-10-22 10:11:33 +0300

    fix products

--- app/GraphQL/Queries/Products.php
+++ app/GraphQL/Queries/Products.php
@@ -201,7 +201,8 @@ final class Products
      (CASE WHEN products.user_id IN (?) THEN 1 ELSE 0 END) * 0.2 AS score',
                 [LevelProductEnum::SPECIAL->value, $popularCategory, implode(',', $followedStores)]
             )
-            ->orderBy('score', 'desc');
+            ->orderBy('score', 'desc')
+        ->orderBy('created_at','desc')->inRandomOrder();
            // ->orderByRaw('RAND()') // ترتيب عشوائي للمنتجات مع نفس مستوى التفاعل
 //            ->orderBy('level')
            /* ->when(isset($args['orderBy']),fn($query)=>$query->orderBy($args['orderBy']),fn($query)=>$query->orderBy('created_at','desc') ->orderBy('level'));*/
