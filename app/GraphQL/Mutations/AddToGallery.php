30ddb7a7b8691ad44298041df3cbf5a5a9ba77b8
ADNAN_DEV <mh.shamey@gmail.com>
2024-10-22 12:13:00 +0300

    add create gallery

---
+++ app/GraphQL/Mutations/AddToGallery.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Exceptions\GraphQLExceptionHandler;
+use App\Models\User;
+
+final class AddToGallery
+{
+    /**
+     * @param null $_
+     * @param array{} $args
+     */
+    public function __invoke($_, array $args)
+    {
+        /**
+         * @var $user User
+         */
+        $user = auth()->user();
+        $image = $args['image'];
+        if (!isset($data['image']) || $data['image'] === null) {
+            throw new GraphQLExceptionHandler('يرجى تحديد صورة صحيحة');
+        }
+        $type = $image->getClientOriginalExtension();
+        if(!in_array($type,['png','webp','jpeg','jpg'])){
+            throw new GraphQLExceptionHandler('صيغة الملف غير صالحة');
+        }
+        try {
+            $media = $user->addMedia($image)->toMediaCollection('gallery');
+            return [
+                'id' => $media->id,
+                'url' => $media->getUrl('webp'),
+            ];
+        } catch (\Exception | \Error $e) {
+            throw new GraphQLExceptionHandler($e->getMessage());
+        }
+
+    }
+}
