371ea7a811171833d2bab2227f3314c18ea6bec9
ADNAN_DEV <mh.shamey@gmail.com>
2024-10-24 16:27:31 +0300

    add create gallery

--- app/GraphQL/Queries/Products.php
+++ app/GraphQL/Queries/Products.php
@@ -172,6 +172,10 @@ final class Products
             ->when(isset($args['sub1_id']), fn($query) => $query->where('sub1_id', $args['sub1_id']))
             ->when(isset($args['city_id']), fn($query) => $query->where('city_id', $args['city_id']))
             ->when(isset($args['user_id']), fn($query) => $query->where('products.user_id', $args['user_id']))
+            ->search($args['search'])
+
+            ->groupBy('products.id') // نضمن أن المنتجات يتم جمعها وتجنب التكرار
+
             ->selectRaw(
                 'products.*,
      (CASE WHEN products.level = ? THEN 1 ELSE 0 END) * 0.3 +
@@ -179,12 +183,7 @@ final class Products
      (CASE WHEN products.user_id IN (?) THEN 1 ELSE 0 END) * 0.2 AS score',
                 [LevelProductEnum::SPECIAL->value, $popularCategory, implode(',', $followedStores)]
             )->when(empty($args['search']),fn($query)=>$query->orderBy('score', 'desc')
-                ->orderBy('created_at','desc')->inRandomOrder())
-            ->when(isset($args['search']) && !empty($args['search']) && $type !== 'seller', fn($query) => Product::search($args['search']))
-
-
-
-          ;
+                ->orderBy('created_at','desc')->inRandomOrder());
 
         return $products;
     }
