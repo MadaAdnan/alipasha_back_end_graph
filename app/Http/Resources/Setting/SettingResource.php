eaa5c1a5772fa93ca1b469fe63a145b10310071b
ADNAN_DEV <mh.shamey@gmail.com>
2024-10-30 13:10:03 +0300

    fix some error

--- app/Http/Resources/Setting/SettingResource.php
+++ app/Http/Resources/Setting/SettingResource.php
@@ -16,8 +16,8 @@ class SettingResource extends JsonResource
     {
         return [
             'address' => $this->address,
-            'longitude' => $this->longitude,
-            'latitude' => $this->latitude,
+           // 'longitude' => $this->longitude,
+           // 'latitude' => $this->latitude,
             'weather_api' => $this->weather_api,
             'current_version' => $this->current_version,
             'force_upgrade' =>(bool) $this->force_upgrade,
@@ -25,11 +25,11 @@ class SettingResource extends JsonResource
             'active_advice' => (bool)$this->active_advice,
             'delivery_service' => $this->delivery_service,
             'auto_update_exchange' =>(bool) $this->auto_update_exchange,
-            'about' => $this->about,
-            'privacy' => $this->privacy,
+           // 'about' => $this->about,
+           // 'privacy' => $this->privacy,
             'active_live' => (bool)$this->active_live,
             'live_id' => $this->live_id,
-            'social' => $this->social,
+           // 'social' => $this->social,
             'less_amount_point_pull' => $this->less_amount_point_pull,
         ];
     }
