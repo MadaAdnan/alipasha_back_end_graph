5ad53735793bee5227bd1833a29aa680dca22d31
adnan <mh.shamey@gmail.com>
2024-11-04 09:59:02 +0300

    test

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -194,7 +194,7 @@ class ProductResource extends Resource
             ->columns([
 Tables\Columns\SpatieMediaLibraryImageColumn::make('image')->collection('image')->conversion('webp')->label('الصورة')->size(50),
                 Tables\Columns\TextColumn::make('id')->label('رقم المنتج')->searchable(),
-                Tables\Columns\TextColumn::make('category.name')->label('اسم القسم'),
+                Tables\Columns\TextColumn::make('category.name')->label('اسم القسم')->description(fn($record)=>$record->sub1?->name),
                 Tables\Columns\TextColumn::make('name')->label('اسم المنتج')->description(fn($record) => $record->expert)->searchable(),
                 Tables\Columns\TextColumn::make('city.name')->label('المدينة'),
                 Tables\Columns\TextColumn::make('user.name')->label('المتجر')->url(fn($record) => UserResource::getUrl('edit', ['record' => $record->user_id]))->searchable(),
