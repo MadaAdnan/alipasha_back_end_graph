ccc82b5f32b5c962931690712ad3231e6eaf319f
adnan <mh.shamey@gmail.com>
2024-11-11 13:40:11 +0300

    fix user model

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -56,7 +56,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => false,
-                'host' => env('PUSHER_HOST','pazarpasha.com'),
+                'host' => env('PUSHER_HOST','127.0.0.1'),
                 'port' => env('PUSHER_PORT',8080),
                 'scheme' => 'http',
             ],
