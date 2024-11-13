c28ed5ece411208d65b1234a366084da186b57e9
adnan <mh.shamey@gmail.com>
2024-11-13 11:16:34 +0300

    fix user model

--- app/GraphQL/Queries/UserByCategory.php
+++ app/GraphQL/Queries/UserByCategory.php
@@ -9,7 +9,7 @@ final  class UserByCategory
     /** @param  array{}  $args */
     public function __invoke( $_, array $args)
     {
-       $users=User::where('category_id',$args['category_id'])->inRandomOrder();
+       $users=User::whereHas('products',fn($query)=>$query->where('category_id',$args['category_id']))->inRandomOrder();
        return $users;
     }
 }
