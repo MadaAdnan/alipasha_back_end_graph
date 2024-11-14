456f88a0c6b3268416bcb301270e67074c0dded8
adnan <mh.shamey@gmail.com>
2024-11-14 14:50:43 +0300

    add replay

--- app/GraphQL/Mutations/CreateChat.php
+++ app/GraphQL/Mutations/CreateChat.php
@@ -3,6 +3,7 @@
 namespace App\GraphQL\Mutations;
 
 use App\Enums\CommunityTypeEnum;
+use App\Exceptions\GraphQLExceptionHandler;
 use App\Models\Community;
 use App\Models\User;
 
@@ -16,29 +17,35 @@ final class CreateChat
     {
         $userId=auth()->id();
         $memberId=$args['memberId'];
-        $member=User::find($memberId);
-        $community = Community::whereHas('users', function($query) use ($userId) {
-            $query->where('users.id', $userId);
-        })->whereHas('users', function($query) use ($memberId) {
-            $query->where('users.id', $memberId);
-        })->where('type', CommunityTypeEnum::CHAT->value)
-            ->withCount('users')
-            ->having('users_count', '=', 2)
-            ->first();
+       try{
+           $member=User::find($memberId);
 
-        if($community===null){
-           $community=Community::create([
-               'last_update'=>now(),
-               'manager_id'=>$userId,
-               'name'=>auth()->user()->name.' - '. $member?->seller_name,
-               'type'=>CommunityTypeEnum::CHAT->value,
-           ]);
+           $community = Community::whereHas('users', function($query) use ($userId) {
+               $query->where('users.id', $userId);
+           })->whereHas('users', function($query) use ($memberId) {
+               $query->where('users.id', $memberId);
+           })->where('type', CommunityTypeEnum::CHAT->value)
+               ->withCount('users')
+               ->having('users_count', '=', 2)
+               ->first();
 
-           $community->users()->sync([$userId,$memberId]);
+           if($community===null){
+               $community=Community::create([
+                   'last_update'=>now(),
+                   'manager_id'=>$userId,
+                   'name'=>auth()->user()->name.' - '. $member?->seller_name,
+                   'type'=>CommunityTypeEnum::CHAT->value,
+               ]);
+
+               $community->users()->sync([$userId,$memberId]);
+           }
+           return [
+               'community'=>$community,
+               'user'=>auth()->user()
+           ];
+       }catch (\Exception |\Error $e){
+           throw new GraphQLExceptionHandler($e->getMessage());
        }
-       return [
-           'community'=>$community,
-           'user'=>auth()->user()
-       ];
+
     }
 }
