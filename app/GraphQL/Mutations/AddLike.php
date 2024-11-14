2d8a7b2d3f361d93bffbf80ceaaae3b098bee6f7
adnan <mh.shamey@gmail.com>
2024-11-14 14:09:27 +0300

    add replay

--- app/GraphQL/Mutations/AddLike.php
+++ app/GraphQL/Mutations/AddLike.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Mutations;
 
+use App\Models\Interaction;
 use App\Models\Like;
 use App\Models\Product;
 
@@ -11,16 +12,27 @@ final  class AddLike
     public function __invoke( $_, array $args)
     {
        $productId=$args['product_id'];
+       $product=Product::find($productId);
        if(Like::where(['product_id'=>$productId,'user_id' => auth()->id()])->exists()){
            Like::where(['product_id'=>$productId,'user_id' => auth()->id()])->delete();
-
        }else{
            Like::create([
                'user_id'=>auth()->id(),
                'product_id'=>$productId,
            ]);
+           try{
+               Interaction::updateOrCreate([
+                   'user_id'=>auth()->id(),
+                   'category_id'=>$product->category_id,
+
+               ],[
+                   'visited'=> \DB::raw('visited + 1'),
+               ]);
+           }catch(\Exception | \Error $e){
+               info('ERROR:'.$e->getMessage());
+           }
        }
 
-        return Product::find($productId);
+        return $product;
     }
 }
