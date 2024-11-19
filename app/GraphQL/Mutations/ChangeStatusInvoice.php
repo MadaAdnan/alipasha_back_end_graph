c5c9b48308f02aef0676de5ea6326e7ee0069f94
adnan <mh.shamey@gmail.com>
2024-11-19 15:35:06 +0300

    add shipping invoice

---
+++ app/GraphQL/Mutations/ChangeStatusInvoice.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Enums\OrderStatusEnum;
+use App\Exceptions\GraphQLExceptionHandler;
+use App\Models\Invoice;
+
+final  class ChangeStatusInvoice
+{
+    /** @param  array{}  $args */
+    public function __invoke( $_, array $args)
+    {
+        $invoice=Invoice::find($args['invoiceId']);
+        if(!$invoice){
+            throw new GraphQLExceptionHandler('الطلب غير موجود');
+        }
+        if($invoice->seller_id !=auth()->id()){
+            throw new GraphQLExceptionHandler('ليس لديك الإذن بالعملية');
+        }
+        $status_arry=[
+            OrderStatusEnum::AGREE->value,
+            OrderStatusEnum::CANCELED->value,
+
+        ];
+        if(!in_array($args['status'],$status_arry)){
+            throw new GraphQLExceptionHandler('الحالة غير صحيحة');
+        }
+        $invoice->update([
+            'status'=>$args['status'],
+            'seller_note'=>$args['msg'],
+        ]);
+        return $invoice->refresh();
+    }
+}
