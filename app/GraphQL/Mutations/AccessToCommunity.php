ff265a4fe622f6774d2158bbcf9f6ecfa66cb243
adnan <mh.shamey@gmail.com>
2024-11-21 10:20:52 +0300

    add Invoice ObServe for Send Notification

---
+++ app/GraphQL/Mutations/AccessToCommunity.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Enums\CommunityTypeEnum;
+use App\Exceptions\GraphQLExceptionHandler;
+use App\Models\Community;
+
+final  class AccessToCommunity
+{
+    /** @param array{} $args */
+    public function __invoke($_, array $args)
+    {
+        $community = Community::whereIn('type', [
+            CommunityTypeEnum::CHANNEL->value,
+            CommunityTypeEnum::GROUP->value,
+        ])->where('url', $args['code'])->first();
+        if ($community == null) {
+            throw new GraphQLExceptionHandler('لم يتم العثور على المجتمع يرجى التأكد من كود الدخول');
+        }
+        $community->users()->attach(auth()->id());
+
+        return $community;
+    }
+}
