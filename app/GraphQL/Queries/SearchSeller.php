9b72209866b7ae4b9499da196965e3d598849b00
adnan <mh.shamey@gmail.com>
2024-11-24 09:51:29 +0300

    search Seller

---
+++ app/GraphQL/Queries/SearchSeller.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use App\Models\User;
+
+final  class SearchSeller
+{
+    /** @param  array{}  $args */
+    public function __invoke( $_, array $args)
+    {
+        $sellers=User::whereHas('products')->where('seller_name','like',"%{$args['search']}%")->orWhere('id',$args['search'])->get();
+    }
+}
