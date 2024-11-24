62588ec0fd69e030bdcab69247dc657febdd9ef5
adnan <mh.shamey@gmail.com>
2024-11-24 15:42:20 +0300

    search Seller

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,7 @@ use Mockery\Exception;
     'login' => false,
 ]);*/
 Route::get('/test', function () {
-for($i=200;$i<=700 ;$i+=10){
-    ShippingPrice::create([
-        'weight'=>$i,
-        'size'=>'',
-    ]);
 
-}
 return "Success";
     /* for ($i = 0; $i < 60; $i++) {
         \App\Models\Message::create([
