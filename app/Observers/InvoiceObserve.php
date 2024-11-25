50d5d231faec161580e6698c79324c78c534fe09
adnan <mh.shamey@gmail.com>
2024-11-25 14:45:33 +0300

    search Seller

--- app/Observers/InvoiceObserve.php
+++ app/Observers/InvoiceObserve.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Enums\OrderStatusEnum;
 use App\Jobs\SendNotificationJob;
 use App\Models\Invoice;
+use App\Service\SendNotifyHelper;
 
 class InvoiceObserve
 {
@@ -31,6 +32,50 @@ class InvoiceObserve
     public function updated(Invoice $invoice): void
     {
 
+        $newStatus=$invoice->status;
+        $oldStatus= $invoice->getOriginal('status');
+        if($invoice->isDirty('status') && $newStatus !== $oldStatus ){
+
+            switch ($newStatus){
+                case OrderStatusEnum::CANCELED->value:
+                    $data['title'] = "طلب رقم {$invoice->id}";
+                    $data['body'] = "للأسف البضاعة غير متوفرة حالياً";
+                    $data['url'] = 'https://ali-pasha.com/exports';
+                    break;
+                case OrderStatusEnum::AGREE->value:
+                    $data['title'] = "طلب رقم {$invoice->id}";
+                    $data['body'] = "تهانينا تم قبول الطلب من التاجر يتم الآن متابعة الطلب للشحن";
+                    $data['url'] = 'https://ali-pasha.com/exports';
+                    break;
+                case OrderStatusEnum::AWAY->value:
+                    $data['title'] = "طلب رقم {$invoice->id}";
+                    $data['body'] = "جاري الشحن , الطلب بالطريق إليكم";
+                    $data['url'] = 'https://ali-pasha.com/exports';
+                    break;
+                case OrderStatusEnum::COMPLETE->value:
+                    $data['title'] = "طلب رقم {$invoice->id}";
+                    $data['body'] = "تم تسليمكم الطلب شكراً لثقتكم";
+                    $data['url'] = 'https://ali-pasha.com/exports';
+                    break;
+
+            }
+            try {
+                SendNotifyHelper::sendNotify($invoice->user ,$data);
+//                    $job=new SendNotificationJob($invoice->user,$data);
+//                    dispatch($job);
+            }catch (\Exception | \Error $e){}
+            if($newStatus==OrderStatusEnum::COMPLETE->value){
+                try {
+                    $data['title'] = "طلب رقم {$invoice->id}";
+                    $data['body'] = "تهانينا أتممت عملية بيع ناجحة";
+                    $data['url'] = 'https://ali-pasha.com/exports';
+                    SendNotifyHelper::sendNotify($invoice->seller ,$data);
+//                      $job=new SendNotificationJob($invoice->seller,$data);
+//                       dispatch($job);
+                }catch (\Exception | \Error $e){}
+            }
+
+        }
 
 
 
