99b497effb1217678bd03a02addcda956bc2019c
adnan <mh.shamey@gmail.com>
2024-11-26 13:47:53 +0300

    search Seller

--- app/Enums/OrderStatusEnum.php
+++ app/Enums/OrderStatusEnum.php
@@ -9,6 +9,7 @@ enum OrderStatusEnum: string
     case AWAY = 'away';
     case COMPLETE = 'complete';
     case CANCELED = 'canceled';
+    case CONFIRM_COMPLETE = 'confirm_complete';
 
 
 
@@ -20,6 +21,7 @@ enum OrderStatusEnum: string
             self::AWAY => 'في الطريق',
             self::COMPLETE => 'تم التسليم',
             self::CANCELED => 'ملغي',
+            self::CONFIRM_COMPLETE => 'استلام مؤكد',
 
         };
     }
@@ -32,6 +34,7 @@ enum OrderStatusEnum: string
             self::AWAY =>'fas-thumbs-up',
             self::COMPLETE =>'fas-check',
             self::CANCELED =>'fas-ban',
+            self::CONFIRM_COMPLETE =>'fas-check',
 
         };
     }
@@ -44,6 +47,7 @@ enum OrderStatusEnum: string
             self::AWAY => 'gold',
             self::COMPLETE => 'success',
             self::CANCELED => 'danger',
+            self::CONFIRM_COMPLETE => 'success',
 
         };
     }
