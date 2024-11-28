b262a7c76358f9145627f224e0a7ea16906e4b0b
adnan <mh.shamey@gmail.com>
2024-11-28 11:39:45 +0300

    add relation manager

--- app/Filament/Resources/UserResource/RelationManagers/ProductsRelationManager.php
+++ app/Filament/Resources/UserResource/RelationManagers/ProductsRelationManager.php
@@ -16,8 +16,7 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 class ProductsRelationManager extends RelationManager
 {
