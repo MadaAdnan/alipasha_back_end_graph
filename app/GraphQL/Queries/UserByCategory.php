edd4ebcd4ef1e27ba21178be9883d90ba165dcc3
adnan <mh.shamey@gmail.com>
2024-11-28 11:13:48 +0300

    search Seller

--- app/GraphQL/Queries/UserByCategory.php
+++ app/GraphQL/Queries/UserByCategory.php
@@ -9,7 +9,7 @@ final  class UserByCategory
     /** @param  array{}  $args */
     public function __invoke( $_, array $args)
     {
-       $users=User::whereHas('products',fn($query)=>$query->where('category_id',$args['category_id']))->inRandomOrder()->take(1);
+       $users=User::whereHas('products',fn($query)=>$query->where('category_id',$args['category_id']))->inRandomOrder();
        return $users;
     }
 }
