12a49088e7e5fccb4e68fae940fd1bf3b560b3ba
adnan <mh.shamey@gmail.com>
2024-11-30 15:01:49 +0300

    add relation manager

--- app/Helpers/HelperMedia.php
+++ app/Helpers/HelperMedia.php
@@ -7,9 +7,14 @@ use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 
 class HelperMedia
 {
-    public static function getFileUpload($label = 'صورة', $name = 'image', $collection = 'image', $is_multible = false, $ratio = ['1:1', '2:1', '1:2'])
+    public static function getFileUpload($label = 'صورة', $name = 'image', $collection = 'image', $is_multible = false, $ratio = ['1:1', '2:1', '1:2'],$isWebp=true)
     {
-        return SpatieMediaLibraryFileUpload::make($name)->collection($collection)->conversion('webp')->label($label)->multiple($is_multible)->image()->imageEditor()->imageEditorAspectRatios($ratio)->openable()->downloadable();
+        if($isWebp){
+            return SpatieMediaLibraryFileUpload::make($name)->collection($collection)->conversion('webp')->label($label)->multiple($is_multible)->image()->imageEditor()->imageEditorAspectRatios($ratio)->openable()->downloadable();
+
+        }
+        return SpatieMediaLibraryFileUpload::make($name)->collection($collection)->label($label)->multiple($is_multible)->image()->imageEditor()->imageEditorAspectRatios($ratio)->openable()->downloadable();
+
 
     }
 
