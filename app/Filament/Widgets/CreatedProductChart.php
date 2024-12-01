66288759771711f1a6c17225784c9dfd471c51dd
adnan <mh.shamey@gmail.com>
2024-12-01 10:55:53 +0300

    add chart

---
+++ app/Filament/Widgets/CreatedProductChart.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Product;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class CreatedProductChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected function getFilters(): ?array
+    {
+        return [
+            'today' => 'اليوم',
+            'week' => 'آخر أسبوع',
+            'month' => 'آخر شهر',
+            'year' => 'آخر سنة',
+        ];
+    }
+
+    protected function getData(): array
+    {
+        $activeFilter = $this->filter;
+        dd($activeFilter);
+        $data = Trend::model(Product::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Blog posts created',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' =>  $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
