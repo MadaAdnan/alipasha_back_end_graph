65dd8c77b8b69aae168a08825c101c63a1fe7f61
adnan <mh.shamey@gmail.com>
2024-12-29 11:09:37 +0300

    add main city

---
+++ app/Observers/CityObServe.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\City;
+
+class CityObServe
+{
+    /**
+     * Handle the City "created" event.
+     */
+    public function created(City $city): void
+    {
+        if($city->is_main==true){
+            $city->update(['city_id'=>$city->id]);
+        }
+    }
+
+    /**
+     * Handle the City "updated" event.
+     */
+    public function updated(City $city): void
+    {
+        //
+    }
+
+    /**
+     * Handle the City "deleted" event.
+     */
+    public function deleted(City $city): void
+    {
+        //
+    }
+
+    /**
+     * Handle the City "restored" event.
+     */
+    public function restored(City $city): void
+    {
+        //
+    }
+
+    /**
+     * Handle the City "force deleted" event.
+     */
+    public function forceDeleted(City $city): void
+    {
+        //
+    }
+}
