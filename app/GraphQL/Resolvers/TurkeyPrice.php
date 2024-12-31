8ec515560f706d3cf32d65493030e5addb70da72
adnan <mh.shamey@gmail.com>
2024-12-31 15:32:23 +0300

    add syrian lira

--- app/GraphQL/Resolvers/TurkeyPrice.php
+++ app/GraphQL/Resolvers/TurkeyPrice.php
@@ -2,12 +2,22 @@
 
 namespace App\GraphQL\Resolvers;
 
+use App\Models\Setting;
+
 final class TurkeyPrice
 {
 
     public static function getTurkeyPrice($root)
     {
-        $usd=33;
+        $usd=Setting::first()->dollar_value??35;
+        return [
+            "price"=>$root->price*$usd,
+            "discount"=>$root->discount*$usd,
+        ];
+    }
+    public static function getSyrPrice($root)
+    {
+        $usd=Setting::first()->dollar_value??14750;
         return [
             "price"=>$root->price*$usd,
             "discount"=>$root->discount*$usd,
