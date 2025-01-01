b5a8b531ea9307dca022141ec4fa622e3680c54a
adnan <mh.shamey@gmail.com>
2025-01-01 12:51:42 +0300

    add show invoice page

--- resources/views/invoice/table/items.blade.php
+++ resources/views/invoice/table/items.blade.php
@@ -18,7 +18,7 @@
                <td>{{$item->qty}}</td>
                <td>{{\App\Enums\IsActiveEnum::tryFrom($item->product?->is_delivery)->getLabel()}}</td>
                <td>
-                   <a href="{{\App\Filament\Resources\ProductResource::getUrl('edit',['record'=>$item->product->id])}}">ذهاب للمنتج</a>
+                   <a class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action" href="{{\App\Filament\Resources\ProductResource::getUrl('edit',['record'=>$item->product->id])}}">ذهاب للمنتج</a>
                </td>
            </tr>
 
