f4ebbad6f63155ab01b4e791f06b02f6490be0a5
adnan <mh.shamey@gmail.com>
2025-01-01 12:47:23 +0300

    add show invoice page

--- resources/views/invoice/table/items.blade.php
+++ resources/views/invoice/table/items.blade.php
@@ -12,7 +12,7 @@
     </tr>
        @foreach($getRecord()->items as $item)
            <tr>
-               <td><img style="width: 75px;border-radius: 50%" src="{{$item->product->getMediaFirstUrl('images')}}" alt=""></td>
+               <td><img style="width: 75px;border-radius: 50%" src="{{$item->product->getFirstMediaUrl('images')}}" alt=""></td>
                <td>{{$item->product->name}}</td>
                <td>{{$item->qty}}</td>
                <td>{{\App\Enums\IsActiveEnum::tryFrom($item->product?->is_delivery)->getLabel()}}</td>
