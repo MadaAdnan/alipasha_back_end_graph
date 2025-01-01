0aecbb629d70a716f8da34f13777b6d11ac53d16
adnan <mh.shamey@gmail.com>
2025-01-01 12:39:24 +0300

    add show invoice page

--- resources/views/invoice/table/items.blade.php
+++ resources/views/invoice/table/items.blade.php
@@ -1,12 +1,14 @@
 <p class="px-4 py-3 bg-gray-100 rounded-lg">
     <span class="font-medium">
-        Email address:
+       المنتجات
     </span>
 
     <table>
        @foreach($getRecord()->items as $item)
            <tr>
                <td>{{$item->product->name}}</td>
+               <td>{{$item->qty}}</td>
+               <td>{{$item->product?->is_delivery}}</td>
            </tr>
 
        @endforeach
