61cb4203dcc78faa8babae8e8c43e8733fbeb598
adnan <mh.shamey@gmail.com>
2025-01-02 14:40:34 +0300

    fix search

--- resources/views/invoice/table/seller.blade.php
+++ resources/views/invoice/table/seller.blade.php
@@ -1,7 +1,7 @@
 
 
     <table class="table table-auto divide-y divide-gray-200 dark:divide-white/5" style="width: 100%">
-    <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2">
+    <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2"  style=" background-color: #4ade80">
         <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white text-center" colspan="4">
             <span class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">التاجر</span>
         </td>
@@ -12,7 +12,7 @@
         <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$getRecord()->seller->address}} </td>
         <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$getRecord()->seller->city?->name}}</td>
     </tr>
-        <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2">
+        <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2"  style=" background-color: #92400e">
             <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white text-center" colspan="4">
                 <span class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">الزبون</span>
             </td>
