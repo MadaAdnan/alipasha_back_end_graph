c338d79a7b0ef8500761cefafe767d3a83241fa6
adnan <mh.shamey@gmail.com>
2025-01-02 14:30:58 +0300

    fix search

--- resources/views/invoice/table/items.blade.php
+++ resources/views/invoice/table/items.blade.php
@@ -1,12 +1,11 @@
-<p class="px-4 py-3 bg-gray-100 rounded-lg">
-    <span class="font-medium">
-        <span class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">المنتجات</span>
 
-
-    </span>
-
-    <table class="table table-auto" style="width: 100%">
-    <tr>
+<table class="table table-auto divide-y divide-gray-200 dark:divide-white/5" style="width: 100%">
+    <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2">
+        <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white text-center" colspan="4">
+            <span class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">المنتجات</span>
+        </td>
+    </tr>
+    <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2">
         <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">صورة</td>
         <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">المنتج</td>
         <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">الكمية </td>
@@ -14,7 +13,7 @@
 
     </tr>
        @foreach($getRecord()->items as $item)
-           <tr>
+           <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2">
                <td><img style="width: 50px;border-radius: 50%;aspect-ratio: 1/1" src="{{$item->product?->getFirstMediaUrl('images')}}" alt=""></td>
                <td><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="{{$item->product?->id !=null?\App\Filament\Resources\ProductResource::getUrl('edit',['record'=>$item->product?->id]):''}}">{{$item->product?->name}}</a></td>
                <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$item->qty}}</td>
@@ -24,4 +23,4 @@
 
        @endforeach
     </table>
-</p>
+
