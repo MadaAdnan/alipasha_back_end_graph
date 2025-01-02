00fe7e389ffbf1af94e827ce70256c8736b2c453
adnan <mh.shamey@gmail.com>
2025-01-02 14:24:45 +0300

    fix search

--- resources/views/invoice/table/seller.blade.php
+++ resources/views/invoice/table/seller.blade.php
@@ -1,10 +1,11 @@
-<p class="divide-y divide-gray-200 dark:divide-white/5">
-    <span class="font-medium">
-         <span class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">التاجر</span>
 
-    </span>
 
-    <table class="table table-auto" style="width: 100%">
+    <table class="table table-auto divide-y divide-gray-200 dark:divide-white/5" style="width: 100%">
+    <tr class="divide-y divide-gray-200 dark:divide-white/5">
+        <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" colspan="4">
+            <span class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">التاجر</span>
+        </td>
+    </tr>
     <tr class="divide-y divide-gray-200 dark:divide-white/5">
         <td><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="{{\App\Filament\Resources\UserResource::getUrl('edit',['record'=>$getRecord()->seller->id])}}">{{$getRecord()->seller->name}}</a></td>
         <td><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="https://wa.me/{{$getRecord()->seller->phone}}">{{$getRecord()->seller->phone}}</a></td>
@@ -13,4 +14,4 @@
     </tr>
 
     </table>
-</p>
+
