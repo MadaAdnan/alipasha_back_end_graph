fe864c2ead4938cd965f248094082040f2e5e4b0
adnan <mh.shamey@gmail.com>
2025-01-02 15:20:15 +0300

    fix search

--- resources/views/invoice/table/seller.blade.php
+++ resources/views/invoice/table/seller.blade.php
@@ -6,6 +6,20 @@
             <span class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">التاجر</span>
         </td>
     </tr>
+        <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2"  style=" background-color: #4ade80">
+            <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white text-center" >
+              الاسم
+            </td>
+            <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white text-center" >
+                الهاتف
+            </td>
+            <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white text-center" >
+                العنوان
+            </td>
+            <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white text-center" >
+                المحافظة
+            </td>
+        </tr>
     <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2">
         <td><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="{{\App\Filament\Resources\UserResource::getUrl('edit',['record'=>$getRecord()->seller->id])}}">{{$getRecord()->seller->name}}</a></td>
         <td><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="https://wa.me/{{$getRecord()->seller->phone}}">{{$getRecord()->seller->phone}}</a></td>
