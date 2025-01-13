33a3b6091e73c701ec97214e5034e5c6e738e150
adnan <mh.shamey@gmail.com>
2025-01-13 16:05:02 +0300

    add replay comment

--- app/GraphQL/Queries/CategoryBySeller.php
+++ app/GraphQL/Queries/CategoryBySeller.php
@@ -13,7 +13,7 @@ final class CategoryBySeller
     public function __invoke($_, array $args)
     {
         $userId = $args['sellerId'];
-        $categorisId = \DB::table('products')->where('user_id', $userId)->pluck('sub1_id')->toArray();
+        $categorisId = \DB::table('products')->whereNull('deleted_at')->where('user_id', $userId)->pluck('sub1_id')->toArray();
         return Category::whereIn('id', $categorisId) ->orderBy('sortable')->orderBy('id')->get();
     }
 }
