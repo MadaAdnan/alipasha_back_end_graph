7996aaf079f953cdd3a189379705485cf93be97c
adnan <mh.shamey@gmail.com>
2025-01-15 10:41:22 +0300

    add go to community in order

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -50,7 +50,7 @@ class UserResource extends Resource
                     Forms\Components\Wizard::make([
                         Forms\Components\Wizard\Step::make('بيانات المستخدم')->schema([
                             Forms\Components\Fieldset::make('بيانات المستخدم')->schema([
-                                Forms\Components\SpatieMediaLibraryFileUpload::make('logo')->collection('logo')->conversion('webp')
+                                Forms\Components\SpatieMediaLibraryFileUpload::make('image')->collection('image')->conversion('webp')
                                     ->openable()
                                     ->label('صورة')->imageEditor()->imageCropAspectRatio('1:1'),
 
@@ -95,7 +95,7 @@ class UserResource extends Resource
                                 Forms\Components\TextInput::make('seller_name')->label('اسم المتجر'),
                                 Forms\Components\TextInput::make('address')->label('عنوان المتجر'),
                                 Forms\Components\Textarea::make('info')->label('وصف مختصر')->columnSpan(2),
-                                Forms\Components\SpatieMediaLibraryFileUpload::make('image')->collection('image')->conversion('webp')->label('صورة Cover')->imageEditor()->imageCropAspectRatio('1:2')
+                                Forms\Components\SpatieMediaLibraryFileUpload::make('logo')->collection('logo')->conversion('webp')->label('صورة Cover')->imageEditor()->imageCropAspectRatio('1:2')
                                     ->openable()->columnSpan(2),
                                 Forms\Components\Grid::make(3)->schema([
                                     Forms\Components\Toggle::make('is_default_active')->label('تفعيل المنتجات تلقائيا'),
