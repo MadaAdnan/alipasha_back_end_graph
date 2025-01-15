0980153d4ca65593256707ba97f792c39abdcb6b
adnan <mh.shamey@gmail.com>
2025-01-15 16:25:00 +0300

    add go to community in order

--- app/GraphQL/Mutations/CreateAdvice.php
+++ app/GraphQL/Mutations/CreateAdvice.php
@@ -5,7 +5,9 @@ namespace App\GraphQL\Mutations;
 use App\Enums\ProductActiveEnum;
 use App\Exceptions\GraphQLExceptionHandler;
 use App\Models\Advice;
+use App\Models\Plan;
 use App\Models\Product;
+use Carbon\Carbon;
 
 final class CreateAdvice
 {
@@ -16,8 +18,26 @@ final class CreateAdvice
     public function __invoke($_, array $args)
     {
 
+        $plans = auth()->user()->plans()->where('ads_count', '>', 0)->get();
+        $myAdvices = Advice::where(['status' => ProductActiveEnum::ACTIVE->value, 'user_id' => auth()->id()])->count();
+        $currentPlan = null;
+        $expiredDate = now();
+        if($plans->count() == 0){
+            throw new GraphQLExceptionHandler('أنت غير مشترك بخطة إعلانات');
+        }
+        /**
+         * @var $plan Plan
+         */
+        foreach ($plans as $plan) {
+            $expiredDate = Carbon::parse($plan->pivot->expired_at);
+            $currentPlan = $plan;
+            break;
+        }
+        if (now()->greaterThan($expiredDate) || $currentPlan?->ads_count >= $myAdvices) {
+            throw new GraphQLExceptionHandler('خطتك لا تدعم المزيد من الإعلانات يرجى ترقية الحساب للمزيد');
+        }
         $data = $args['input'];
-       // throw new GraphQLExceptionHandler($data['image']);
+        // throw new GraphQLExceptionHandler($data['image']);
         $userId = auth()->id();
         $advice = Advice::create([
             'name' => $data['name'] ?? null,
@@ -26,8 +46,8 @@ final class CreateAdvice
             'category_id' => $data['category_id'] ?? null,
             'sub1_id' => $data['sub1_id'] ?? null,
             'user_id' => $userId,
-            'status' =>  ProductActiveEnum::PENDING->value,
-            'expired_date'=>now()->addWeek(),
+            'status' => ProductActiveEnum::PENDING->value,
+            'expired_date' => $expiredDate,
 
 
         ]);
