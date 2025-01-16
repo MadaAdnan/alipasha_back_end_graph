ae97e67b016310efd019a7ebf38808dd6add0e99
adnan <mh.shamey@gmail.com>
2025-01-16 13:23:59 +0300

    fix community

--- app/GraphQL/Queries/GetMyCommunity.php
+++ app/GraphQL/Queries/GetMyCommunity.php
@@ -14,19 +14,7 @@ final class GetMyCommunity
     public function __invoke($_, array $args)
     {
         $search = $args['search'] ?? '';
-      /*  $communities = Community::whereHas('users', function ($query) {
-            $query->where('users.id', Auth::id());
-        })
-            ->with(['users' => function ($query) {
-                $query->select('users.*')
-                    ->join('community_user as t', 'users.id', '=', 't.user_id')
-                    ->where('users.id', '!=', Auth::id())
-                    ->take(3);
-            }])
-            ->when(!empty($search), function ($query) use ($search) {
-                $query->where('name', 'like', "%$search%");
-            })
-            ->latest('last_update');*/
+
         $communities = Community::whereNot('type','live')->whereHas('messages')->whereHas('allUsers', function ($query) {
             $query->where('users.id', Auth::id());  // جلب المجتمعات التي يشارك فيها المستخدم الحالي
         })->when(!empty($search), function ($query) use ($search) {
