2fa33637d96c5d19311e44a0e6d0f3c16d1d8a9f
adnan <mh.shamey@gmail.com>
2025-01-16 10:11:17 +0300

    add go to community in order

--- app/GraphQL/Mutations/UpdateAdvice.php
+++ app/GraphQL/Mutations/UpdateAdvice.php
@@ -26,7 +26,7 @@ final class UpdateAdvice
             'city_id' => $data['city_id'] ?? $advice->city_id,
             'category_id' => $data['category_id'] ?? $advice->category_id,
             'sub1_id' => $data['sub1_id'] ?? $advice->sub1_id,
-            'status' => ProductActiveEnum::PENDING->value,
+            'status' =>isset($data['image']) && $data['image']!=null? ProductActiveEnum::PENDING->value:$advice->status,
 
         ]);
 
