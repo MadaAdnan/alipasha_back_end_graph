9c8017cfae50e38648ac9b69d42c24a490643c93
adnan <mh.shamey@gmail.com>
2025-01-19 09:49:14 +0300

    add login ByGoogle

--- app/Models/SocialiteUser.php
+++ app/Models/SocialiteUser.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use DutchCodingCompany\FilamentSocialite\Models\Contracts\FilamentSocialiteUser as FilamentSocialiteUserContract;
+use Exception;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Laravel\Socialite\Contracts\User as SocialiteUserContract;
 
@@ -10,7 +11,17 @@ class SocialiteUser implements FilamentSocialiteUserContract
 {
     public function getUser(): Authenticatable
     {
-        return auth()->user();
+        $user = $this->user; // Assuming $this->user is your relationship to the user model
+
+        if (!$user) {
+            // Handle the case where the user is not found
+            // For example, throw an exception or create a new user
+            throw new Exception('User not found.');
+            // Alternatively, create a new user:
+            // $user = User::create([...]);
+        }
+
+        return $user;
     }
 
     public static function findForProvider(string $provider, SocialiteUserContract $oauthUser): ?self
