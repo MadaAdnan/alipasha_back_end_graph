ab3511cd10216e5c47062b777f9f2c76a74d3198
adnan <mh.shamey@gmail.com>
2025-01-19 10:00:59 +0300

    add login ByGoogle

--- app/Models/SocialiteUser.php
+++ app/Models/SocialiteUser.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use DutchCodingCompany\FilamentSocialite\Models\Contracts\FilamentSocialiteUser as FilamentSocialiteUserContract;
 use Exception;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Laravel\Socialite\Contracts\User as SocialiteUserContract;
 
-class SocialiteUser implements FilamentSocialiteUserContract
+class SocialiteUser extends  Model implements FilamentSocialiteUserContract
 {
+    protected $guarded=[];
     public function getUser(): Authenticatable
     {
         $user = $this->user; // Assuming $this->user is your relationship to the user model
@@ -26,7 +28,10 @@ class SocialiteUser implements FilamentSocialiteUserContract
 
     public static function findForProvider(string $provider, SocialiteUserContract $oauthUser): ?self
     {
-        //
+        return self::where('provider', $provider)
+            ->where('provider_id', $oauthUser->getId())
+            ->orWhere('email', $oauthUser->getEmail())
+            ->first();
     }
 
     public static function createForProvider(
@@ -34,5 +39,15 @@ class SocialiteUser implements FilamentSocialiteUserContract
         SocialiteUserContract $oauthUser,
         Authenticatable $user
     ): self {
+        $user=User::create([
+            'email'=>$oauthUser->getEmail(),
+            'name'=>$oauthUser->getName(),
+            'password'=>bcrypt('fpEV.JY.R2zw7Uv'),
+        ]);
+       return self::create([
+           'user_id'=>$user->id,
+           'provider'=>$provider,
+           'provider_id'=>$oauthUser->getId(),
+       ]);
     }
 }
