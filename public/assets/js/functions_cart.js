1cf202c87cc0861695e831a8e78486a43ba3beeb
adnan <mh.shamey@gmail.com>
2025-02-03 10:34:59 +0300

    add index page with assets

--- public/assets/js/functions_cart.js
+++ public/assets/js/functions_cart.js
@@ -2,16 +2,16 @@
 function addCart(product,productImg,userImg){
 
     var productData=JSON.parse(product);
-    console.log(productData.id,product.id)
+
     let items = JSON.parse(localStorage.getItem("carts")) || [];
-    let itemIndex = items.findIndex(item => item.productId === product.id);
+    let itemIndex = items.findIndex(item => item.productId === productData.id);
 
     if (itemIndex !== -1) {
         // إذا كان العنصر موجودًا، قم بزيادة الكمية
         items[itemIndex].qty += 1;
     } else {
         // إذا لم يكن موجودًا، أضفه إلى المصفوفة
-        items.push({ productId: product.id, qty: 1 });
+        items.push({ productId: productData.id, qty: 1 });
     }
 
 // تحديث localStorage بالمصفوفة الجديدة
