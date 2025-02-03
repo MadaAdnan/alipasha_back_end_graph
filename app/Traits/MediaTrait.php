f97edd61eb0cff7cd0a85ee29a450d8c5ddfac38
adnan <mh.shamey@gmail.com>
2025-02-03 09:53:24 +0300

    add index page with assets

--- app/Traits/MediaTrait.php
+++ app/Traits/MediaTrait.php
@@ -40,9 +40,9 @@ trait MediaTrait
      * */
     public function getImage($collection = 'image', $conversation = 'webp'): string
     {
-        if($this->getModel() ==Product::class && ($this->type!=CategoryTypeEnum::PRODUCT->value && $this->type!=CategoryTypeEnum::RESTAURANT->value)){
+       /* if($this->getModel() ==Product::class && ($this->type!=CategoryTypeEnum::PRODUCT->value && $this->type!=CategoryTypeEnum::RESTAURANT->value)){
             return $this->user?->getFirstMediaUrl($collection, $conversation);
-        }
+        }*/
         return $this->getFirstMediaUrl($collection, $conversation);
     }
 
