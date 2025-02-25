0d1ed9279ec01ad9fe2eebf55fab98f02ac3db61
sham <albasha.sq@gmail.com>
2025-02-25 10:23:34 +0300

    sort plan by sortable

--- routes/web.php
+++ routes/web.php
@@ -60,9 +60,9 @@ Route::get('testnot/{id?}',function($id=null){
     'is_seller'=>1,
     'level'=>\App\Enums\LevelUserEnum::SELLER->value,
 ]);*/
-    \App\Models\Plan::where('type',\App\Enums\PlansTypeEnum::PRESENT->value)->where('ads_count',0)->update(['ads_count'=>1000]);
-    \App\Models\Plan::where('type',\App\Enums\PlansTypeEnum::PRESENT->value)->where('products_count',0)->update(['products_count'=>1000]);
-    \App\Models\Plan::where('type',\App\Enums\PlansTypeEnum::PRESENT->value)->where('special_count',0)->update(['special_count'=>1000]);
+    \App\Models\Plan::where('type',\App\Enums\PlansTypeEnum::PRESENT->value)->whereNull('ads_count')->update(['ads_count'=>1000]);
+    \App\Models\Plan::where('type',\App\Enums\PlansTypeEnum::PRESENT->value)->whereNull('products_count')->update(['products_count'=>1000]);
+    \App\Models\Plan::where('type',\App\Enums\PlansTypeEnum::PRESENT->value)->whereNull('special_count')->update(['special_count'=>1000]);
 return 'success';
     $message=\App\Models\Message::create([
       'body'=>fake()->paragraph,
