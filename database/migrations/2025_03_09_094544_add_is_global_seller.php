b4433317655a8d552ec474b6af5ced1020f966ec
sham <albasha.sq@gmail.com>
2025-03-09 09:53:24 +0300

    add global users and global sellers in communities

--- database/migrations/2025_03_09_094544_add_is_global_seller.php
+++ database/migrations/2025_03_09_094544_add_is_global_seller.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('communities', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_global_seller');
         });
     }
 };
