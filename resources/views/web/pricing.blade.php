0b85b8d5089b5366f40191b860c7f4d80335ff70
sham <albasha.sq@gmail.com>
2025-03-11 13:57:43 +0300

    add refresh exchange automatic

--- resources/views/web/pricing.blade.php
+++ resources/views/web/pricing.blade.php
@@ -129,7 +129,7 @@ if(auth()->check()){
                                                             <li><i class="bi bi-check-circle"></i> عدد المنتجات المميزة {{$item->special_count}}</li>
                                                         @endif
 @foreach($item->items as $option)
-                                                                <li>@if($option['active']) <i class="bi bi-check-circle"></i> @else  <i class="bi bi-cancel-circle"></i> @endif {{$option['item']}}</li>
+                                                                <li>@if($option['active']) <i class="bi bi-check-circle"></i> @else  <i class="bi bi-x-circle text-danger"></i> @endif {{$option['item']}}</li>
 @endforeach
 
 
