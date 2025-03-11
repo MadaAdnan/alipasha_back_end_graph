0ddd309089b967bacf1bbb40d23a13897b92117f
sham <albasha.sq@gmail.com>
2025-03-11 14:44:56 +0300

    add refresh exchange automatic

--- resources/views/layouts/master_layouts.blade.php
+++ resources/views/layouts/master_layouts.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{$settings->name}} @section('title')  @show</title>
+    <title>{{$settings->social['name']}} @section('title')  @show</title>
     <link rel="icon" type="image/png" href="{{asset('assets/logo.svg')}}">
     <!-- Bootstrap CSS -->
     <link
