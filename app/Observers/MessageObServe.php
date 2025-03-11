5f794ccfb8c46d7b8f4d508de13691ec5aaf1578
sham <albasha.sq@gmail.com>
2025-03-11 10:38:47 +0300

    add refresh exchange automatic

--- app/Observers/MessageObServe.php
+++ app/Observers/MessageObServe.php
@@ -7,6 +7,7 @@ use App\Events\MessageSentEvent;
 use App\Jobs\SendFirebaseNotificationJob;
 use App\Models\Community;
 use App\Models\Message;
+use App\Models\User;
 use Mockery\Exception;
 
 
@@ -24,36 +25,41 @@ class MessageObServe
             $lastMessage = now()->subMinutes()->equalTo($created_at);
 
 
-            if ($lastMessage) {
-                try {
-
-                    $ids = $community->users()->whereNot('users.id', $message->user_id)->whereNotNull('users.device_token')->pluck('users.device_token')->toArray();
-
-                    if ($community->type == CommunityTypeEnum::CHAT->value) {
-                        $name = $community->users()->whereNot('users.id', $message->user_id)->first()?->name ?? '';
-                    } else {
-                        $name = $community->name;
-                    }
-                    $data = [
-                        'title' => 'يوجد رسائل جديدة في المحادثة',
-                        'body' => $name,
-                        'url'=>'https://ali-pasha.com/communities/'.$message->community->id.'/'.$message->community->type,
-                    ];
-
-                    try {
-                        $job=new SendFirebaseNotificationJob($ids ?? [], $data);
-                        dispatch($job);
-                    } catch (\Exception | \Error $e) {
-                        info('Exception ' . $e->getMessage());
-                    }
-
-                } catch (\Exception | \Error $e) {
-                    throw new \Exception('Exception2 '.$e->getLine());
-                }
-            }
-
         } catch (\Exception | \Error $e) {
             info($e->getMessage());
+        }
+
+
+        try {
+            $ids = [];
+            $isSend = false;
+            if ($message->community->type == CommunityTypeEnum::CHAT->value) {
+                $ids = $community->users()->whereNot('users.id', $message->user_id)->whereNotNull('users.device_token')->pluck('users.device_token')->toArray();
+                $name = $community->users()->whereNot('users.id', $message->user_id)->first()?->name ?? '';
+                $isSend = true;
+            } elseif (($message->community->type == CommunityTypeEnum::GROUP->value || $message->community->type == CommunityTypeEnum::CHANNEL->value)) {
+                $name = $community->name;
+                $isSend = true;
+                /**
+                 * @var $user User
+                 */
+                foreach ($community->users as $user) {
+                    if ($user->communities()->find($community->id)?->pivot?->notify) {
+                        $ids[] = $user->device_token;
+                    }
+                }
+            }
+            if ($isSend) {
+                $data = [
+                    'title' => 'يوجد رسائل جديدة في المحادثة',
+                    'body' => $name,
+                    'url' => 'https://ali-pasha.com/communities/' . $message->community->id . '/' . $message->community->type,
+                ];
+                $job = new SendFirebaseNotificationJob($ids ?? [], $data);
+                dispatch($job);
+            }
+        } catch (\Exception | \Error $e) {
+            info('Exception ' . $e->getMessage());
         }
 
     }
