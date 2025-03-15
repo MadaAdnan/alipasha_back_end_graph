aa6e3dd25fffdafc40f7e5f54322f70ae97dfbd5
sham <albasha.sq@gmail.com>
2025-03-15 13:47:13 +0300

    add refresh exchange automatic

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -137,12 +137,7 @@ class ProductResource extends Resource
                     Forms\Components\TextInput::make('video')->label('رابط الفيديو إن وجد'),
                     Forms\Components\TextInput::make('name')->label('اسم المنتج'),
                     Forms\Components\Textarea::make('info')->label('وصف المنتج'),
-                    Forms\Components\TextInput::make('phone')->label('رقم الهاتف')->default(function($record){
-                        if($record && $record->phone==null){
-                            return $record->user?->phone;
-                        }
-                        return $record->name;
-                    }),
+                    Forms\Components\TextInput::make('phone')->label('رقم الهاتف'),
 
                     Forms\Components\TagsInput::make('tags')->suggestions(fn() => Product::product()->pluck('tags')->flatten()->unique())->label('تاغات'),
                     Forms\Components\Fieldset::make('الأسعار والتوفر')->schema([
