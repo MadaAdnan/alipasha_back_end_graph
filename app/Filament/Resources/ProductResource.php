06ca2503bd93ec17076c6ca840cc2800ebd00359
sham <albasha.sq@gmail.com>
2025-03-15 13:40:40 +0300

    add refresh exchange automatic

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -137,6 +137,8 @@ class ProductResource extends Resource
                     Forms\Components\TextInput::make('video')->label('رابط الفيديو إن وجد'),
                     Forms\Components\TextInput::make('name')->label('اسم المنتج'),
                     Forms\Components\Textarea::make('info')->label('وصف المنتج'),
+                    Forms\Components\Textarea::make('phone')->label('وصف المنتج'),
+
                     Forms\Components\TagsInput::make('tags')->suggestions(fn() => Product::product()->pluck('tags')->flatten()->unique())->label('تاغات'),
                     Forms\Components\Fieldset::make('الأسعار والتوفر')->schema([
                         Forms\Components\Toggle::make('is_available')->label('التوفر في المخزون'),
