6e7380054da723bb1fa513c02d263a2c57d2b639
sham <albasha.sq@gmail.com>
2025-03-16 10:53:10 +0300

    add refresh exchange automatic

--- resources/views/web/post-info.blade.php
+++ resources/views/web/post-info.blade.php
@@ -127,6 +127,7 @@
                                 <a href="../pages/post-info.html">
                                     <p class="title" style="text-align: right">
                                         معلومات عن المنشور
+                                        <br>
                                         {!! $post->info !!}
                                     </p>
                                     <p class="sub-title" style="text-align: right">{{$post->created_at?->diffForHumans()}}</p>
