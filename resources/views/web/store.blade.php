0bcce91c22b6d3be606d35b2be61f95c3e5aea61
sham <albasha.sq@gmail.com>
2025-03-18 12:30:39 +0300

    add refresh exchange automatic

--- resources/views/web/store.blade.php
+++ resources/views/web/store.blade.php
@@ -125,8 +125,10 @@
                           font-weight: 600;
                         "
                                         >
+                                            {{$store->total_views}}
+                                            ---
                                             @if($store->total_views<=1000 )
-                                                {{$store->total_views}}
+
                                                 @elseif($store->total_views>1000 && $store->total_views<=1000000 )
                                                 {{sprintf('%.2f', $store->total_views/1000)}} K
                                             @elseif($store->total_views>1000000)
