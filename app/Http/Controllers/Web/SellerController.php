f695ab12f6945e881df7d9679828923ec37e7dc1
sham <albasha.sq@gmail.com>
2025-03-22 11:07:51 +0300

    add refresh exchange automatic

--- app/Http/Controllers/Web/SellerController.php
+++ app/Http/Controllers/Web/SellerController.php
@@ -31,7 +31,7 @@ class SellerController extends Controller
         $categoryId=\request()->get('category_id');
         $products=Product::whereActive(ProductActiveEnum::ACTIVE->value)->where('user_id',$id)
             ->when(!empty($categoryId),fn($query)=>$query->where('category_id',$categoryId))
-            ->inRandomOrder()->latest()->paginate();
+            ->inRandomOrder()->latest()->paginate(21);
         $categoryIds=$store->products->pluck('category_id')->toArray();
         $categories = Category::whereIn('id', $categoryIds)
             ->withCount(['products' => function ($query) use ($store) {
