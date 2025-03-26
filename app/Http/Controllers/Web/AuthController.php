69cecfeb52dd18a7b7a675dafd01b4d68b687137
sham <albasha.sq@gmail.com>
2025-03-26 12:04:35 +0300

    add forget password ui

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -14,98 +14,114 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function loginUi(){
+    public function loginUi()
+    {
         return view('web.login');
     }
-    public function registerUi(){
-        $cities=City::where('is_main',true)->orderBy('name')->get();
-        return view('web.register',compact('cities'));
+
+    public function registerUi()
+    {
+        $cities = City::where('is_main', true)->orderBy('name')->get();
+        return view('web.register', compact('cities'));
     }
 
-    public function login(Request $request){
-        $email=$request->email;
-        $password=$request->password;
+    public function login(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
 
-        $user=User::where('email',$email)->first();
-        if(!$user){
-            return back()->with('error','يرجى التأكد من البيانات المدخلة');
+        $user = User::where('email', $email)->first();
+        if (!$user) {
+            return back()->with('error', 'يرجى التأكد من البيانات المدخلة');
         }
-        if(!\Hash::check($password,$user->password)){
-            return back()->with('error','يرجى التأكد من البيانات المدخلة');
+        if (!\Hash::check($password, $user->password)) {
+            return back()->with('error', 'يرجى التأكد من البيانات المدخلة');
         }
         auth()->login($user);
         return redirect('/');
     }
 
-    public function register(Request $request){
-        $this->validate($request,[
-            'name'=>'required|string|min:3',
-            'email'=>'required|email|unique:users,email',
-            'password'=>'required|min:8',
-            'confiermPassword'=>'same:password',
-            'phone'=>'required|min:10',
-            'city'=>'required|exists:cities,id',
-            'address'=>'required|string',
-        ],[
-            'name.*'=>'يرجى إدخال اسم صالح',
-            'email.*'=>'يرجى إدخال بريد إلكتروني صالح',
-            'email.unique'=>'البريد الإلكتروني موجود بالفعل',
-            'password.*'=>'يرجى إدخال كلمة مرور من 8 احرف على الأقل',
-            'confiermPassword.*'=>'كلمة المرور غير متطابقة',
-            'phone.*'=>'يرجى إدخال رقم هاتف صالح مع رمز الدولة',
-            'city'=>'يرجى تحديد المدينة',
-            'address'=>'يرجى إدخال عنوانك التفصيلي'
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|min:3',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+            'confiermPassword' => 'same:password',
+            'phone' => 'required|min:10',
+            'city' => 'required|exists:cities,id',
+            'address' => 'required|string',
+        ], [
+            'name.*' => 'يرجى إدخال اسم صالح',
+            'email.*' => 'يرجى إدخال بريد إلكتروني صالح',
+            'email.unique' => 'البريد الإلكتروني موجود بالفعل',
+            'password.*' => 'يرجى إدخال كلمة مرور من 8 احرف على الأقل',
+            'confiermPassword.*' => 'كلمة المرور غير متطابقة',
+            'phone.*' => 'يرجى إدخال رقم هاتف صالح مع رمز الدولة',
+            'city' => 'يرجى تحديد المدينة',
+            'address' => 'يرجى إدخال عنوانك التفصيلي'
         ]);
-        $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password),
-            'phone'=>$request->phone,
-            'city_id'=>$request->city,
-            'address'=>$request->address
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'phone' => $request->phone,
+            'city_id' => $request->city,
+            'address' => $request->address
         ]);
         auth()->login($user);
         return redirect('/');
     }
 
-    public function forgetPasswordUi(){
+    public function forgetPasswordUi()
+    {
         return view('web.forget-password');
     }
 
-    public function forgetPassword(Request $request){
-        $this->validate($request,[
-            'email'=>'required|email|exists:users,email'
-        ],[
-            'email.required'=>'يرجى إدخال بريدك الإلكتروني',
-            'email.email'=>'يرجى إدخال بريدك الإلكتروني',
-            'email.exists'=>'لم يتم العثور على بريدك في سجلاتنا',
+    public function forgetPassword(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|exists:users,email'
+        ], [
+            'email.required' => 'يرجى إدخال بريدك الإلكتروني',
+            'email.email' => 'يرجى إدخال بريدك الإلكتروني',
+            'email.exists' => 'لم يتم العثور على بريدك في سجلاتنا',
         ]);
-        $email=$request->email;
-        $user=User::where('email',$email)->first();
-        if(!$user){
-            return back()->with('error','لم يتم العثور على بريدك في سجلاتنا');
+        $email = $request->email;
+        $user = User::where('email', $email)->first();
+        if (!$user) {
+            return back()->with('error', 'لم يتم العثور على بريدك في سجلاتنا');
         }
-        $code=StrHelper::getResetPassword();
-        $user->update(['reset_password'=>$code]);
-        $job=new SendEmailJob($user,new ForgetPasswordEmail($code));
+        $code = StrHelper::getResetPassword();
+        $user->update(['reset_password' => $code]);
+        $job = new SendEmailJob($user, new ForgetPasswordEmail($code));
         dispatch($job);
 
-        return redirect('/')/*->route('change-password.ui',['code'=>$code])*/->with('success','تم إرسال رسالة إلى بريدك الإلكتروني');
+        return redirect('/')/*->route('change-password.ui',['code'=>$code])*/ ->with('success', 'تم إرسال رسالة إلى بريدك الإلكتروني');
     }
 
-    public function changePasswordUi(){
+    public function changePasswordUi()
+    {
         return view('web.change-password');
     }
 
-    public function changePassword(Request $request){
-        $this->validate($request,[
-            'code'=>'required|exists:users,reset_password',
-            'password'=>'required|min:8|same:confiemPassword',
+    public function changePassword(Request $request)
+    {
+        $this->validate($request, [
+            'code' => 'required|exists:users,reset_password',
+            'password' => 'required|min:8|same:confiemPassword',
 
-        ],[
-          'code.*'=>'للأسف الكود الخاص بك غير موجود يرجى طلب تغيير كلمة المرور من جديد',
-          'password.min'=>'يرجى غدخال كلمة مرور من 8 أحرف على الاقل',
-            'password.same'=>'كلمة المرور غير متطابقة'
+        ], [
+            'code.*' => 'للأسف الكود الخاص بك غير موجود يرجى طلب تغيير كلمة المرور من جديد',
+            'password.min' => 'يرجى غدخال كلمة مرور من 8 أحرف على الاقل',
+            'password.same' => 'كلمة المرور غير متطابقة'
         ]);
+
+        $user = User::whereNotNull('reset_password')->where(['reset_password' => $request->code])->first();
+        if ($user) {
+            $user->update(['password' => bcrypt($request->password), 'reset_password' => null]);
+            return redirect()->route('login.ui')->with('success', 'تم تغيير كلمة المرور');
+        }
+
     }
 }
