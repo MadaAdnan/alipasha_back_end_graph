bc6bf94e1f7590c083fce39226ae6f500ed07d30
sham <albasha.sq@gmail.com>
2025-04-06 11:46:11 +0300

    add forget password ui

--- resources/views/web/community.blade.php
+++ resources/views/web/community.blade.php
@@ -8,7 +8,7 @@
                     @php
                         $user=$message->user;
                     @endphp
-                    <div class="d-flex flex-column align-items-center justify-content-center">
+                    <div class="d-flex flex-column align-items-center justify-content-start">
                        <div class="d-flex flex-row justify-content-center">
                            <img style="width: 50px;height: 50px" src="{{$user?->getImage()}}" alt="" class="rounded-circle">
                            <span>{{$user?->seller_name ??$user?->name}}</span>
