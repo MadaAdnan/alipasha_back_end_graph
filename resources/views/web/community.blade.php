251c6bfb20415e0ec71a44fe99c89f06e3612e9a
sham <albasha.sq@gmail.com>
2025-04-06 12:24:00 +0300

    add forget password ui

--- resources/views/web/community.blade.php
+++ resources/views/web/community.blade.php
@@ -40,7 +40,7 @@ foreach ($body as $b){
                             @method('post')
                             <button class="btn btn-sm btn-outline-danger" type="submit"><i class="bi bi-send-fill"></i></button>
                             <div class="bg-secondary-subtle w-100 p-1 rounded">
-                                <input type="text" class="form-control " name="body">
+                                <input type="text" class="form-control " name="body" placeholder="اكتب شيئاً ...">
                             </div>
                             <input type="hidden" name="communityId" value="{{$community->id}}">
 
