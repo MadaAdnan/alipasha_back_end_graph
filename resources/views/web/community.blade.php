05c2805de0c1a8e9657839dfbffe6a390306fc91
sham <albasha.sq@gmail.com>
2025-04-06 12:06:02 +0300

    add forget password ui

--- resources/views/web/community.blade.php
+++ resources/views/web/community.blade.php
@@ -37,7 +37,7 @@ foreach ($body as $b){
                     <div class="position-absolute bottom-0 w-100">
                         <form action="" class="d-flex gap-1">
                             <input type="text" class="form-control">
-                            <button class="btn btn-sm" type="submit"><i class="bi bi-send-fill"></i></button>
+                            <button class="btn btn-sm btn-outline-danger" type="submit"><i class="bi bi-send-fill"></i></button>
                         </form>
                     </div>
                 </div>
