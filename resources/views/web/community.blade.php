0b64d11c161561ebfd1f17e81c41c518ce5cfc95
sham <albasha.sq@gmail.com>
2025-04-06 12:12:06 +0300

    add forget password ui

--- resources/views/web/community.blade.php
+++ resources/views/web/community.blade.php
@@ -35,9 +35,11 @@ foreach ($body as $b){
 
                 @endforeach
                     <div class="position-absolute bottom-0 w-100">
-                        <form action="" class="d-flex gap-1 px-3">
-                            <input type="text" class="form-control">
+                        <form action="" method="post" class="d-flex gap-1 px-3">
                             <button class="btn btn-sm btn-outline-danger" type="submit"><i class="bi bi-send-fill"></i></button>
+                            <input type="text" class="form-control" name="message">
+                            <input type="hidden" name="communityId" value="{{$community->id}}">
+
                         </form>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ Route::middleware('auth:web')->group(function (){
     Route::resource('/profile',\App\Http\Controllers\Web\ProfileController::class);
     Route::resource('/comments',\App\Http\Controllers\Web\CommentController::class)->only(['store']);
     Route::resource('/communities',\App\Http\Controllers\Web\CommunityController::class)->only(['index','show','store']);
+    Route::resource('/messages',\App\Http\Controllers\Web\MessageController::class)->only(['store']);
     Route::post('/markets/followers',[\App\Http\Controllers\Web\SellerController::class,'followers']);
     Route::post('/products/like',[\App\Http\Controllers\Web\PostController::class,'like'])->name('post.like');
 });
