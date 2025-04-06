fb5903bd273e37b6573d7a9b1b9249dc50c92281
sham <albasha.sq@gmail.com>
2025-04-06 12:35:27 +0300

    add forget password ui

--- resources/views/web/community.blade.php
+++ resources/views/web/community.blade.php
@@ -5,40 +5,45 @@
 
             <div class="col-md-6 ">
                 <div class="card position-relative pb-5 px-2">
-                @foreach($messages as $message)
-                    @php
-                        $user=$message->user;
-                    @endphp
-                    <div class="d-flex flex-column @if($user->id==auth()->id()) align-items-start  @else align-items-end  @endif justify-content-center">
-                       <div class="d-flex flex-row justify-content-center align-items-center @if($user->id!=auth()->id()) flex-row-reverse    @endif">
-                           <img style="width: 50px;height: 50px" src="{{$user?->getImage()}}" alt="" class="rounded-circle">
-                           <span>{{$user?->seller_name ??$user?->name}}</span>
-                       </div>
-                        <div class="card card-body my-1 w-75 @if($user->id==auth()->id()) bg-success-subtle   @endif">
-                            <div class="d-flex flex-column gap-2">
-@php
-    $body=explode(' ',$message->body);
-$messageBody='';
-foreach ($body as $b){
-    if(filter_var(trim($b,'.'),FILTER_SANITIZE_URL) ){
-        $messageBody.=" <a href='{$b}'>{$b}</a> ";
-    }else{
-        $messageBody.=" {$b}";
-    }
-}
-@endphp
-                                <span>{!! $messageBody!!}</span>
-                                <span class="text-muted small">{{$message->created_at?->diffForHumans()}}</span>
+                    @foreach($messages as $message)
+                        @php
+                            $user=$message->user;
+                        @endphp
+                        <div
+                            class="d-flex flex-column @if($user->id==auth()->id()) align-items-start  @else align-items-end  @endif justify-content-center">
+                            <div
+                                class="d-flex flex-row justify-content-center align-items-center @if($user->id!=auth()->id()) flex-row-reverse    @endif">
+                                <img style="width: 50px;height: 50px" src="{{$user?->getImage()}}" alt=""
+                                     class="rounded-circle">
+                                <span>{{$user?->seller_name ??$user?->name}}</span>
+                            </div>
+                            <div
+                                class="card card-body my-1 w-75 @if($user->id==auth()->id()) bg-success-subtle   @endif">
+                                <div class="d-flex flex-column gap-2">
+                                    @php
+                                        $body=explode(' ',$message->body);
+                                    $messageBody='';
+                                    foreach ($body as $b){
+                                        if(filter_var(trim($b,'.'),FILTER_SANITIZE_URL) ){
+                                            $messageBody.=" <a href='{$b}'>{$b}</a> ";
+                                        }else{
+                                            $messageBody.=" {$b}";
+                                        }
+                                    }
+                                    @endphp
+                                    <span>{!! $messageBody!!}</span>
+                                    <span class="text-muted small">{{$message->created_at?->diffForHumans()}}</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                @endforeach
+                    @endforeach
                     <div class="position-absolute bottom-0  w-100  overflow-hidden">
                         <form action="{{route('messages.store')}}" method="post" class="d-flex w-100 gap-1 px-3 py-1 ">
                             @csrf
                             @method('post')
-                            <button class="btn btn-sm btn-outline-danger" type="submit"><i class="bi bi-send-fill"></i></button>
+                            <button class="btn btn-sm btn-outline-danger" type="submit"><i class="bi bi-send-fill"></i>
+                            </button>
                             <div class="bg-secondary-subtle w-100 p-1 rounded">
                                 <input type="text" class="form-control " name="body" placeholder="اكتب شيئاً ...">
                             </div>
