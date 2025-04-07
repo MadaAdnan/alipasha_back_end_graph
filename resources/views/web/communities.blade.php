3a04062149eeebb856670d2536f6bc81b4e2ffcb
sham <albasha.sq@gmail.com>
2025-04-07 11:48:42 +0300

    add forget password ui

--- resources/views/web/communities.blade.php
+++ resources/views/web/communities.blade.php
@@ -7,8 +7,11 @@
                 @foreach($communities as $community)
                 <div class="card card-body">
                     <div class="d-flex align-items-center">
-                        <img src="{{$community->getImage()}}" style="width: 100px;" alt="" class="rounded-circle">
-                        <span>{{$community->name}}</span>
+                      <div>
+                          <img src="{{$community->getImage()}}" style="width: 100px;" alt="" class="rounded-circle">
+                          <span>{{$community->name}}</span>
+                      </div>
+                        <a href="{{route('communities.show',$community->id)}}" class="btn btn-sm btn-info">إنتقل إلى المحادثة</a>
                     </div>
                 </div>
                 @endforeach
