85b91d2ea9a0b61f70fba10e51b1f66491136251
sham <albasha.sq@gmail.com>
2025-04-07 11:49:48 +0300

    add forget password ui

--- resources/views/web/communities.blade.php
+++ resources/views/web/communities.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-6">
                 @foreach($communities as $community)
                 <div class="card card-body">
-                    <div class="d-flex align-items-center justify-content-around">
+                    <div class="d-flex align-items-center justify-content-between">
                       <div>
                           <img src="{{$community->getImage()}}" style="width: 100px;" alt="" class="rounded-circle mx-2">
                           <span>{{$community->name}}</span>
