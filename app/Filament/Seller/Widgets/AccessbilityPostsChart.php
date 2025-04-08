0d4b25371bad9254ddb5f7cdd7711d5f738f3d66
sham <albasha.sq@gmail.com>
2025-04-08 12:55:23 +0300

    add forget password ui

---
+++ app/Filament/Seller/Widgets/AccessbilityPostsChart.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Seller\Widgets;
+
+use App\Models\ProductView;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class AccessbilityPostsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected function getData(): array
+    {
+
+        $data = Trend::query(ProductView::whereHas('product', fn($query) => $query->where('products.user_id', auth()->id())))
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->sum('count');
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Blog posts',
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn(TrendValue $value) => $value->date),
+        ];
+
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
