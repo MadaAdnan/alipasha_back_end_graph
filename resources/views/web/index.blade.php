47c2fe9f97466829ff64f990d2456ddb469dd24a
sham <albasha.sq@gmail.com>
2025-04-08 12:32:23 +0300

    add forget password ui

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -386,7 +386,7 @@
             <div id="left-sidebar" class="col-3 d-none d-xl-block">
                 <div class="media-scroll bg-light p-4">
                     <div style="text-align: center">
-                        <a href="{{\App\Filament\Seller\Resources\ProductResource::getUrl('/create')}}">
+                        <a href="{{url(\App\Filament\Seller\Resources\ProductResource::getUrl('/create'))}}">
                         <button
                             class="new-post"
                          {{--   data-bs-toggle="modal"
