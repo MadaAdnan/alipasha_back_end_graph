fbea42063b2cbdae0abadce2684bf13855430f0d
sham <albasha.sq@gmail.com>
2025-04-08 12:31:32 +0300

    add forget password ui

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -386,6 +386,7 @@
             <div id="left-sidebar" class="col-3 d-none d-xl-block">
                 <div class="media-scroll bg-light p-4">
                     <div style="text-align: center">
+                        <a href="{{\App\Filament\Seller\Resources\ProductResource::getUrl('/create')}}">
                         <button
                             class="new-post"
                             data-bs-toggle="modal"
@@ -393,6 +394,7 @@
                         >
                             منشور جديد
                         </button>
+                        </a>
                     </div>
                     <div class="categories">
                         <p class="category-text">التصنيفات</p>
