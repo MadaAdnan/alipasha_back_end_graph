8eff61e1ff4c6164c99116a8fc511b515a8f7148
sham <albasha.sq@gmail.com>
2025-04-08 12:50:02 +0300

    add forget password ui

---
+++ app/Filament/Seller/Widgets/StatsOverview.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Seller\Widgets;
+
+use App\Enums\ProductActiveEnum;
+use App\Models\Product;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $product=Product::product()->where('user_id',auth()->id())->where('active',ProductActiveEnum::ACTIVE->value)->count();
+        $jobs= Product::job()->where('user_id',auth()->id())->where('active',ProductActiveEnum::ACTIVE->value)->count();
+        $tenders=Product::product()->where('user_id',auth()->id())->where('active',ProductActiveEnum::ACTIVE->value)->count();
+      $list=[];
+      if($product>0){
+          $list[]=Stat::make('عدد المنتجات المفعلة',$product );
+      }
+      if($jobs>0){
+          $list[]=Stat::make('عدد الوظائف المفعلة',$jobs);
+      }
+      if($tenders>0){
+          Stat::make('عدد المناقصات المفعلة', $tenders);
+      }
+      return $list;
+    }
+}
