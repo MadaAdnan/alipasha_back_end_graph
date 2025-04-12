7d5734955d0ca3edc0f44041dd04075ef6770874
sham <albasha.sq@gmail.com>
2025-04-12 11:41:07 +0300

    add forget password ui

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.master_layouts')
 @section('content')
-    <h4 class="alert alert-danger">الصفحة غير موجودة</h4>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h4 class="alert alert-danger">الصفحة غير موجودة</h4>
+            </div>
+        </div>
+    </div>
 @endsection
