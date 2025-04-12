ed3722a3dbc50e935050181b0036ed657043cd93
sham <albasha.sq@gmail.com>
2025-04-12 12:46:59 +0300

    add forget password ui

--- app/Http/Controllers/Web/IndexController.php
+++ app/Http/Controllers/Web/IndexController.php
@@ -28,7 +28,7 @@ class IndexController extends Controller
         $notifications=null;
         if(auth()->check()){
             // unreadNotifications
-            $notifications=auth()->user()->notifications()->limit(7)->get();
+            $notifications=auth()->user()->unreadNotifications()->limit(7)->get();
             auth()->user()->unreadNotifications->markAsRead();
         }
         $products = Product::when($categoryId,fn($query)=>$query->where('category_id',$categoryId))
