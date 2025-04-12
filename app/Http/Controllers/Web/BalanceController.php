14108748c0574fa15bbf2aca1a0d55eb1b97e42a
sham <albasha.sq@gmail.com>
2025-04-12 12:57:53 +0300

    add forget password ui

--- app/Http/Controllers/Web/BalanceController.php
+++ app/Http/Controllers/Web/BalanceController.php
@@ -13,7 +13,7 @@ class BalanceController extends Controller
      */
     public function index()
     {
-        $balances=Balance::where('user_id',auth()->id())->latest()->paginate(25);
+        $balances=Balance::where('user_id',auth()->id())->latest()->paginate(50);
         return view('web.balance',compact('balances'));
     }
 
