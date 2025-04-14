5fe48e37e761efdfbe9aa48c1ac4e6dbac1bfc60
sham <albasha.sq@gmail.com>
2025-04-14 11:17:13 +0300

    add carts DB

--- resources/views/web/cart.blade.php
+++ resources/views/web/cart.blade.php
@@ -58,8 +58,8 @@
 
                     </tr>
                     <tr>
-                        <th colspan="2" class="text-center bg-info-subtle">الإجمالي</th>
-                        <th colspan="1" class="text-center bg-info-subtle">{{$result+$shipping}} $</th>
+                        <th colspan="2" class="text-center bg-danger-subtle">الإجمالي</th>
+                        <th colspan="1" class="text-center bg-danger-subtle">{{$result+$shipping}} $</th>
                         <th colspan="4" class="text-center bg-info-subtle">
                             @if($items->count()>0)
                             <form action="{{route('carts.update',$user->id)}}" method="post">
