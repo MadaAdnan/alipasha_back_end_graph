9d5e666aca23549335c544a0550f524e08bb5e49
sham <albasha.sq@gmail.com>
2025-05-01 11:00:56 +0300

    add can special and fix create ,update products

---
+++ app/GraphQL/Mutations/CreateNewInvoice.php
@@ -0,0 +1,95 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Enums\OrderStatusEnum;
+use App\Exceptions\GraphQLExceptionHandler;
+use App\Models\Balance;
+use App\Models\Invoice;
+use App\Models\Item;
+use App\Models\Product;
+use App\Models\ShippingPrice;
+use App\Models\User;
+
+final  class CreateInvoice
+{
+    /** @param array{} $args */
+    public function __invoke($_, array $args)
+    {
+        $data = $args['input'];
+        if(!auth()->user()->is_active){
+            throw new GraphQLExceptionHandler('تم حظر حسابك يرجى مراجعة الإدارة');
+        }
+
+        if(auth()->user()->getTotalBalance()<=0){
+            throw new GraphQLExceptionHandler('لا تملك رصيد كاف لإتمام الطلب');
+        }
+        \DB::beginTransaction();
+        try {
+
+            $weight=0;
+            if(!auth()->check() || auth()->id()==null){
+                throw new GraphQLExceptionHandler('خطأ في الطلب يرجى المحاولة من جديد');
+            }
+
+            $invoice = new Invoice();
+            $invoice->seller_id = $data['seller_id'];
+            $invoice->user_id = auth()->id();
+            $invoice->phone =$data['phone'] ??auth()->user()->phone;
+            $invoice->address =$data['address'] ?? auth()->user()->address;
+            $invoice->status = OrderStatusEnum::PENDING->value;
+            $invoice->save();
+
+            $seller = User::findOrFail($data['seller_id']);
+
+            $total = 0;
+            foreach ($data['items'] as $item) {
+                $product = Product::find($item['product_id']);
+                if($product->is_delivery){
+                    $weight+=$product->weight;
+                }
+                if (!$product) {
+                    throw new \Exception("Product with ID {$item['product_id']} not found.");
+                }
+                $price = $product->is_discount ? $product->discount : $product->price;
+                $total_price = $price * $item['qty'];
+                Item::create([
+                    'invoice_id' => $invoice->id, // $invoice->id متاح الآن
+                    'product_id' => $item['product_id'],
+                    'price' => $price,
+                    'qty' => $item['qty'],
+                    'total' => $total_price,
+                ]);
+                $total += $total_price;
+            }
+            $ship_price = ShippingPrice::where('weight', '>=', $weight)->orderBy('weight')->first();
+            if ($ship_price == null) {
+                throw new \Exception('لا يتوفر توصيل حالياً');
+            }
+            // set Far
+            if($weight==0){
+                $far=0;
+            }elseif (auth()->user()->getIsSameMainCity($seller)) {
+                $far = $ship_price->internal_price;
+            } else {
+                $far = $ship_price->external_price;
+            }
+            $invoice->weight = $weight;
+            $invoice->shipping = $far;
+            $invoice->total = $total;
+            $invoice->save();
+
+            $balance=Balance::create([
+                'credit'=>0,
+                'debit'=>10,
+                'info'=>"طلب شحن رقم {$invoice->id}",
+                'user_id'=>auth()->id(),
+            ]);
+            \DB::commit();
+            return $invoice;
+        } catch (\Exception | \Error $e) {
+            \DB::rollBack();
+            throw new GraphQLExceptionHandler($e->getMessage().'ase');
+        }
+    }
+}
