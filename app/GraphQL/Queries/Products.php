74c82f8db42dd144231267b97df19dc702aed5eb
sham <albasha.sq@gmail.com>
2025-05-01 18:11:50 +0300

    add can special and fix create ,update products

--- app/GraphQL/Queries/Products.php
+++ app/GraphQL/Queries/Products.php
@@ -84,7 +84,7 @@ final class Products
 
             }))
             // ->whereNotNull('sub1_id')
-          //  ->orderBy($orderBy['column'], $orderBy['orderBy'])
+            ->orderBy($orderBy['column'], $orderBy['orderBy'])
         ;
 
 
