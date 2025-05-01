0b419d6ec78f12c211df5081c55bc3ee86fe290e
sham <albasha.sq@gmail.com>
2025-05-01 12:48:41 +0300

    add can special and fix create ,update products

--- app/GraphQL/Queries/Products.php
+++ app/GraphQL/Queries/Products.php
@@ -56,7 +56,7 @@ final class Products
                     $query->where('user_id',$userId);
                 }
             })
-           // ->when($userId!='', fn($query) => $query->where('user_id', $userId))
+            ->when($userId!='', fn($query) => $query->where('user_id', $userId))
             ->when($type === 'product' && isset($args['max_price']) && $args['max_price'] > 0, fn($query) => $query->where('price', '>=', [$args['min_price'] ?? 0])->where('price', "<=", $args['max_price'] ?? 10000))
             ->when(collect($colors ?? [])->count() > 0, fn($query) => $query->whereHas('colors', fn($q) => $q->whereIn('colors.id', $colors)))
 
