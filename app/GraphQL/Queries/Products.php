9317118d4c47e3687489f41e46547e47df77fad6
sham <albasha.sq@gmail.com>
2025-05-01 11:41:36 +0300

    add can special and fix create ,update products

--- app/GraphQL/Queries/Products.php
+++ app/GraphQL/Queries/Products.php
@@ -50,10 +50,7 @@ final class Products
                     $query->where('type', $type);
                 }
             })
-           ->when($categoryId!=null, fn($query) => $query->where('category_id', $categoryId))
-            ->when($sub1Id!=null, fn($query) => $query->where('sub1_id', $sub1Id))
-            ->when($cityId!=null, fn($query) => $query->where('city_id', $cityId))
-            ->when($userId!=null, fn($query) => $query->where('user_id', $userId))
+
             ->when(isset($args['search']) && !empty($args['search']) && $type !== 'seller', fn($query) => $query->where(function ($query) use ($args) {
 
                 /**
