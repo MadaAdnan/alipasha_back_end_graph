f9cdc92689057443ae8dbead7193e1958d2cc008
sham <albasha.sq@gmail.com>
2025-05-06 06:08:05 +0300

    fix city_id and area_id

--- routes/web.php
+++ routes/web.php
@@ -81,8 +81,48 @@ Route::get('/.well-known/assetlinks.json', function () {
 
 
 Route::get('testnot/{id?}',function($id=null){
-    $user=User::where('email','mh.shamey@gmail.com')->first();
-    \App\Jobs\SendFirebaseNotificationJob::dispatch([$user->device_token],['title'=>'test','body'=>'Test Body']);
+  /* $users=User::with('city')->whereNull('area_id')->get();
+   foreach ($users as $user){
+       if($user->city?->is_main){
+           continue;
+       }
+       if($user->city?->city_id!=null){
+           $user->update([
+               'city_id'=>$user->city?->city_id,
+               'area_id'=>$user->city_id,
+           ]);
+       }
+
+   }*/
+    $users = User::with('city')
+        ->whereNull('area_id')
+        ->get();
+
+    $updates = [];
+
+    foreach ($users as $user) {
+        $city = $user->city;
+
+        if (!$city || $city->is_main || is_null($city->city_id)) {
+            continue;
+        }
+
+        $updates[] = [
+            'id' => $user->id,
+            'city_id' => $city->city_id,
+            'area_id' => $user->city_id,
+        ];
+    }
+
+// تنفيذ التحديثات بشكل جماعي
+    foreach ($updates as $data) {
+        DB::table('users')
+            ->where('id', $data['id'])
+            ->update([
+                'city_id' => $data['city_id'],
+                'area_id' => $data['area_id'],
+            ]);
+    }
     return 'success';
 });
 Route::get('/server-resources', function() {
