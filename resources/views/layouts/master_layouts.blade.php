81bafafdf5cd9dc56a29b9fa89204af133060852
sham <albasha.sq@gmail.com>
2025-05-10 09:08:59 +0300

    fix city_id and area_id

--- resources/views/layouts/master_layouts.blade.php
+++ resources/views/layouts/master_layouts.blade.php
@@ -148,7 +148,7 @@ required=""
         <div class="d-lg-flex align-items-center gap-2">
             <a href="https://ali-pasha.com/app/qr">
                 <button
-                    class="btn"
+                    class="btn d-sm-none"
                     style="
               background-color: red;
               color: white;
