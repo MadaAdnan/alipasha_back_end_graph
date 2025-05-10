8dbd9c3edaeb9efec391d3b8290f52fcf1ed729f
sham <albasha.sq@gmail.com>
2025-05-10 11:42:54 +0300

    fix city_id and area_id

--- app/Filament/Resources/VendorResource.php
+++ app/Filament/Resources/VendorResource.php
@@ -37,7 +37,7 @@ class VendorResource extends Resource
                 Tables\Columns\TextColumn::make('name'),
                 Tables\Columns\SelectColumn::make('city_id')->options(City::where('is_main',1)->orderBy('name')->pluck('name','id'))->label('المحافظة'),
                 Tables\Columns\SelectColumn::make('area_id')->options(City::where('is_main',false)->orderBy('name')->pluck('name','id'))->label('المدينة')->sortable(),
-                Tables\Columns\TextColumn::make('phone')->url(fn($record)=>$record->phone!=''?'https://wa.me/'.$record->phone.'?text=يكتب السلام عليكم معك الدعم الفني لتطبيق علي باشا الرجاء إرسال العنوان الدقيق لتحديث بيناتك معرفك هو '.$record->id:""),
+                Tables\Columns\TextColumn::make('phone')->url(fn($record)=>$record->phone!=''?'https://wa.me/'.$record->phone.'?text= السلام عليكم معك الدعم الفني لتطبيق علي باشا الرجاء إرسال العنوان الدقيق لتحديث بيناتك معرفك هو '.$record->id:""),
                 Tables\Columns\TextColumn::make('address'),
             ])
             ->filters([
@@ -45,7 +45,7 @@ class VendorResource extends Resource
                 Tables\Filters\SelectFilter::make('area_id')->options(City::where('is_main',0)->orderBy('name')->pluck('name','id'))->label('المدينة'),
             ])
             ->actions([
-                Tables\Actions\Action::make('whats')->url(fn($record)=>$record->phone!=''?'https://wa.me/'.$record->phone.'?text=يكتب السلام عليكم معك الدعم الفني لتطبيق علي باشا الرجاء إرسال العنوان الدقيق لتحديث بيناتك معرفك هو '.$record->id:"")->label('تواصل واتس'),
+                Tables\Actions\Action::make('whats')->url(fn($record)=>$record->phone!=''?'https://wa.me/'.$record->phone.'?text= السلام عليكم معك الدعم الفني لتطبيق علي باشا الرجاء إرسال العنوان الدقيق لتحديث بيناتك معرفك هو '.$record->id:"")->label('تواصل واتس'),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
