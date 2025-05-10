dc41bc25f9363b1e33ccff0608bfaaaa8e669040
sham <albasha.sq@gmail.com>
2025-05-10 11:33:51 +0300

    fix city_id and area_id

--- app/Filament/Resources/VendorResource/Pages/ListVendors.php
+++ app/Filament/Resources/VendorResource/Pages/ListVendors.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\VendorResource\Pages;
 
 use App\Filament\Resources\VendorResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListVendors extends ListRecords
@@ -14,6 +15,15 @@ class ListVendors extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+            'الكل'=>Tab::make('all')->query(fn($query)=>$query)->label('الكل'),
+            'محظور'=>Tab::make('block')->query(fn($query)=>$query->where('is_active',0))->label('محظور'),
+            'غير مؤكد'=>Tab::make('not_verified')->query(fn($query)=>$query->whereNull('email_verified_at'))->label('غير مؤكد'),
+            'متاجر'=>Tab::make('is_seller')->query(fn($query)=>$query->where('is_seller',1))->label('متاجر'),
         ];
     }
 }
