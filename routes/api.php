720b8f78df85cc5ceb8022c88f5d3a4a3b3e5e2e
sham <albasha.sq@gmail.com>
2025-05-10 10:25:01 +0300

    fix city_id and area_id

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Broadcast::routes(['middleware' => ['auth:sanctum','throttle:5,1']]);
+Broadcast::routes(['middleware' => ['auth:sanctum','throttle:10,1']]);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
