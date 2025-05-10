e213224750781520c9b1e3055060d6e67c85bda2
sham <albasha.sq@gmail.com>
2025-05-10 11:57:18 +0300

    fix city_id and area_id

--- app/Filament/Resources/VendorResource.php
+++ app/Filament/Resources/VendorResource.php
@@ -7,6 +7,7 @@ use App\Filament\Resources\VendorResource\RelationManagers;
 use App\Models\City;
 use App\Models\User;
 use App\Models\Vendor;
+use Filament\Actions\DeleteAction;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -20,7 +21,7 @@ class VendorResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-    protected static ?string $slug='vendors';
+    protected static ?string $slug = 'vendors';
 
     public static function form(Form $form): Form
     {
@@ -35,17 +36,19 @@ class VendorResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\SelectColumn::make('city_id')->options(City::where('is_main',1)->orderBy('name')->pluck('name','id'))->label('المحافظة'),
-                Tables\Columns\SelectColumn::make('area_id')->options(City::where('is_main',false)->orderBy('name')->pluck('name','id'))->label('المدينة')->sortable(),
-                Tables\Columns\TextColumn::make('phone')->url(fn($record)=>$record->phone!=''?'https://wa.me/'.$record->phone.'?text= السلام عليكم معك الدعم الفني لتطبيق علي باشا الرجاء إرسال العنوان الدقيق لتحديث بيناتك معرفك هو '.$record->id:"",true),
-                Tables\Columns\TextColumn::make('address'),
+                Tables\Columns\TextColumn::make('products_count')->label('المنتجات'),
+                Tables\Columns\SelectColumn::make('city_id')->options(City::where('is_main', 1)->orderBy('name')->pluck('name', 'id'))->label('المحافظة'),
+                Tables\Columns\SelectColumn::make('area_id')->options(City::where('is_main', false)->orderBy('name')->pluck('name', 'id'))->label('المدينة')->sortable(),
+                Tables\Columns\TextColumn::make('phone')->url(fn($record) => $record->phone != '' ? 'https://wa.me/' . $record->phone . '?text= السلام عليكم معك الدعم الفني لتطبيق علي باشا الرجاء إرسال العنوان الدقيق لتحديث بيناتك معرفك هو ' . $record->id : "", true),
+                Tables\Columns\TextColumn::make('address')->words(5),
             ])
             ->filters([
-                Tables\Filters\SelectFilter::make('city_id')->options(City::where('is_main',1)->orderBy('name')->pluck('name','id'))->label('المحافظة'),
-                Tables\Filters\SelectFilter::make('area_id')->options(City::where('is_main',0)->orderBy('name')->pluck('name','id'))->label('المدينة'),
+                Tables\Filters\SelectFilter::make('city_id')->options(City::where('is_main', 1)->orderBy('name')->pluck('name', 'id'))->label('المحافظة'),
+                Tables\Filters\SelectFilter::make('area_id')->options(City::where('is_main', 0)->orderBy('name')->pluck('name', 'id'))->label('المدينة'),
             ])
             ->actions([
-                Tables\Actions\Action::make('whats')->url(fn($record)=>$record->phone!=''?'https://wa.me/'.$record->phone.'?text= السلام عليكم معك الدعم الفني لتطبيق علي باشا الرجاء إرسال العنوان الدقيق لتحديث بيناتك معرفك هو '.$record->id:"",true)->label('تواصل واتس'),
+                Tables\Actions\Action::make('whats')->url(fn($record) => $record->phone != '' ? 'https://wa.me/' . $record->phone . '?text= السلام عليكم معك الدعم الفني لتطبيق علي باشا الرجاء إرسال العنوان الدقيق لتحديث بيناتك معرفك هو ' . $record->id : "", true)->label('تواصل واتس'),
+                DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
