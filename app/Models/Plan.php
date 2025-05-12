2e8b1660a0f9d0130193e26e62957b1026057214
sham <albasha.sq@gmail.com>
2025-05-12 14:09:46 +0300

    add is_validate plans table

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -27,6 +27,6 @@ class Plan extends Model
     }
     public function users(): BelongsToMany
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class)->withPivot(['expired_date','subscription_date']);
     }
 }
