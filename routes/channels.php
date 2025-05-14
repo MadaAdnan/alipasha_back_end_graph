2b654d4a0c7cd0723197e0a2cb5fc963cedadff2
sham <albasha.sq@gmail.com>
2025-05-14 16:51:03 +0300

    fix realtime

--- routes/channels.php
+++ routes/channels.php
@@ -17,9 +17,9 @@ use Illuminate\Support\Facades\Broadcast;
 });*/
 
 
-/*Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+Broadcast::channel('user.{id}', function ($user, $id) {
     return (int)$user->id === (int)$id;
-});*/
+});
 Broadcast::channel('message.{communityId}.{userId}', function (\App\Models\User $user, $communityId,$userId) {
 
     return in_array($communityId,$user->communities->pluck('id')->toArray()) && $user->id==$userId;
