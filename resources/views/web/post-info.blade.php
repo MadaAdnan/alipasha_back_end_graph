335d9b0e147f952d6a7e3f059ded017f6c96a677
sham <albasha.sq@gmail.com>
2025-05-19 10:34:19 +0300

    fix realtime

--- resources/views/web/post-info.blade.php
+++ resources/views/web/post-info.blade.php
@@ -243,7 +243,7 @@
                                        @if(!$post->discount)
                                            ${{$post->price}}
                                         @else
-                                           <del style="font-size: 9pt">${{$post->price}}</del> &nbsp;&nbsp;
+                                           <del style="font-size: 9pt">${{$post->price}} &nbsp;&nbsp;</del>
                                            <span style="font-weight: bold;font-size: 14pt">${{$post->discount}}</span>
                                         @endif
                                     </div>
