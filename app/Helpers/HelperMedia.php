7db67b321e4cbbe0537eefa6b14bc056e38fe82f
sham <albasha.sq@gmail.com>
2025-05-20 11:58:16 +0300

    fix realtime

--- app/Helpers/HelperMedia.php
+++ app/Helpers/HelperMedia.php
@@ -10,7 +10,9 @@ class HelperMedia
     public static function getFileUpload($label = 'صورة', $name = 'image', $collection = 'image', $is_multible = false, $ratio = ['1:1', '2:1', '1:2'],$isWebp=true)
     {
         if($isWebp==true){
-            return SpatieMediaLibraryFileUpload::make($name)->collection($collection)->conversion('webp')->label($label)->multiple($is_multible)->image()->imageEditor()->imageEditorAspectRatios($ratio)->openable()->downloadable()->deletable();
+            return SpatieMediaLibraryFileUpload::make($name)->collection($collection)->conversion('webp')->label($label)->multiple($is_multible)->image()->imageEditor()->imageEditorAspectRatios($ratio)
+                ->maxFiles(3)
+                ->openable()->downloadable()->deletable();
 
         }
         return SpatieMediaLibraryFileUpload::make($name)->collection($collection)->label($label)->multiple($is_multible)->image()->imageEditor()->imageEditorAspectRatios($ratio)->openable()->downloadable()->deletable();
