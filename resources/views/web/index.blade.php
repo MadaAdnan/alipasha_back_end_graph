61f06dee5c2683ce4870a4b98ee0a8a1023dc904
sham <albasha.sq@gmail.com>
2025-05-21 13:01:16 +0300

    fix realtime

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -104,8 +104,8 @@
                                         >
                                             <a href="{{route('communities.show',$community->id)}}"
                                                class="rounded-circle">
-                                                <img class="rounded-circle" width="45" src="@if($community->hasMedia('image') && $community->type!=\App\Enums\CommunityTypeEnum::CHAT->value) {{$community->getImage()}} @else {{$community->users()->where('users.id','!=',auth()->id())->first()?->getImage()}}  @endif "
-                                                     style="width: 100px;height: 100px" alt="avatar"/>
+                                                <img class="rounded-circle"  src="@if($community->hasMedia('image') && $community->type!=\App\Enums\CommunityTypeEnum::CHAT->value) {{$community->getImage()}} @else {{$community->users()->where('users.id','!=',auth()->id())->first()?->getImage()}}  @endif "
+                                                     style="width: 45px;height: 100px" alt="avatar"/>
                                             </a>
                                             @php
                                                 if($community->type==\App\Enums\CommunityTypeEnum::CHAT->value){
