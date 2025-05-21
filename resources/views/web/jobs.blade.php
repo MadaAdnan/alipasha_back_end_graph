e5aee8990f7ad7404e2ff79297fedb39b3cd879a
sham <albasha.sq@gmail.com>
2025-05-21 12:17:32 +0300

    fix realtime

--- resources/views/web/jobs.blade.php
+++ resources/views/web/jobs.blade.php
@@ -236,14 +236,24 @@
                         <p class="category-text">التصنيفات</p>
                         <div class="divider"></div>
                         @foreach($categories as $category)
-                            <div class="category-item">
-                                <a href="?category_id={{$category->id}}">
-                                    <p>{{$category->name}}</p>
-                                </a>
-                                    <div class="count">{{$category->products_count}}</div>
+                            <dl>
+                                <dt>
+                                    <div class="category-item">
+                                        <a href="?category_id={{$category->id}}">
+                                            <p>{{$category->name}}</p>
+                                        </a>
+                                        <div class="count">{{$category->products_count}}</div>
 
 
-                            </div>
+                                    </div>
+                                </dt>
+                                @foreach($category->children as $child)
+                                    <a href="?sub_id={{$child->id}}">
+                                        <p>{{$child->name}}</p>
+                                    </a>
+                                @endforeach
+                                <dd></dd>
+                            </dl>
                         @endforeach
 
                     </div>
