f02004f4a77462b05d9ee10f0972506d38998c22
sham <albasha.sq@gmail.com>
2025-05-21 13:09:33 +0300

    fix realtime

--- resources/views/layouts/master_layouts.blade.php
+++ resources/views/layouts/master_layouts.blade.php
@@ -41,18 +41,35 @@
 </head>
 <body>
 {{--TOAST --}}
-<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
-    <div class="toast" style="position: absolute; top: 0; right: 0;">
+<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;" dir="rtl">
+    <div class="toast @if(session()->has('success')) show @endif" style="position: absolute; top: 60px; right: 0;">
         <div class="toast-header">
             <img src="..." class="rounded mr-2" alt="...">
-            <strong class="mr-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
+            <strong class="mr-auto">نجاح</strong>
+
             <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
         <div class="toast-body">
-            Hello, world! This is a toast message.
+           {{session()->get('success')}}
+        </div>
+    </div>
+</div>
+
+{{--Error--}}
+<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;" dir="rtl">
+    <div class="toast @if(session()->has('error')) show @endif" style="position: absolute; top: 60px; right: 0;">
+        <div class="toast-header">
+            <img src="..." class="rounded mr-2" alt="...">
+            <strong class="mr-auto">نجاح</strong>
+
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            {{session()->get('error')}}
         </div>
     </div>
 </div>
