f8e5fe5064fad2f865a2da805d2114b71235d85b
sham <albasha.sq@gmail.com>
2025-05-21 12:32:50 +0300

    fix realtime

--- resources/views/web/tenders.blade.php
+++ resources/views/web/tenders.blade.php
@@ -229,8 +229,8 @@
             <div id="left-sidebar" class="col-3 d-none d-xl-block">
                 <div class="media-scroll bg-light p-4 h-100">
                     <div style="text-align: center">
-                        <button class="new-post"     data-bs-toggle="modal"
-                                data-bs-target="#addPostModal">أضف وظيفة غير متوفرة</button>
+                        <a class="new-post btn btn-danger"   href="{{url('/seller/tenders/create')}}">أضف مناقصة جديدة</a>
+
                     </div>
                     <div class="categories">
                         <p class="category-text">التصنيفات</p>
