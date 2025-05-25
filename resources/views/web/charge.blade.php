a92252d65abc1800a5337f9815192159a4b95f88
sham <albasha.sq@gmail.com>
2025-05-25 10:06:41 +0300

    fix realtime

---
+++ resources/views/web/charge.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master_layouts')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center" style="margin-top: 100px">
+<div class="col-12">
+    <p class="text-bold"><span class="text-red">ملاحظة :</span> قد يستغرق شحن الرصيد في تطبيق علي باشا من 5 إلى 30 دقيقة</p>
+</div>
+
+
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -78,6 +78,7 @@ Route::middleware('throttle:20,1')->group(function () {
         Route::post('/markets/followers', [\App\Http\Controllers\Web\SellerController::class, 'followers']);
         Route::post('/products/like', [\App\Http\Controllers\Web\PostController::class, 'like'])->name('post.like');
         Route::resource('/carts', \App\Http\Controllers\Web\CartController::class)->only(['index', 'show', 'store', 'destroy', 'update']);
+        Route::resource('/charges', \App\Http\Controllers\Web\CartController::class)->only(['index']);
         Route::resource('/galleries', \App\Http\Controllers\Web\GalleryController::class)->only(['show']);
 
     });
