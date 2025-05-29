a7cea3312916b7d848db8d1313487ba31fb3b565
sham <albasha.sq@gmail.com>
2025-05-29 10:51:52 +0300

    fix balance float

--- resources/views/web/jobs.blade.php
+++ resources/views/web/jobs.blade.php
@@ -187,7 +187,7 @@
                             </tbody>
                         </table>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div class="d-flex justify-content-between px-4">
                         <a class="btn btn-sm btn-secondary" href="{{$jobs->nextPageUrl()}}">التالي</a>
                         <a class="btn btn-sm btn-secondary" href="{{$jobs->previousPageUrl()}}">السابق</a>
                     </div>
