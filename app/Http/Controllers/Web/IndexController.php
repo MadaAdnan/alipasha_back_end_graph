f1e7a2425b46b3f0e2d0d88c8f7b3b1f7bfea582
sham <albasha.sq@gmail.com>
2025-05-29 10:33:00 +0300

    fix balance float

--- app/Http/Controllers/Web/IndexController.php
+++ app/Http/Controllers/Web/IndexController.php
@@ -136,6 +136,7 @@ class IndexController extends Controller
                 $query->where('users.id', auth()->id());  // جلب المجتمعات التي يشارك فيها المستخدم الحالي
             })->latest('last_update')->limit(10)->get();
         }
+
         return view('web.index', compact('specialSeller', 'latests','hobbies','specials', 'categories', 'subCategory', 'communities', 'notifications'));
     }
 
--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -771,7 +771,7 @@
                 @endforeach
 
                 <div class="d-flex justify-content-between">
-                    @if($latests->hasMorePage() || $special->hasMorePage() || $hobbbies->hasMorePage())
+                    @if($latests->hasMorePages() || $special->hasMorePages() || $hobbbies->hasMorePages())
                     <a class="btn btn-sm btn-secondary"
                        href="{{$latests->withQueryString()->nextPageUrl()}}">التالي</a>
                     @endif
