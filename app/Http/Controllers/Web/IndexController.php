a8624097f0fa3210c023bca2983f3469f15f4837
sham <albasha.sq@gmail.com>
2025-05-29 10:27:28 +0300

    fix balance float

--- app/Http/Controllers/Web/IndexController.php
+++ app/Http/Controllers/Web/IndexController.php
@@ -97,9 +97,9 @@ class IndexController extends Controller
         $categories = Category::where('is_active', true)
             ->where(fn($query) => $query->where('type', CategoryTypeEnum::PRODUCT->value)->orWhere('type', CategoryTypeEnum::RESTAURANT->value))->orderBy('sortable')->get();
 
-dd(array_merge($latests->pluck('id')->toArray(),$hobbies->pluck('id')->toArray(),$specials->pluck('id')->toArray()));
-        $ids = collect()->pluck('id')->toArray();
-        dd($ids);
+
+        $ids = array_merge($latests->pluck('id')->toArray(),$hobbies->pluck('id')->toArray(),$specials->pluck('id')->toArray());
+
         $today = today();
 
         \DB::transaction(function () use ($ids, $today) {
