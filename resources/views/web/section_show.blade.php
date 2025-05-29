f311d3fe4476c3eb197e6b4318631e27342f078a
sham <albasha.sq@gmail.com>
2025-05-29 10:49:28 +0300

    fix balance float

--- resources/views/web/section_show.blade.php
+++ resources/views/web/section_show.blade.php
@@ -96,9 +96,9 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="d-flex justify-content-between">
-                                @if($products->hasMorePages())
+
                                     <a class="btn btn-sm btn-secondary" href="{{$products->withQueryString()->nextPageUrl()}}">التالي</a>
-                                @endif
+
                                     @if($products->currentPage()>1)
                                 <a class="btn btn-sm btn-secondary" href="{{$products->withQueryString()->previousPageUrl()}}">السابق</a>
                                     @endif
