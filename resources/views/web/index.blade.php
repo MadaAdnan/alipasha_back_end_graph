505ea8170ac2fbfd153c8e475b84605142021510
sham <albasha.sq@gmail.com>
2025-05-31 12:11:08 +0300

    fix balance float

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -773,7 +773,7 @@
                 @endforeach
 
                 <div class="d-flex justify-content-between ps-2">
-                    @if($latests->hasMorePages() || $special->hasMorePages() || $hobbbies->hasMorePages())
+                    @if($latests->hasMorePages() || $specials->hasMorePages() || $hobbbies->hasMorePages())
                     <a class="btn btn-sm btn-secondary"
                        href="{{$latests->withQueryString()->nextPageUrl()}}">التالي</a>
                     @endif
