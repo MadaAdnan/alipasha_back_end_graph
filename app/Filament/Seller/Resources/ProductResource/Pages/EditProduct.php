42371d671514fc8082bbaa4681c3d0fae569cc4a
sham <albasha.sq@gmail.com>
2025-05-31 11:15:33 +0300

    fix balance float

--- app/Filament/Seller/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Seller/Resources/ProductResource/Pages/EditProduct.php
@@ -17,7 +17,8 @@ class EditProduct extends EditRecord
         parent::mount($record); parent::mount($record);
         $product=Product::where('type',CategoryTypeEnum::PRODUCT->value)->orWhere('type',CategoryTypeEnum::RESTAURANT->value)->find($record);
         abort_if($product==null,403,'المنتج غير موجود');
-        abort_if($product->user_id != auth()->id(),403,'غير مصرح لك بالدخول');
+        $id=auth()->id();
+        abort_if($product->user_id != auth()->id(),403,"{$product->user_id}- {$id}");
 
 
     }
