ddbfde1a43e3362daa692f1f24b0fa6867faa5a5
sham <albasha.sq@gmail.com>
2025-06-02 10:12:41 +0300

    fix balance float

--- resources/views/web/section_show.blade.php
+++ resources/views/web/section_show.blade.php
@@ -32,7 +32,7 @@
             <input
                 class="search-nav d-inline-block form-control border-1 border-secondary shadow-none"
                 type="search"
-
+value="{{request()->input('q')}}"
                 placeholder="ابحث في هذا المتجر"
                 aria-label="Search"
                 style="background-color: #fff; box-shadow: none"
