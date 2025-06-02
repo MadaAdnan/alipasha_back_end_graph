ff88abdbe964d9fbdf282b168e208b891d978eca
sham <albasha.sq@gmail.com>
2025-06-02 10:06:39 +0300

    fix balance float

--- resources/views/web/section_show.blade.php
+++ resources/views/web/section_show.blade.php
@@ -27,7 +27,8 @@
     >
 
 
-
+        <button type="submit" class="bg-transparent border-none outline-none"><i class="bi bi-search"
+                                                                                 style="margin-right: 8px; color: #aaa"></i></button>
             <input
                 class="search-nav d-inline-block form-control border-0 shadow-none"
                 type="search"
@@ -37,8 +38,7 @@
                 style="background-color: #fff; box-shadow: none"
                 name="q"
             />
-            <button type="submit" class="bg-transparent border-none outline-none"><i class="bi bi-search"
-                                                                                     style="margin-right: 8px; color: #aaa"></i></button>
+
 
         <select class="search-nav form-control border-0 shadow-none" name="city_id" id="">
             <option value="">اختر مدينة</option>
