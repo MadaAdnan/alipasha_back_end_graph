dec146ad1eff87d5d0ce16cb90999259df056668
sham <albasha.sq@gmail.com>
2025-06-02 10:51:30 +0300

    fix balance float

--- resources/views/web/orders.blade.php
+++ resources/views/web/orders.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center mt-5">
-            <div class="col-md-8 pt-3">
+            <div class="col-md-8 pt-5">
               <div class="card ">
                   <div class="card-header">إنشاء شحنة</div>
                   <div class="card-body">
