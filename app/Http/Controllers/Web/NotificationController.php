e31f1f1048b50a10a004ba1108fd41da0c4d8c40
sham <albasha.sq@gmail.com>
2025-06-02 10:25:15 +0300

    fix balance float

--- app/Http/Controllers/Web/NotificationController.php
+++ app/Http/Controllers/Web/NotificationController.php
@@ -13,6 +13,7 @@ class NotificationController extends Controller
     public function index()
     {
         $notifications=auth()->user()->notifications()->latest()->paginate(10);
+        auth()->user()->notifications->markAsRead();
         return view('web.notifications',compact('notifications'));
     }
 
