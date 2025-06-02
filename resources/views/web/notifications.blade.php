99a11be1067cc199068ef29709db8b4ebf8486b2
sham <albasha.sq@gmail.com>
2025-06-02 10:23:04 +0300

    fix balance float

--- resources/views/web/notifications.blade.php
+++ resources/views/web/notifications.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center mt-5">
-            <div class="col-md-8 " dir="rtl">
+            <div class="col-md-8 mt-5" dir="rtl">
             @forelse($notifications as $notification)
 <div class="card my-2 p-2">
     <div class="card-title">{{$notification->data['title']}}</div>
