d74db53865c28e0b39b12782312890ccad149924
sham <albasha.sq@gmail.com>
2025-06-04 11:54:05 +0300

    fix balance float

--- resources/views/web/orders.blade.php
+++ resources/views/web/orders.blade.php
@@ -253,11 +253,11 @@
 @section('js')
     <script>
         const cities = @json($cities);
-        const pricingWeight = @json($pricing);
-        pricingWeight.sort((a, b) => a.weight - b.weight);
+        const pricing = @json($pricing);
+       // pricingWeight.sort((a, b) => a.weight - b.weight);
         const pricingSize = @json($pricing);
-        pricingSize.sort((a, b) => a.size - b.size);
-        console.log(pricingWeight,pricingSize)
+      //  pricingSize.sort((a, b) => a.size - b.size);
+      //  console.log(pricingWeight,pricingSize)
         var areaSource;
         var areaTarget;
 
