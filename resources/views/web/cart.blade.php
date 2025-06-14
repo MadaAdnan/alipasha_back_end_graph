4962f6d20445c5ad5c9d90531c4a2b880bfc3de4
sham <albasha.sq@gmail.com>
2025-06-14 12:31:38 +0300

    fix balance float

--- resources/views/web/cart.blade.php
+++ resources/views/web/cart.blade.php
@@ -36,7 +36,7 @@
                             <td class="text-center">{{$item->product?->name}}</td>
 
                             <td class="text-center">{{$item->product?->getPrice()}} $</td>
-                            <td class="text-center d-flex">
+                            <td class="text-center d-flex gap-1">
                                 <form action="{{route('carts.store')}}" method="post">
                                     @method('POST')
                                     @csrf
