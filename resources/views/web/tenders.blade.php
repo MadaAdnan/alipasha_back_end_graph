f635f03c173aa0569f73b2dc4c9b6f21307926c7
sham <albasha.sq@gmail.com>
2025-06-15 10:30:33 +0300

    fix balance float

--- resources/views/web/tenders.blade.php
+++ resources/views/web/tenders.blade.php
@@ -191,30 +191,7 @@
                             </div>
                         </div>
                     </div>
-                   {{-- <div class="pagination-wrapper">
-                        <nav aria-label="Page navigation">
-                            <ul class="pagination">
-                                <li class="page-item">
-                                    <a class="page-link" href="#">
-                                        <i class="bi bi-chevron-right"></i>
-                                    </a>
-                                </li>
-                                <li class="page-item active" aria-current="page">
-                                    <a class="page-link" href="#">1</a>
-                                </li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
 
-                                <li class="page-item disabled">
-                                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-                                        <i class="bi bi-chevron-left"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                        <p style="font-size: 14px; font-weight: 500; color: #B5B7C0;"> Showing data 1 to 8 of  256K entries </p>
-
-                    </div>--}}
                 </div>
             </div>
 
