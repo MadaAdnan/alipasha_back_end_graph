6cbe5dfdf6bd4b3ec99832383f22c99fe4815cf4
sham <albasha.sq@gmail.com>
2025-06-23 16:01:56 +0300

    fix balance float

--- routes/web.php
+++ routes/web.php
@@ -111,14 +111,14 @@ Route::middleware('throttle:60,1')->group(function () {
 
 
 Route::get('testnot/{id?}', function ($id = null) {
-    $user=User::find(13);
+  /*  $user=User::find(13);
     $sms = new SmsService();
     $message = "أهلا بك في تطبيق علي باشا \n
             كود التحقق الخاص بك هو \n {$user->code_verified}";
     $phone = $user->phone;
     if (!empty($phone)) {
       return  $sms->sendSms([$user->phone], $message);
-    }
+    }*/
    /* $mail=[
         "mshqwe98@gmail.com",
         "mh.shamey@gmail.com"
