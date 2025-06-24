ad44a5f92c64c1c9768464e8170610e256041dfc
sham <albasha.sq@gmail.com>
2025-06-24 12:38:51 +0300

    fix balance float

--- app/Service/SmsService.php
+++ app/Service/SmsService.php
@@ -17,6 +17,7 @@ class SmsService
         $url = $this->apiUrl;
         $username = $this->username;
         $password = $this->password;
+        info("FONS");
         foreach ($phoneNumbers as $key => $phoneNumber) {
             if (\Str::startsWith($phoneNumber, '+') || \Str::startsWith($phoneNumber, '00')) {
                 continue;
