8c74090f038884a563497f533e11fdbeed5d71bb
sham <albasha.sq@gmail.com>
2025-06-28 10:16:29 +0300

    fix balance float

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -232,6 +232,7 @@ class ProductResource extends Resource
                 Tables\Columns\SpatieMediaLibraryImageColumn::make('image')->collection('images')->conversion('webp')->label('الصورة')->size(100),
                 Tables\Columns\TextColumn::make('id')->label('رقم المنتج')->searchable(),
                 Tables\Columns\TextInputColumn::make('weight')->label('وزن المنتج')->searchable(),
+                Tables\Columns\TextInputColumn::make('power')->label('قوة المنتج')->searchable(),
                 Tables\Columns\ToggleColumn::make('is_delivery')->label('قابل للتوصيل')->searchable(),
                 Tables\Columns\TextColumn::make('category.name')->label('اسم القسم')->description(fn($record) => $record->sub1?->name),
                 Tables\Columns\TextColumn::make('name')->label('اسم المنتج')->description(fn($record) => $record->expert)->searchable(),
