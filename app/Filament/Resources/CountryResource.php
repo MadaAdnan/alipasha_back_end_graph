5a22de6039713f003f05d9791bbbc2bbcefd114d
sham <albasha.sq@gmail.com>
2025-07-14 13:37:47 +0300

    fix balance float

--- app/Filament/Resources/CountryResource.php
+++ app/Filament/Resources/CountryResource.php
@@ -42,7 +42,10 @@ class CountryResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('image')->collection('image')->conversion('webp')->circular()->label('علم الدولة'),
+               Tables\Columns\TextColumn::make('name')->label('الاسم'),
+                Tables\Columns\TextColumn::make('code')->label('الكود'),
+                Tables\Columns\TextColumn::make('is_active')->label('الحالة'),
             ])
             ->filters([
                 //
