9bfd5f55333227dddc3e4a50a478e526aca41e26
sham <albasha.sq@gmail.com>
2025-07-21 17:28:32 +0300

    add code phone to users

--- app/Filament/Resources/ExportResource.php
+++ app/Filament/Resources/ExportResource.php
@@ -39,6 +39,7 @@ public static function canEdit(Model $record): bool
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('completed_at','desc')
             ->columns([
                 Tables\Columns\TextColumn::make('file_name')->label('الملف'),
                 Tables\Columns\TextColumn::make('created_at')->date('Y-m-d H:i')->label('الملف'),
