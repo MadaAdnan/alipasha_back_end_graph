34dd5590d599dffb952f8c737a5aac5424f42254
sham <albasha.sq@gmail.com>
2025-08-02 16:21:43 +0300

    add command an link any event with webhok

--- app/Filament/Resources/InvoiceResource.php
+++ app/Filament/Resources/InvoiceResource.php
@@ -97,6 +97,7 @@ protected static ?string $navigationLabel='طلبات الشراء';
             ->actions([
 
                 //Tables\Actions\Action::make('show_invoice')->url(InvoiceResource::getUrl('list')),
+                Tables\Actions\ViewAction::make(),
                // Tables\Actions\EditAction::make(),
               /*  Tables\Actions\ActionGroup::make([
                     Tables\Actions\Action::make('take')->requiresConfirmation()->action(fn($record) => $record->update(['status' => OrderStatusEnum::AWAY->value]))->label('تأكيد إستلام البضاعة من التاجر')->visible(fn($record) => $record->status == OrderStatusEnum::AGREE->value),
