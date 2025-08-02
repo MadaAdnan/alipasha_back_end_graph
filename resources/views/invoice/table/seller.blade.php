e8719385300d1a2fa34fd268ada4c598db768152
sham <albasha.sq@gmail.com>
2025-08-02 12:25:35 +0300

    add some settings

--- resources/views/invoice/table/seller.blade.php
+++ resources/views/invoice/table/seller.blade.php
@@ -32,10 +32,10 @@
             </td>
         </tr>
         <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2">
-            <td><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="{{\App\Filament\Resources\UserResource::getUrl('edit',['record'=>$getRecord()->user->id])}}">{{$getRecord()->user?->name ?? $getRecord()->user?->seller_name}}</a></td>
-            <td><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="https://wa.me/{{$getRecord()->phone}}">{{$getRecord()->phone}}</a></td>
-            <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$getRecord()->address}} </td>
-            <td class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$getRecord()->user->city?->name}}</td>
+            <td class="text-center"><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="{{\App\Filament\Resources\UserResource::getUrl('edit',['record'=>$getRecord()->user->id])}}">{{$getRecord()->user?->name ?? $getRecord()->user?->seller_name}}</a></td>
+            <td class="text-center"><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="https://wa.me/{{$getRecord()->phone}}">{{$getRecord()->phone}}</a></td>
+            <td class="text-center fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$getRecord()->address}} </td>
+            <td class="text-center fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$getRecord()->user->city?->name}}</td>
         </tr>
     </table>
 
