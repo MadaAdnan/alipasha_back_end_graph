1042ef9060681891fadc2b6d4e214d13fec85b26
sham <albasha.sq@gmail.com>
2025-08-02 15:08:43 +0300

    add command an link any event with webhok

--- resources/views/invoice/table/seller.blade.php
+++ resources/views/invoice/table/seller.blade.php
@@ -32,7 +32,7 @@
             </td>
         </tr>
         <tr class="divide-y divide-gray-200 dark:divide-white/5 border border-2">
-            <td class="text-center"><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="{{\App\Filament\Resources\UserResource::getUrl('edit',['record'=>$getRecord()->user->id])}}">{{$getRecord()->user?->name ?? $getRecord()->user?->seller_name}}</a></td>
+            <td class="text-center"><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="{{\App\Filament\Resources\UserResource::getUrl('edit',['record'=>$getRecord()->user?->id])}}">{{$getRecord()->user?->name ?? $getRecord()->user?->seller_name}}</a></td>
             <td class="text-center"><a class="fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white" href="https://wa.me/{{$getRecord()->phone}}">{{$getRecord()->phone}}</a></td>
             <td class="text-center fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$getRecord()->address}} </td>
             <td class="text-center fi-ta-header-cell-label text-sm font-semibold text-gray-950 dark:text-white">{{$getRecord()->user->city?->name}}</td>
