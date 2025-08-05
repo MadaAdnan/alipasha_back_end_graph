8ac0af98ef86b689a1c33f864c88bdb0bb231955
sham <albasha.sq@gmail.com>
2025-08-05 12:25:40 +0300

    add command an link any event with webhok

--- routes/web.php
+++ routes/web.php
@@ -58,61 +58,65 @@ Route::get('oauth/callback/google', function () {
         return 'OAuth Error: ' . $e->getMessage();
     }
 });
-Route::post('login', [\App\Http\Controllers\Web\AuthController::class, 'login'])->name('login')->middleware('throttle.login:5,1');
-Route::post('register', [\App\Http\Controllers\Web\AuthController::class, 'register'])->name('register')->middleware('throttle.login:5,1');
-
-Route::middleware('throttle:60,1')->group(function () {
-    Route::get('download-app',function(){
-        return response()->file(Setting::first()?->getFirstMediaPath('apk'),[
-            'Content-Type' => 'application/vnd.android.package-archive',
-            'Content-Disposition' => 'attachment; filename="ali-pasha.apk"',
-        ]);
-    });
-    Route::get('login', [\App\Http\Controllers\Web\AuthController::class, 'loginUi'])->name('login.ui');
-    Route::get('forget-password', [\App\Http\Controllers\Web\AuthController::class, 'forgetPasswordUi'])->name('forget-password.ui');
-    Route::get('change-password', [\App\Http\Controllers\Web\AuthController::class, 'changePasswordUi'])->name('change-password.ui');
-    Route::get('register', [\App\Http\Controllers\Web\AuthController::class, 'registerUi'])->name('register.ui');
-
-    Route::post('logout', [\App\Http\Controllers\Web\AuthController::class, 'logout'])->name('logout')->middleware('auth:web');
-    Route::post('forget-password', [\App\Http\Controllers\Web\AuthController::class, 'forgetPassword'])->name('forget-password');
-    Route::post('change-password', [\App\Http\Controllers\Web\AuthController::class, 'changePassword'])->name('change-password');
-    Route::resource('/', \App\Http\Controllers\Web\IndexController::class)->only('index');
-    Route::resource('/search', \App\Http\Controllers\Web\SearchController::class)->only('index');
-    Route::resource('/jobs', \App\Http\Controllers\Web\JobController::class)->only('index', 'show');
-    Route::resource('/tenders', \App\Http\Controllers\Web\TenderController::class)->only('index', 'show');
-    Route::resource('/services', \App\Http\Controllers\Web\ServiceController::class)->only('index', 'show');
-    Route::resource('/posts', \App\Http\Controllers\Web\PostController::class)->only('index', 'show');
-    Route::resource('/pricing', \App\Http\Controllers\Web\PricingController::class)->only('index', 'store');
-    Route::get('/profile/{id?}', [\App\Http\Controllers\Web\SellerController::class, 'profile'])->name('seller.profile');
-    Route::get('/category/{id}', [\App\Http\Controllers\Web\CategoryController::class, 'show'])->name('category.show');
 
 
-    Route::middleware('auth:web')->group(function () {
-        Route::resource('/my-profile', \App\Http\Controllers\Web\ProfileController::class)
-            ->only('index', 'store')
-            ->names([
-                'index' => 'profile.index',
-                'store' => 'profile.store',
+
+Route::middleware([\App\Http\Middleware\XFrameOptionMiddleware::class])->group(function (){
+    Route::post('login', [\App\Http\Controllers\Web\AuthController::class, 'login'])->name('login')->middleware('throttle.login:5,1');
+    Route::post('register', [\App\Http\Controllers\Web\AuthController::class, 'register'])->name('register')->middleware('throttle.login:5,1');
+
+    Route::middleware('throttle:60,1')->group(function () {
+        Route::get('download-app',function(){
+            return response()->file(Setting::first()?->getFirstMediaPath('apk'),[
+                'Content-Type' => 'application/vnd.android.package-archive',
+                'Content-Disposition' => 'attachment; filename="ali-pasha.apk"',
             ]);
-        Route::resource('/comments', \App\Http\Controllers\Web\CommentController::class)->only(['store']);
-        Route::resource('/communities', \App\Http\Controllers\Web\CommunityController::class)->only(['index', 'show', 'store']);
-        Route::resource('/messages', \App\Http\Controllers\Web\MessageController::class)->only(['store']);
-        Route::resource('/balances', \App\Http\Controllers\Web\BalanceController::class)->only(['index']);
-        Route::resource('/invoices', \App\Http\Controllers\Web\InvoiceController::class)->only(['index', 'update']);
-        Route::resource('/my-invoices', \App\Http\Controllers\Web\MyInvoiceController::class)->only(['index','store']);
-        Route::resource('/orders', \App\Http\Controllers\Web\OrderController::class)->only(['index','store']);
-        Route::post('/markets/followers', [\App\Http\Controllers\Web\SellerController::class, 'followers']);
-        Route::post('/products/like', [\App\Http\Controllers\Web\PostController::class, 'like'])->name('post.like');
-        Route::resource('/carts', \App\Http\Controllers\Web\CartController::class)->only(['index', 'show', 'store', 'destroy', 'update'])/*->middleware(\App\Http\Middleware\RateLimitPerSecond::class)*/;
-        Route::resource('/charges', \App\Http\Controllers\Web\ChargeController::class)->only(['index']);
-        Route::resource('/charges', \App\Http\Controllers\Web\ChargeController::class)->only(['index']);
-        Route::resource('/galleries', \App\Http\Controllers\Web\GalleryController::class)->only(['show']);
-        Route::resource('/notifications', \App\Http\Controllers\Web\NotificationController::class)->only(['index']);
+        });
+        Route::get('login', [\App\Http\Controllers\Web\AuthController::class, 'loginUi'])->name('login.ui');
+        Route::get('forget-password', [\App\Http\Controllers\Web\AuthController::class, 'forgetPasswordUi'])->name('forget-password.ui');
+        Route::get('change-password', [\App\Http\Controllers\Web\AuthController::class, 'changePasswordUi'])->name('change-password.ui');
+        Route::get('register', [\App\Http\Controllers\Web\AuthController::class, 'registerUi'])->name('register.ui');
 
-    });
+        Route::post('logout', [\App\Http\Controllers\Web\AuthController::class, 'logout'])->name('logout')->middleware('auth:web');
+        Route::post('forget-password', [\App\Http\Controllers\Web\AuthController::class, 'forgetPassword'])->name('forget-password');
+        Route::post('change-password', [\App\Http\Controllers\Web\AuthController::class, 'changePassword'])->name('change-password');
+        Route::resource('/', \App\Http\Controllers\Web\IndexController::class)->only('index');
+        Route::resource('/search', \App\Http\Controllers\Web\SearchController::class)->only('index');
+        Route::resource('/jobs', \App\Http\Controllers\Web\JobController::class)->only('index', 'show');
+        Route::resource('/tenders', \App\Http\Controllers\Web\TenderController::class)->only('index', 'show');
+        Route::resource('/services', \App\Http\Controllers\Web\ServiceController::class)->only('index', 'show');
+        Route::resource('/posts', \App\Http\Controllers\Web\PostController::class)->only('index', 'show');
+        Route::resource('/pricing', \App\Http\Controllers\Web\PricingController::class)->only('index', 'store');
+        Route::get('/profile/{id?}', [\App\Http\Controllers\Web\SellerController::class, 'profile'])->name('seller.profile');
+        Route::get('/category/{id}', [\App\Http\Controllers\Web\CategoryController::class, 'show'])->name('category.show');
 
-    Route::get('/.well-known/assetlinks.json', function () {
-        return json_decode('[{
+
+        Route::middleware('auth:web')->group(function () {
+            Route::resource('/my-profile', \App\Http\Controllers\Web\ProfileController::class)
+                ->only('index', 'store')
+                ->names([
+                    'index' => 'profile.index',
+                    'store' => 'profile.store',
+                ]);
+            Route::resource('/comments', \App\Http\Controllers\Web\CommentController::class)->only(['store']);
+            Route::resource('/communities', \App\Http\Controllers\Web\CommunityController::class)->only(['index', 'show', 'store']);
+            Route::resource('/messages', \App\Http\Controllers\Web\MessageController::class)->only(['store']);
+            Route::resource('/balances', \App\Http\Controllers\Web\BalanceController::class)->only(['index']);
+            Route::resource('/invoices', \App\Http\Controllers\Web\InvoiceController::class)->only(['index', 'update']);
+            Route::resource('/my-invoices', \App\Http\Controllers\Web\MyInvoiceController::class)->only(['index','store']);
+            Route::resource('/orders', \App\Http\Controllers\Web\OrderController::class)->only(['index','store']);
+            Route::post('/markets/followers', [\App\Http\Controllers\Web\SellerController::class, 'followers']);
+            Route::post('/products/like', [\App\Http\Controllers\Web\PostController::class, 'like'])->name('post.like');
+            Route::resource('/carts', \App\Http\Controllers\Web\CartController::class)->only(['index', 'show', 'store', 'destroy', 'update'])/*->middleware(\App\Http\Middleware\RateLimitPerSecond::class)*/;
+            Route::resource('/charges', \App\Http\Controllers\Web\ChargeController::class)->only(['index']);
+            Route::resource('/charges', \App\Http\Controllers\Web\ChargeController::class)->only(['index']);
+            Route::resource('/galleries', \App\Http\Controllers\Web\GalleryController::class)->only(['show']);
+            Route::resource('/notifications', \App\Http\Controllers\Web\NotificationController::class)->only(['index']);
+
+        });
+
+        Route::get('/.well-known/assetlinks.json', function () {
+            return json_decode('[{
   "relation": ["delegate_permission/common.handle_all_urls"],
   "target": {
     "namespace": "android_app",
@@ -122,50 +126,44 @@ Route::middleware('throttle:60,1')->group(function () {
     "77:6B:B2:2A:A3:E2:2B:1D:2A:6C:90:C8:63:59:E6:C6:A2:A5:63:C1:30:8C:9D:0A:E4:C8:6E:E3:79:DC:B8:56"]
   }
 }]');
+        });
     });
-});
 
 
-####################################################################
-###################### IMPORT DATA #################################
-//Route::post('/import', [ImportController::class, 'import'])->name('import');
-/*Route::get('/import', function () {
-  return  \App\Helpers\StrHelper::generateMd5();
-    return view('import');
-});*/
 
-Route::get('/download-file/{record}', function (\App\Models\Export $record) {
-    $path = "filament_exports/$record->id/$record->file_name.xlsx";
+    Route::get('/download-file/{record}', function (\App\Models\Export $record) {
+        $path = "filament_exports/$record->id/$record->file_name.xlsx";
 
-    if (!Storage::disk('local')->exists($path)) {
-       return "{$path}";
-    }
+        if (!Storage::disk('local')->exists($path)) {
+            return "{$path}";
+        }
 
-    return Storage::disk('local')->download($path);
-})->name('download.file');
+        return Storage::disk('local')->download($path);
+    })->name('download.file');
 
-Route::get('testnot/{id?}', function ($id = null) {
+    Route::get('testnot/{id?}', function ($id = null) {
 
-  /*  DB::update("
-    UPDATE users
-    JOIN (
-        SELECT user_id, MIN(category_id) as category_id
-        FROM products
-        GROUP BY user_id
-    ) as p ON users.id = p.user_id
-    SET users.category_id = p.category_id
-    WHERE users.category_id IS NULL
-");*/
+        /*  DB::update("
+          UPDATE users
+          JOIN (
+              SELECT user_id, MIN(category_id) as category_id
+              FROM products
+              GROUP BY user_id
+          ) as p ON users.id = p.user_id
+          SET users.category_id = p.category_id
+          WHERE users.category_id IS NULL
+      ");*/
 
 
-    return 'success';
-});
-Route::get('/server-resources', function () {
-    return [
-        'memory_limit' => ini_get('memory_limit'),
-        'max_execution_time' => ini_get('max_execution_time'),
-        'disk_free_space' => disk_free_space('/'),
-        'disk_total_space' => disk_total_space('/'),
-        'cpu_load' => sys_getloadavg()
-    ];
+        return 'success';
+    });
+    Route::get('/server-resources', function () {
+        return [
+            'memory_limit' => ini_get('memory_limit'),
+            'max_execution_time' => ini_get('max_execution_time'),
+            'disk_free_space' => disk_free_space('/'),
+            'disk_total_space' => disk_total_space('/'),
+            'cpu_load' => sys_getloadavg()
+        ];
+    });
 });
