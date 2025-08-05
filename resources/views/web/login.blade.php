a527e50e336d58779bdd03c8f48f830117a20192
sham <albasha.sq@gmail.com>
2025-08-05 13:57:05 +0300

    add command an link any event with webhok

--- resources/views/web/login.blade.php
+++ resources/views/web/login.blade.php
@@ -89,11 +89,11 @@
         >
             نسيت كلمة المرور
         </a>
-        @if($errors->any())
+   {{--     @if($errors->any())
             @foreach($error as $errors->all())
                 <span>{{$error}}</span>
             @endforeach
-        @endif
+        @endif--}}
         <!-- <div class="mb-3 form-check">
           <input type="checkbox" class="form-check-input" id="check" />
           <label class="form-check-label" for="check">تذكرني</label>
