e7acecad0d561069f6f001cd6aeacc57fabea3be
sham <albasha.sq@gmail.com>
2025-08-07 05:02:57 +0300

    add command an link any event with webhok

--- app/Jobs/WebhokProductsJob.php
+++ app/Jobs/WebhokProductsJob.php
@@ -30,7 +30,7 @@ class WebhokProductsJob implements ShouldQueue
     {
         try {
             \Log::info("Start Sync");
-            $response = \Http::post($this->url_webhok, [
+            $response = \Http::asForm()->post($this->url_webhok,[
                 'action' =>'create',
                 'type' => 'products',
                 'data' => ProductResource::collection($this->products)->jsonSerialize(),
