c9751db6af5c157b50234477ea92d1fdc60ac8fe
sham <albasha.sq@gmail.com>
2025-08-11 11:08:28 +0300

    add command an link any event with webhok

--- app/Helpers/ProductsHelper.php
+++ app/Helpers/ProductsHelper.php
@@ -15,21 +15,21 @@ class ProductsHelper
     /**
      * @var $user User
      */
-    public static function getPresentPlanActive(?User $user=null): ?Plan
+    public static function getPresentPlanActive(?User $user = null): ?Plan
     {
 
-        if($user==null){
+        if ($user == null) {
             $user = auth()->user();
         }
 
         $plans = $user->plans()->where('type', PlansTypeEnum::PRESENT->value)->get();
-        $planfree=null;
-        $plan=null;
-        foreach ( $plans as $item) {
-            if($item->dueration==PlansDurationEnum::FREE->value){
-                $palnfree=$item;
-            }else{
-                $plan=$item;
+        $planfree = null;
+        $plan = null;
+        foreach ($plans as $item) {
+            if ($item->dueration == PlansDurationEnum::FREE->value) {
+                $planfree = $item;
+            } else {
+                $plan = $item;
                 break;
             }
 
@@ -47,18 +47,18 @@ class ProductsHelper
         $plan = $user->plans()
             ->where('type', PlansTypeEnum::PRESENT->value)
             ->whereNot('duration', PlansDurationEnum::FREE->value)
-            ->where('special_count','>',0)
+            ->where('special_count', '>', 0)
             ->first();
         //info("ADNAN {$plan->name} - {$user->special_product_count} - {$plan->special_count}");
-        return $plan!=null &&  $user->special_product_count < $plan->special_count;
+        return $plan != null && $user->special_product_count < $plan->special_count;
     }
 
-    public static function isAvailableCreateProduct(Plan $plan,?User $user=null)
+    public static function isAvailableCreateProduct(Plan $plan, ?User $user = null)
     {
-        if($user==null){
+        if ($user == null) {
             $user = auth()->user();
         }
-        if($user?->is_verified==true){
+        if ($user?->is_verified == true) {
             return true;
         }
 
