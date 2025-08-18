f7ce367dc6c3a559ec5d277f6a96133b7edcb2e6
sham <albasha.sq@gmail.com>
2025-08-18 08:17:50 +0300

    add api for erp

---
+++ app/GraphQL/Queries/HomeQuery/OldLatest.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries\HomeQuery;
+
+use App\Enums\CategoryTypeEnum;
+use App\Enums\LevelProductEnum;
+use App\Enums\ProductActiveEnum;
+use App\Models\Interaction;
+use App\Models\Product;
+use App\Models\Setting;
+
+final class OldLatest
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+
+        //return Product::where('id',0);
+        $setting=Setting::first();
+        $products= Product::active()->where('power','>',20)
+            ->where(fn( $query)=>$query->whereDoesntHave('category',fn($query)=>$query->where('type',CategoryTypeEnum::RESTAURANT->value)))
+            ->whereNot('level',LevelProductEnum::SPECIAL->value)
+            ->whereIn('type',[
+                CategoryTypeEnum::PRODUCT->value,
+                CategoryTypeEnum::TENDER->value,
+                CategoryTypeEnum::JOB->value,
+                CategoryTypeEnum::SEARCH_JOB->value,
+                CategoryTypeEnum::NEWS->value,
+            ]) ->where(function ($query) {
+                $query->whereNull('end_date')
+                    ->orWhere('end_date', '>', now());
+            })  ->where('created_at','>=',now()->subDays($setting->options['recommended_month']??30))->inRandomOrder()
+            ->when(auth()->check(),fn($query)=>$query->where(fn($q)=>
+            $q->whereNotIn('category_id',$this->getPopularCategoryProducts())
+                ->whereNotIn('user_id',$this->getPopularSelelrProducts())
+            ))
+            ;
+      //  $ids = $products->pluck('id')->toArray();
+        $ids=[];
+        $today = today();
+
+        \DB::transaction(function () use ($ids, $today) {
+
+            // تحديث السجلات الموجودة
+            \DB::table('product_views')
+                ->whereIn('product_id', $ids)
+                ->whereDate('view_at', $today)
+                ->update(['count' => \DB::raw('count + 1')]);
+            $existingIds = \DB::table('product_views')
+                ->whereIn('product_id', $ids)
+                ->whereDate('view_at', $today)
+                ->pluck('product_id')
+                ->toArray();
+            $newIds = array_diff($ids, $existingIds);
+            if (
+                !empty($newIds)) {
+                $inserts = array_map(function ($id) use ($today) {
+                    return [
+                        'product_id' => $id,
+                        'view_at' => $today,
+                        'count' => 1,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ];
+                }, $newIds);
+
+                \DB::table('product_views')->insert($inserts);
+            }
+        });
+        return $products;
+    }
+    private function getPopularCategoryProducts()
+    {
+        return Interaction::where('user_id', auth()->id())->whereNotNull('category_id')
+            ->latest()
+            ->groupBy('category_id')
+            ->orderByRaw('SUM(visited) DESC')
+            ->pluck('category_id')->toArray();
+    }
+    private function getPopularSelelrProducts()
+    {
+        return Interaction::where('user_id', auth()->id())->whereNotNull('seller_id')
+            ->groupBy('seller_id')
+
+            ->pluck('seller_id')->toArray();
+    }
+}
