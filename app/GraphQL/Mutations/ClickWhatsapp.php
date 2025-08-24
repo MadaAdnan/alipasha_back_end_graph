8521f339d6c5ec21cb6cdc506ad60e65106a7140
sham <albasha.sq@gmail.com>
2025-08-24 13:18:16 +0300

    add click whats app

---
+++ app/GraphQL/Mutations/ClickWhatsapp.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Jobs\SendFirebaseNotificationJob;
+use App\Models\Interaction;
+use App\Models\Like;
+use App\Models\Product;
+use Exception;
+
+final  class ClickWhatsapp
+{
+    /** @param array{} $args */
+    public function __invoke($_, array $args)
+    {
+        $productId = $args['product_id'];
+        $product = Product::find($productId);
+        $user = auth()->user()->name;
+        $name = $product->name ?? \Str::substr($product->expert, 0, 20);
+        $data['title'] = 'مراسلة جديدة';
+        $data['body'] = "قد يتواصل الزبون {$user} عبر واتسأب للإستفسار عن المنتج {$name}";
+        try {
+            $job = new SendFirebaseNotificationJob([$product->user->device_token], $data);
+            dispatch($job);
+        } catch (Exception|\Error $e) {
+
+        }
+
+        return $product->refresh();
+    }
+}
