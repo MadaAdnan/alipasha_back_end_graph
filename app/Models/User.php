1edda6baa3ca1631ca6df48ba82fc1f510ee7e12
sham <albasha.sq@gmail.com>
2025-08-25 12:05:03 +0300

    add click whats app

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,7 +65,8 @@ class User extends Authenticatable implements HasMedia, MustVerifyEmail
         'social' => 'array',
         'notify_date' => 'date',
         'send_at' => 'datetime',
-        'is_default_active' => 'boolean'
+        'is_default_active' => 'boolean',
+        'is_sync_webhok' => 'boolean'
 
     ];
 
