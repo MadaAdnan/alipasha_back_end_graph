2d61cfe0f7257f66b06ff87e97930d036b45e73d
sham <albasha.sq@gmail.com>
2025-08-25 12:09:14 +0300

    add click whats app

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -141,8 +141,8 @@ class UserResource extends Resource
                                 Forms\Components\ColorPicker::make('id_color')->label('هوية المتجر')->default("#FF0000"),
                                 Forms\Components\Group::make()->schema([
                                     Forms\Components\TextInput::make('url_webhok')->url()->label('رابط ويب هوك الخاص بالمتجر')
-                                    ->suffixAction(Forms\Components\Actions\Action::make('sync')->action(function ($state) {
-                                    dd($state);
+                                    ->suffixAction(Forms\Components\Actions\Action::make('sync')->action(function ($state,$record) {
+                                    dd($record);
                                     }
                                     ))
                                     ,
