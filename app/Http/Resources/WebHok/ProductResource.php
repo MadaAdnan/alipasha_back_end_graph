aefe506802b7f0336209d5aa5de166d3784abeac
sham <albasha.sq@gmail.com>
2025-09-11 12:28:13 +0300

    add click whats app

--- app/Http/Resources/WebHok/ProductResource.php
+++ app/Http/Resources/WebHok/ProductResource.php
@@ -30,7 +30,7 @@ class ProductResource extends JsonResource
             'address' => $this->address,
             'city' => $this->user?->city?->name,
             'area' => $this->user?->area?->name,
-            'is_available' =>(boolean) $this->is_available,
+            'is_available' =>(bool) $this->is_available,
             'video'=>$this->video,
             'category_name'=>$this->category?->name
         ];
