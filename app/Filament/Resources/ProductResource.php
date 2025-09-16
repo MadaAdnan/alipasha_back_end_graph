245ae800c5fe93a0a1d8c6302499d9bd5467b1af
sham <albasha.sq@gmail.com>
2025-09-17 00:04:21 +0300

    add click whats app

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -140,7 +140,7 @@ class ProductResource extends Resource
                     Forms\Components\Textarea::make('info')->label('وصف المنتج'),
                     Forms\Components\TextInput::make('phone')->label('رقم الهاتف')->required()->rules(['numeric']),
                    /* Forms\Components\Fieldset::make('هاتف المتجر')->schema([
-                        
+
                         Forms\Components\Grid::make()->schema([
                             Forms\Components\Select::make('city_id')->options(City::where('is_main',1)->pluck('name','id'))->searchable()->label('المحافظة')->required()->live(),
                             Forms\Components\Select::make('area_id')->options(fn($get)=>City::where('is_main',0)->where('city_id',$get('city_id'))->pluck('name','id'))->searchable()->label('المدينة')->required()
