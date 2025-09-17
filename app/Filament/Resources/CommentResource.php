6ebd39485ded54f159a699c690158c02192a8d91
sham <albasha.sq@gmail.com>
2025-09-17 20:38:01 +0300

    add click whats app

--- app/Filament/Resources/CommentResource.php
+++ app/Filament/Resources/CommentResource.php
@@ -29,7 +29,7 @@ class CommentResource extends Resource
         return $form
             ->schema([
                Forms\Components\Section::make('التعليقات')->schema([
-                   Forms\Components\Select::make('product_id')->options(fn($record)=>$record ?Product::selectRaw('id, CONCAT(COALESCE(name, "بدون اسم"), "#", id) as fullname')->where('id',$record->id)
+                   Forms\Components\Select::make('product_id')->options(fn($record)=>$record ?Product::selectRaw('id, CONCAT(COALESCE(name, "بدون اسم"), "#", id) as fullname')->where('id',$record->product_id)
                        ->pluck('fullname', 'id') :Product::selectRaw('id, CONCAT(COALESCE(name, "بدون اسم"), "#", id) as fullname')
                        ->pluck('fullname', 'id'))->label('المنتج')->required(),
                    Forms\Components\Textarea::make('comment')->label('التعليق')->required(),
