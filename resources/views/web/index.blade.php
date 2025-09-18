6e134266555038103ff06e7594f672226a69e3d2
sham <albasha.sq@gmail.com>
2025-09-18 11:18:57 +0300

    add click whats app

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -145,7 +145,7 @@
 
                         </div>
                         @else
-                            <div class="chat-wrapper text-center">
+                            <div class="chat-wrapper text-center py-3">
                                 <img src="{{asset('images/no-chat.png')}}" alt="No Chat Found">
                             </div>
                         @endif
