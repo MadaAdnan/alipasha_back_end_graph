b682b71f53f4d5259f762b6225188ced10e4d57f
sham <albasha.sq@gmail.com>
2025-09-20 11:16:38 +0300

    add click whats app

--- routes/web.php
+++ routes/web.php
@@ -143,16 +143,16 @@ Route::middleware([\App\Http\Middleware\XFrameOptionMiddleware::class])->group(f
     })->name('download.file');
 
     Route::get('testnot/{id?}', function ($id = null) {
-
-        $products = \App\Models\Product::where([
+        dd( \DB::table('users')->where('email', 'redadnan@gmail.com')->exists());
+      /*  $products = \App\Models\Product::where([
             'user_id' => 54184,
-           /* 'is_sync_webhok' => false,*/
+           'is_sync_webhok' => false,
         ])->get();
         $user = User::find(54184);
         $job = new \App\Jobs\WebhokProductsJob($products, $user->url_webhok);
-        dispatch($job);
+        dispatch($job);*/
 
-        return "Success {$products->count()}";
+        return "Success ";
     });
     Route::get('/server-resources', function () {
         /*  $user=User::find(51491);
