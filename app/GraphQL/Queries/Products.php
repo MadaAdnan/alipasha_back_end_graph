1beea5820bbc639b33656107d66a87e5517f4d14
sham <albasha.sq@gmail.com>
2025-09-24 09:43:53 +0300

    add register win

--- app/GraphQL/Queries/Products.php
+++ app/GraphQL/Queries/Products.php
@@ -86,7 +86,7 @@ final class Products
             $products->orderBy($orderBy['column'], $orderBy['orderBy']);
         }*/
         $products = Product::active()
-            ->whereBetween('price', [$minPrice, $maxPrice])
+            ->whereBetween(\DB::raw('CAST(price AS DECIMAL(10,2))'), [$minPrice, $maxPrice])
 
             // 🔹 فلترة حسب المدينة
             ->when($cityId, function ($query) use ($cityId) {
