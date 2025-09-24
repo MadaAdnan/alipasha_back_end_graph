65e0297836e4516e21f5cd7e64c3a5bf79b0b303
sham <albasha.sq@gmail.com>
2025-09-24 09:35:40 +0300

    add register win

--- app/GraphQL/Queries/Products.php
+++ app/GraphQL/Queries/Products.php
@@ -62,6 +62,9 @@ final class Products
 
                 }*/ else {
                     $query->where('type', $type);
+                    if($type === 'product' &&( isset($args['min_price']) || isset($args['min_price']))){
+                        $query->whereBetween('price', [$args['min_price']??0,  $args['max_price']??100000]);
+                    }
                 }
             })
             ->where(function ($query) use ($userId) {
@@ -71,21 +74,13 @@ final class Products
             })
 
             //->when($userId!='', fn($query) => $query->where('user_id', $userId))
-            ->when($type === 'product' && isset($args['max_price']) && $args['max_price'] > 0, fn($query) => $query->where('price', '>=', [$args['min_price'] ?? 0])->where('price', "<=", $args['max_price'] ?? 10000))
             ->when(collect($colors ?? [])->count() > 0, fn($query) => $query->whereHas('colors', fn($q) => $q->whereIn('colors.id', $colors)))
 
             ->when($categoryId != null, fn($query) => $query->where('category_id', $categoryId))
             ->when($sub1Id != null, fn($query) => $query->where('sub1_id', $sub1Id))
 
-            ->when(isset($args['search']) && !empty($args['search']) && $type !== 'seller', fn($query) => $query->where(function ($query) use ($args) {
-
-                /**
-                 * @var $searchTerms array<string>
-                 */
-                $searchTerms = explode(' ', $args['search']); // تحويل البحث إلى مصفوفة كلمات
-                /**
-                 * @var $term string
-                 */
+            ->when(isset($args['search']) && !empty($args['search']) && $type !== 'seller',
+                fn($query) => $query->where(function ($query) use ($args) {
                 $query->where('name', 'LIKE', "%" . $args['search'] . "%")
                     ->orWhere('expert', 'LIKE', "%" . $args['search'] . "%")
                     ->orWhere('info', 'LIKE', "%" . $args['search'] . "%");
