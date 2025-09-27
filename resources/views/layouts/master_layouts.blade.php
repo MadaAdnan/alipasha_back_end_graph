f81fa33fb14a9118fa64b2cea568bef6c2f253b4
sham <albasha.sq@gmail.com>
2025-09-27 12:52:20 +0300

    add register win

--- resources/views/layouts/master_layouts.blade.php
+++ resources/views/layouts/master_layouts.blade.php
@@ -358,7 +358,7 @@
                     <span class="position-absolute top-0 badge badge-danger cart-badge">{{$cartsCount}}</span>
                     <img src="{{asset('assets/market.svg')}}" alt=""/>
                     @auth
-                    <span>{{auth()->user()->getTotalBalance()}} $</span>
+                    <span class="badge badge-secondary">{{auth()->user()->getTotalBalance()}} $</span>
                     @endauth
                 </a>
             @endauth
