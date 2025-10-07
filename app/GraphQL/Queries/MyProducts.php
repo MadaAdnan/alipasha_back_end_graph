c0bb84d9402e9e122b5647b01f6c4b9e6101c159
sham <albasha.sq@gmail.com>
2025-10-07 11:11:09 +0300

    add register win

--- app/GraphQL/Queries/MyProducts.php
+++ app/GraphQL/Queries/MyProducts.php
@@ -12,6 +12,7 @@ final class MyProducts
     {
         return \App\Models\Product::where('user_id', auth()->id())
            ->when(isset($args['search']) && !empty($args['search']),fn($query)=>$query ->where(fn($query) => $query->where('name', 'LIKE', "%{$args['search']}%")->orWhere('expert', 'LIKE', "%{$args['search']}%")))
+           ->when(isset($args['categoryId']),fn($query)=>$query ->where('category_id', $args['categoryId']))
             ->orderBy('created_at','desc');
     }
 }
