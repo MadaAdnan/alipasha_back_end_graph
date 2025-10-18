29a965117e4dd96102166ed466470e9b1397d87e
sham <albasha.sq@gmail.com>
2025-10-18 06:47:17 +0300

    add register win

--- app/Console/Commands/syncProductWebHokCommand.php
+++ app/Console/Commands/syncProductWebHokCommand.php
@@ -37,7 +37,7 @@ class syncProductWebHokCommand extends Command
                     ->chunk(30, function ($products) use ($user) {
                         try {
                             dispatch(new WebhokProductsJob($products, $user->url_webhok));
-                            \Log::info('Job dispatched for user: ' . $user->id . ' count: ' . $products->count());
+
                         } catch (\Throwable $e) {
                             \Log::error("Error dispatching job: " . $e->getMessage());
                         }
