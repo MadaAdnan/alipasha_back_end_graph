6f0e9c818058700618edf80f2a21ad714807bed9
sham <albasha.sq@gmail.com>
2025-10-18 12:32:08 +0300

    add register win

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -22,11 +22,11 @@ class OrderResource extends Resource
     protected static ?string $model = Order::class;
 
   //  protected static ?string $navigationIcon = 'fas-truck';
-    protected static ?string $navigationGroup='الخدمات المدفوعة';
+    protected static ?string $navigationGroup='الشحن';
     protected static ?string $label='طلبات الشحن';
     protected static ?string $navigationLabel='طلبات الشحن';
     protected static ?string $pluralLabel='طلبات الشحن';
-protected static ?int $navigationSort=-14;
+
 
     public static function getNavigationBadge(): ?string
     {
--- app/Filament/Resources/SubscriptionResource.php
+++ app/Filament/Resources/SubscriptionResource.php
@@ -21,7 +21,8 @@ class SubscriptionResource extends Resource
     protected static ?string $model = PlanUser::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
+protected static ?string $label="الخدمات المدفوعة";
+protected static ?int $navigationSort=-1;
     public static function form(Form $form): Form
     {
         return $form
