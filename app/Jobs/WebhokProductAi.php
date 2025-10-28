2fedc5bc48d71cff40c921a17a13780273b4e448
sham <albasha.sq@gmail.com>
2025-10-28 11:10:06 +0300

    notification when replay comment

--- app/Jobs/WebhokProductAi.php
+++ app/Jobs/WebhokProductAi.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Enums\ProductActiveEnum;
 use App\Http\Resources\WebHok\ProductResource;
 use App\Models\Product;
 use Illuminate\Bus\Queueable;
@@ -32,8 +33,12 @@ class WebhokProductAi implements ShouldQueue
     {
         $product=new ProductResource($this->product);
         $res = \Http::post('http://85.215.154.88:5000/calculate-weight',$product);
-        if ($res->successful() && (double)$res->json('total_weight') > 0) {
-
+        if ($res->successful() && (double)$res->json('ratio') > 0) {
+            $this->product->update([
+                'weight' => $res->json('weight'),
+                'power'=>$res->json('ratio'),
+                'active'=> $res->json('status') == 'success' ? ProductActiveEnum::ACTIVE->value : ($res->json('status') == 'block' ? ProductActiveEnum::BLOCK->value : ProductActiveEnum::PENDING->value)
+            ]);
         }
     }
 }
