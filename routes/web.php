ee1d0a6ce995549f1eb6e70120d2355b2804a92c
sham <albasha.sq@gmail.com>
2025-10-28 11:03:01 +0300

    notification when replay comment

--- routes/web.php
+++ routes/web.php
@@ -146,7 +146,7 @@ Route::middleware([\App\Http\Middleware\XFrameOptionMiddleware::class])->group(f
 
     Route::get('testnot/{id?}', function ($id = null) {
         $p=\App\Models\Product::first();
-        return  ProductResource::toArray($p)->toArray(request());
+        return new ProductResource($p);
        // dd( \DB::table('users')->where('email', 'redadnan@gmail.com')->exists());
       /*  $products = \App\Models\Product::where([
             'user_id' => 54184,
