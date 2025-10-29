88c9b1277068725de09befa8f3f61b8ef4e707af
sham <albasha.sq@gmail.com>
2025-10-29 09:37:42 +0300

    notification when replay comment

--- app/GraphQL/Resolvers/ProductInfo.php
+++ app/GraphQL/Resolvers/ProductInfo.php
@@ -17,7 +17,7 @@ class ProductInfo
     {
 
            if($root->type==CategoryTypeEnum::SERVICE->value){
-             return   "{$root->info} <br/> <b>العنوان :</b> {$root->address}<br/> {$root->phone}";
+             return   "{$root->info}";
            }
            return $root->name .' '.$root->info;
        }
