a76ab7eed6601b2347d228c7e06d81102f5df780
sham <albasha.sq@gmail.com>
2025-11-01 11:57:31 +0300

    notification when replay comment

---
+++ app/GraphQL/Queries/SliderGroup/NearSeller.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries\SliderGroup;
+
+use App\Models\Product;
+use App\Models\Setting;
+use App\Models\User;
+
+final  class NearSeller
+{
+    /** @param array{} $args */
+    public function __invoke($_, array $args)
+    {
+        if (auth()->check()) {
+            return User::seller()->whereHas('media', fn($query) => $query->where('collection_name', 'image'))->where('city_id', auth()->user()->city_id)->inRandomOrder()->get();
+        }
+        return [];
+    }
+}
