fec1f0e82f0829316c56631e5910b6cd77bde2c8
sham <albasha.sq@gmail.com>
2025-11-13 11:42:47 +0300

    add virtual likes

--- app/Filament/Resources/ProductResource/RelationManagers/CommentsRelationManager.php
+++ app/Filament/Resources/ProductResource/RelationManagers/CommentsRelationManager.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources\ProductResource\RelationManagers;
 
+use App\Models\Comment;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -13,9 +15,10 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class CommentsRelationManager extends RelationManager
 {
     protected static string $relationship = 'comments';
-protected static ?string $title='التعليقات';
-protected static ?string $label='التعليقات';
-protected static ?string $pluralLabel='التعليقات';
+    protected static ?string $title = 'التعليقات';
+    protected static ?string $label = 'التعليقات';
+    protected static ?string $pluralLabel = 'التعليقات';
+
     public function form(Form $form): Form
     {
         return $form
@@ -23,8 +26,8 @@ protected static ?string $pluralLabel='التعليقات';
                 Forms\Components\Textarea::make('comment')
                     ->required()
                     ->maxLength(255)->label('التعليق')->columnSpan(2),
-                Forms\Components\TextInput::make('user_id')->default(fn($record,$context) =>$context=='view'|| $context=='edit'? $record->user?->name:'')->label('المستخدم')->visible(fn($context)=>$context=='view'),
-                Forms\Components\Placeholder::make('created_at')->content(fn($record,$context) =>$context=='view'|| $context=='edit'?$record->created_at?->format('Y-m-d'):'')->label('تاريخ كتابة التعليق')->visible(fn($context)=>$context=='view')
+                Forms\Components\TextInput::make('user_id')->default(fn($record, $context) => $context == 'view' || $context == 'edit' ? $record->user?->name : '')->label('المستخدم')->visible(fn($context) => $context == 'view'),
+                Forms\Components\Placeholder::make('created_at')->content(fn($record, $context) => $context == 'view' || $context == 'edit' ? $record->created_at?->format('Y-m-d') : '')->label('تاريخ كتابة التعليق')->visible(fn($context) => $context == 'view')
             ]);
     }
 
@@ -47,6 +50,18 @@ protected static ?string $pluralLabel='التعليقات';
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
+                Tables\Actions\Action::make('replay')->form([
+                    Forms\Components\Textarea::make('replay')->required()->maxLength(255)->label('الرد'),
+                ])->action(function ($record, $data) {
+                    Comment::create([
+                        'comment' => $data['replay'],
+                        'user_id' => auth()->id(),
+                        'product_id' => $record->product_id,
+                        'comment_id' => $record->id,
+
+                    ]);
+                    Notification::make('success')->title('نجاح العملية')->body('تم إضافة الرد')->success()->send();
+                })
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
