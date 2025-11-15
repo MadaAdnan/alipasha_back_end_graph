0926ac89aa62d32d8ddd16f595ccc2e9a60a6785
sham <albasha.sq@gmail.com>
2025-11-15 07:41:26 +0300

    add clickWhats

--- app/Filament/Resources/ClickWhatsResource.php
+++ app/Filament/Resources/ClickWhatsResource.php
@@ -38,9 +38,9 @@ public static function canCreate(): bool
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user.name')->label('سجل إعجابه'),
-                Tables\Columns\TextColumn::make('seller.name')->label('المتجر'),
-                Tables\Columns\TextColumn::make('product.name')->label('المنتجات'),
+                Tables\Columns\TextColumn::make('user.name')->label('سجل إعجابه')->url(fn($record)=>UserResource::getUrl('edit',['record'=>$record->user_id])),
+                Tables\Columns\TextColumn::make('seller.name')->label('المتجر')->url(fn($record)=>UserResource::getUrl('edit',['record'=>$record->seller_id])),
+                Tables\Columns\TextColumn::make('product.name')->label('المنتجات')->url(fn($record)=>ProductResource::getUrl('edit',['record'=>$record->product_id])),
             ])
             ->filters([
                 //
