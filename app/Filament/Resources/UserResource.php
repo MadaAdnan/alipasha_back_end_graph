7003cee668cafa7874c742d4171a0cc2403aaa26
sham <albasha.sq@gmail.com>
2025-11-29 13:23:08 +0300

    add clickWhats

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -399,7 +399,15 @@ class UserResource extends Resource
             ])
             ->headerActions([
                 ExportAction::make()->exports([
-                    ExcelExport::make()->fromTable()->withChunkSize(200)->askForFilename()->queue()
+                    ExcelExport::make()->modifyQueryUsing(fn (Builder $query) => User::query()
+                        ->select('id', 'name', 'full_phone','email','seller_name','email_verified_at', 'created_at') // إختر فقط الحقول اللازمة
+                        ->orderBy('id'))
+                ])->exports([
+                    ExcelExport::make()
+                        ->fromTable()            // يبقى fromTable لكن سيأخذ الـ query المعدّل أعلاه
+                        ->withChunkSize(500)     // حجم الـ chunk لمعالجة أقل ذاكرة
+                        ->queue()                // ضع التصدير في queue لأن العملية ثقيلة
+                        ->askForFilename()
                         ->withFilename(fn($filename) => 'ali-pasha-' . $filename),
                 ])->visible(/*auth()->user()->can('export_users')*/true),
                 Tables\Actions\Action::make('send_msg_phone')->form([
