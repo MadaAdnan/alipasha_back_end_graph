cadf6859a4def84c33d36ea0cbccf86e3a9831ac
sham <albasha.sq@gmail.com>
2025-12-15 11:08:20 +0300

    add clickWhats

--- app/Jobs/WebhokProductAi.php
+++ app/Jobs/WebhokProductAi.php
@@ -40,7 +40,7 @@ class WebhokProductAi implements ShouldQueue
                 $this->product->update([
                     'weight' => $res->json('weight'),
                     'power'=>$res->json('ratio'),
-                    'active'=> $res->json('status') == 'success' ? ProductActiveEnum::ACTIVE->value : ($res->json('status') == 'block' ? ProductActiveEnum::BLOCK->value : ProductActiveEnum::PENDING->value),
+                    'active'=> $res->json('status') == 'success' ? ProductActiveEnum::ACTIVE->value : ProductActiveEnum::BLOCK->value,
                     'block_msg' => $res->json('description')
                 ]);
             }
