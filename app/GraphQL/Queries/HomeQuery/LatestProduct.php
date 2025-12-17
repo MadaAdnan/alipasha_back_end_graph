dc1983213d09c9c32a881720a78ea1e2c1d24d9b
sham <albasha.sq@gmail.com>
2025-12-17 10:24:01 +0300

    add filter home query

--- app/GraphQL/Queries/HomeQuery/LatestProduct.php
+++ app/GraphQL/Queries/HomeQuery/LatestProduct.php
@@ -17,70 +17,26 @@ final class LatestProduct
      */
     public function __invoke($_, array $args)
     {
+        $popularCategories = [];
+        $popularSellers =[];
+        $categoryId= $args['category_id'] ?? null;
+        $cityId= $args['city_id'] ?? null;
         $setting = Setting::first();
         $now = now(); // خزّن الآن مرة واحدة لتجنب فروق زمنية صغيرة
 
-        $popularCategories = [];//$this->getPopularCategoryProducts() ?? [];
-        $popularSellers =[];// $this->getPopularSelelrProducts() ?? [];
-$categoryId=$args['category_id']??null;
-$cityId=$args['city_id']??null;
-       /* $productsQuery = Product::active()
+
+
+        $products = Product::active()
             ->whereHas('user', fn($q) => $q->where('users.is_active', 1))
             ->where('power', '>', 20)
             ->whereDoesntHave('category', fn($q) => $q->where('type', CategoryTypeEnum::RESTAURANT->value))
             ->whereNot('level', LevelProductEnum::SPECIAL->value)
-            // ===== هنا: الشرط الخاص بـ end_date (NULL أو صالح) =====
-            ->where(function ($q) use ($now) {
-                $q->whereNull('end_date')                    // أظهر المنتجات التي end_date = NULL
-                ->orWhere('end_date', '>', $now)           // أو التي تاريخها في المستقبل
-                ->orWhere('end_date', '')                  // أو حقل فارغ '' (إذا كان لديك مثل هذه القيم)
-                ->orWhereRaw("end_date = '0000-00-00' OR end_date = '0000-00-00 00:00:00'"); // تعامل مع الـ zero-date إن وجد
-            })
-            ->whereIn('type', [
-                CategoryTypeEnum::PRODUCT->value,
-                CategoryTypeEnum::TENDER->value,
-                CategoryTypeEnum::JOB->value,
-                CategoryTypeEnum::SEARCH_JOB->value,
-                CategoryTypeEnum::NEWS->value,
-            ])
-            ->where('created_at', '>=', now()->subDays($setting->options['recommended_month'] ?? 30));
-        if (auth()->check()) {
-            $productsQuery->where(function ($q) use ($popularCategories, $popularSellers) {
-                if (!empty($popularCategories)) {
-                    $q->whereNotIn('category_id', $popularCategories);
-                }
-                if (!empty($popularSellers)) {
-                    $q->whereNotIn('user_id', $popularSellers);
-                }
-            });
-        }
-
-        $products = $productsQuery->inRandomOrder();*/
-        $products = Product::query()
-            ->active()
-            ->where('power', '>', 20)
-
-            // المستخدم فعّال
-            ->whereHas('user', fn ($q) =>
-            $q->where('users.is_active', 1)
-            )
-
-            // استثناء المطاعم
-            ->whereDoesntHave('category', fn ($q) =>
-            $q->where('type', CategoryTypeEnum::RESTAURANT->value)
-            )
-
-            // استثناء المنتجات الخاصة
-            ->where('level', '!=', LevelProductEnum::SPECIAL->value)
-
-            // فلترة end_date
             ->where(function ($q) use ($now) {
                 $q->whereNull('end_date')
-                    ->orWhere('end_date', '>', $now)
-                    ->orWhereIn('end_date', ['', '0000-00-00', '0000-00-00 00:00:00']);
+                ->orWhere('end_date', '>', $now)
+                ->orWhere('end_date', '')
+                ->orWhereRaw("end_date = '0000-00-00' OR end_date = '0000-00-00 00:00:00'");
             })
-
-            // أنواع المنتجات
             ->whereIn('type', [
                 CategoryTypeEnum::PRODUCT->value,
                 CategoryTypeEnum::TENDER->value,
@@ -88,76 +44,44 @@ $cityId=$args['city_id']??null;
                 CategoryTypeEnum::SEARCH_JOB->value,
                 CategoryTypeEnum::NEWS->value,
             ])
+            ->where('created_at', '>=', now()->subDays($setting->options['recommended_month'] ?? 30))->inRandomOrder();
 
-            // المدة الزمنية
-            ->where('created_at', '>=',
-                now()->subDays($setting->options['recommended_month'] ?? 30)
-            )
-
-            // فلترة حسب القسم (إن وُجد)
-            ->when($categoryId, fn ($q) =>
-            $q->where('category_id', $categoryId)
-            )
-
-            // فلترة حسب مدينة المستخدم (إن وُجد)
-            ->when($cityId, fn ($q) =>
-            $q->whereHas('user', fn ($q) =>$q->where('users.city_id',$cityId))
-            )
-
-            // استثناء التصنيفات والبائعين الشائعين للمستخدم المسجّل
-          /*  ->when(auth()->check(), function ($q) use ($popularCategories, $popularSellers) {
-                $q->when(!empty($popularCategories), fn ($qq) =>
-                $qq->whereNotIn('category_id', $popularCategories)
-                );
-
-                $q->when(!empty($popularSellers), fn ($qq) =>
-                $qq->whereNotIn('user_id', $popularSellers)
-                );
-            })*/
-
-            ->inRandomOrder()
-           ;
         $ids = $products->pluck('id')->toArray();
         $ratio = $setting->ratio_view_home;
-         // نحسب النصف (في حال كان العدد فردي)
-        if(count($ids)>0) {
-            $half = ceil(count($ids) * $ratio ?? 0.5);
-            if ($half > 0) {
-                $idsChunks = array_chunk($ids, (int)$half); // يقسم المصفوفة إلى أجزاء
+        $half = ceil(count($ids) * $ratio ?? 0.5); // نحسب النصف (في حال كان العدد فردي)
+        $idsChunks = array_chunk($ids, (int)$half); // يقسم المصفوفة إلى أجزاء
 
-                list($firstHalf, $secondHalf) = $idsChunks; // نفصلها في متغيرين
-                $today = today();
+        list($firstHalf, $secondHalf) = $idsChunks; // نفصلها في متغيرين
+        $today = today();
 
-                \DB::transaction(function () use ($firstHalf, $today) {
+        \DB::transaction(function () use ($firstHalf, $today) {
 
-                    // تحديث السجلات الموجودة
-                    \DB::table('product_views')
-                        ->whereIn('product_id', $firstHalf)
-                        ->whereDate('view_at', $today)
-                        ->update(['count' => \DB::raw('count + 1')]);
-                    $existingIds = \DB::table('product_views')
-                        ->whereIn('product_id', $firstHalf)
-                        ->whereDate('view_at', $today)
-                        ->pluck('product_id')
-                        ->toArray();
-                    $newIds = array_diff($firstHalf, $existingIds);
-                    if (
-                        !empty($newIds)) {
-                        $inserts = array_map(function ($firstHalf) use ($today) {
-                            return [
-                                'product_id' => $firstHalf,
-                                'view_at' => $today,
-                                'count' => 1,
-                                'created_at' => now(),
-                                'updated_at' => now(),
-                            ];
-                        }, $newIds);
+            // تحديث السجلات الموجودة
+            \DB::table('product_views')
+                ->whereIn('product_id', $firstHalf)
+                ->whereDate('view_at', $today)
+                ->update(['count' => \DB::raw('count + 1')]);
+            $existingIds = \DB::table('product_views')
+                ->whereIn('product_id', $firstHalf)
+                ->whereDate('view_at', $today)
+                ->pluck('product_id')
+                ->toArray();
+            $newIds = array_diff($firstHalf, $existingIds);
+            if (
+                !empty($newIds)) {
+                $inserts = array_map(function ($firstHalf) use ($today) {
+                    return [
+                        'product_id' => $firstHalf,
+                        'view_at' => $today,
+                        'count' => 1,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ];
+                }, $newIds);
 
-                        \DB::table('product_views')->insert($inserts);
-                    }
-                });
+                \DB::table('product_views')->insert($inserts);
             }
-        }
+        });
         return $products;
     }
 
--- app/GraphQL/Queries/HomeQuery/SpecialProduct.php
+++ app/GraphQL/Queries/HomeQuery/SpecialProduct.php
@@ -12,40 +12,37 @@ use App\Models\Setting;
 final class SpecialProduct
 {
     /**
-     * @param  null  $_
-     * @param  array{}  $args
+     * @param null $_
+     * @param array{} $args
      */
     public function __invoke($_, array $args)
     {
-
-//return Product::where('id',0);
-        $setting=Setting::first();
- $now=now();
-      $products= Product::where(['active'=>ProductActiveEnum::ACTIVE->value,
-            'level'=>LevelProductEnum::SPECIAL->value])
-          ->whereHas('user',fn($q)=>$q->where('users.is_active', 1))
-
-            ->where(fn( $query)=>$query->whereDoesntHave('category',fn($query)=>$query->where('type',CategoryTypeEnum::RESTAURANT->value)))
+        $categoryId= $args['category_id'] ?? null;
+        $cityId= $args['city_id'] ?? null;
+        $setting = Setting::first();
+        $now = now();
+        $products = Product::where(['active' => ProductActiveEnum::ACTIVE->value,
+            'level' => LevelProductEnum::SPECIAL->value])
+            ->whereHas('user', fn($q) => $q->where('users.is_active', 1))
+            ->where(fn($query) => $query->whereDoesntHave('category', fn($query) => $query->where('type', CategoryTypeEnum::RESTAURANT->value)))
             ->where(function ($q) use ($now) {
                 $q->whereNull('end_date')                    // أظهر المنتجات التي end_date = NULL
                 ->orWhere('end_date', '>', $now)           // أو التي تاريخها في المستقبل
                 ->orWhere('end_date', '')                  // أو حقل فارغ '' (إذا كان لديك مثل هذه القيم)
                 ->orWhereRaw("end_date = '0000-00-00' OR end_date = '0000-00-00 00:00:00'"); // تعامل مع الـ zero-date إن وجد
             })
-          ->whereIn('type',[
-              CategoryTypeEnum::PRODUCT->value,
-              CategoryTypeEnum::TENDER->value,
-              CategoryTypeEnum::JOB->value,
-              CategoryTypeEnum::SEARCH_JOB->value,
-              CategoryTypeEnum::NEWS->value,
-          ])->inRandomOrder()->where('created_at','>=',now()->subDays($setting->options['recommended_month']??30))
-            ->when(auth()->check(),fn($query)=>$query->where(fn($q)=>
-            $q->whereNotIn('category_id',$this->getPopularCategoryProducts())
-                ->whereNotIn('user_id',$this->getPopularSelelrProducts())
-            ))
-        ;
-        $products=$this->newQuery();
-       $ids = $products->pluck('id')->toArray();
+            ->whereIn('type', [
+                CategoryTypeEnum::PRODUCT->value,
+                CategoryTypeEnum::TENDER->value,
+                CategoryTypeEnum::JOB->value,
+                CategoryTypeEnum::SEARCH_JOB->value,
+                CategoryTypeEnum::NEWS->value,
+            ])->inRandomOrder()->where('created_at', '>=', now()->subDays($setting->options['recommended_month'] ?? 30))
+            ->when(auth()->check(), fn($query) => $query->where(fn($q) => $q->whereNotIn('category_id', $this->getPopularCategoryProducts())
+                ->whereNotIn('user_id', $this->getPopularSelelrProducts())
+            ));
+        $products = $this->newQuery();
+        $ids = $products->pluck('id')->toArray();
         $ratio = $setting->ratio_view_home;
         $half = ceil(count($ids) * $ratio ?? 0.5);
         $idsChunks = array_chunk($ids, (int)$half); // يقسم المصفوفة إلى أجزاء
@@ -93,22 +90,23 @@ final class SpecialProduct
             ->orderByRaw('SUM(visited) DESC')
             ->pluck('category_id')->toArray();
     }
+
     private function getPopularSelelrProducts()
     {
         return Interaction::where('user_id', auth()->id())->whereNotNull('seller_id')
             ->groupBy('seller_id')
-
             ->pluck('seller_id')->toArray();
     }
 
-    private function newQuery(){
-        $sellers=[];
-        $setting=Setting::first();
-        if(auth()->check()){
-            $sellers=auth()->user()->followers->pluck('seller_id')->toArray();
+    private function newQuery()
+    {
+        $sellers = [];
+        $setting = Setting::first();
+        if (auth()->check()) {
+            $sellers = auth()->user()->followers->pluck('seller_id')->toArray();
         }
-        $specialLevel=LevelProductEnum::SPECIAL->value;
-        $now=now();
+        $specialLevel = LevelProductEnum::SPECIAL->value;
+        $now = now();
         $products = Product::active()->whereIn('type', [
             CategoryTypeEnum::PRODUCT->value,
             CategoryTypeEnum::TENDER->value,
@@ -116,22 +114,20 @@ final class SpecialProduct
             CategoryTypeEnum::SEARCH_JOB->value,
             CategoryTypeEnum::NEWS->value,
         ])
-            ->whereHas('user',fn($q)=>$q->where('users.is_active', 1))
-
-  ->where(function ($q) use ($now) {
-            $q->whereNull('end_date')                    // أظهر المنتجات التي end_date = NULL
-            ->orWhere('end_date', '>', $now)           // أو التي تاريخها في المستقبل
-            ->orWhere('end_date', '')                  // أو حقل فارغ '' (إذا كان لديك مثل هذه القيم)
-            ->orWhereRaw("end_date = '0000-00-00' OR end_date = '0000-00-00 00:00:00'"); // تعامل مع الـ zero-date إن وجد
-        })
-          //  ->where('created_at', '>=', now()->subDays($setting->options['recommended_month'] ?? 30))->inRandomOrder()
+            ->whereHas('user', fn($q) => $q->where('users.is_active', 1))
+            ->where(function ($q) use ($now) {
+                $q->whereNull('end_date')                    // أظهر المنتجات التي end_date = NULL
+                ->orWhere('end_date', '>', $now)           // أو التي تاريخها في المستقبل
+                ->orWhere('end_date', '')                  // أو حقل فارغ '' (إذا كان لديك مثل هذه القيم)
+                ->orWhereRaw("end_date = '0000-00-00' OR end_date = '0000-00-00 00:00:00'"); // تعامل مع الـ zero-date إن وجد
+            })
+            //  ->where('created_at', '>=', now()->subDays($setting->options['recommended_month'] ?? 30))->inRandomOrder()
             ->where('power', '>=', 20)
             ->orderByRaw("
         (level = ?) DESC,
         (user_id IN (" . ($sellers ? implode(',', $sellers) : 0) . ")) DESC,
         RAND()
-    ", [$specialLevel])
-        ;
+    ", [$specialLevel]);
         return $products;
     }
 }
