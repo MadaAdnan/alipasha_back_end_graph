f63641b848f072246254e0837168b5956fed75da
sham <albasha.sq@gmail.com>
2025-12-18 10:48:21 +0300

    add filter home query

--- app/GraphQL/Queries/Suggestions.php
+++ app/GraphQL/Queries/Suggestions.php
@@ -2,15 +2,24 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Models\User;
+
 final  class Suggestions
 {
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
         $search=  $args['search']??null;
-        return \App\Models\Product::active()->whereNotNull('name')->where(function ($query)use($search){
-            $query->where('name', 'like', "%$search%");
-            $query->orWhere('expert', 'like', "%$search%");
-        })->take(10)->pluck('name')->toArray();
+        $type=  $args['type']??'products';
+        if($type=='products'){
+            return \App\Models\Product::active()->whereNotNull('name')->where(function ($query)use($search){
+                $query->where('name', 'like', "%$search%");
+                $query->orWhere('expert', 'like', "%$search%");
+            })->take(10)->pluck('name')->toArray();
+        }
+        if($type=='sellers'){
+            return User::seller()->take(10)->whereNotNull('seller_name')->pluck('seller_name')->toArray();
+        }
+       
     }
 }
