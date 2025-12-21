802a04d0f0e5ca00e52003a3441e4df24a19693e
sham <albasha.sq@gmail.com>
2025-12-21 12:05:17 +0300

    add filter home query

--- app/Filament/Resources/SellerResource.php
+++ app/Filament/Resources/SellerResource.php
@@ -264,7 +264,7 @@ class SellerResource extends Resource implements HasShieldPermissions
                             ->label('المدينة'),
                     ])
                     ->query(function (Builder $query, array $data) {
-
+dd($query->first());
                         return $query
                                 ->when($data['from'], function (Builder $query, $date) {
                                     $query->having('last_product_date', '>=', $date);
