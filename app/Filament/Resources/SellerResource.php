f8177b3229962dfc2d2084fa6c8a997650bbee7e
sham <albasha.sq@gmail.com>
2025-12-21 11:22:05 +0300

    add filter home query

--- app/Filament/Resources/SellerResource.php
+++ app/Filament/Resources/SellerResource.php
@@ -304,6 +304,7 @@ class SellerResource extends Resource implements HasShieldPermissions
                     ->exports([
                         ExcelExport::make()
                             ->fromTable() // يصدر نفس بيانات الجدول + الفلاتر
+                            ->withWriterType(\Maatwebsite\Excel\Excel::CSV)
                             ->withFilename('users-' . now()->format('Y-m-d')),
                     ]),
             ]);
