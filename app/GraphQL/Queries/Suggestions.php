c5be5be7879d523568cb5abd763025c209f30daa
sham <albasha.sq@gmail.com>
2025-12-21 12:25:12 +0300

    add filter home query

--- app/GraphQL/Queries/Suggestions.php
+++ app/GraphQL/Queries/Suggestions.php
@@ -14,10 +14,13 @@ final  class Suggestions
         if($type=='seller'){
             return User::seller()->take(10)->whereNotNull('seller_name')->pluck('seller_name')->toArray();
         }else{
-            return \App\Models\Product::active()->whereNotNull('name')->where(function ($query)use($search){
+            return \App\Models\Product::active()
+                ->whereNotNull('name')->where(function ($query)use($search){
                 $query->where('name', 'like', "%$search%");
                 $query->orWhere('expert', 'like', "%$search%");
-            })->take(10)->pluck('name')->toArray();
+            })
+                ->where('type',$type)
+                ->take(10)->pluck('name')->toArray();
         }
 
 
