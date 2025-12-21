79b5f744564cc790a3b5c1e18179a6347d631a30
sham <albasha.sq@gmail.com>
2025-12-21 10:13:03 +0300

    add filter home query

--- app/Filament/Resources/SellerResource.php
+++ app/Filament/Resources/SellerResource.php
@@ -233,7 +233,7 @@ class SellerResource extends Resource implements HasShieldPermissions
               Tables\Columns\TextColumn::make('seller_name')->label('اسم المتجر'),
               Tables\Columns\TextColumn::make('products_count')->label('عدد المنتجات'),
               Tables\Columns\TextColumn::make('followers_count')->label('عدد المتابعين')->sortable(),
-              Tables\Columns\TextColumn::make('last_product_date')->label('تاريخ آخر نشر')->sortable(),
+              Tables\Columns\TextColumn::make('last_product_date')->since()->label('تاريخ آخر نشر')->sortable(),
             ])
             ->filters([
                 //
