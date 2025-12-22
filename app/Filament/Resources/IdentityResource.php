dbe0a594c5a45ffff4f0b15972011f4dd5e0fe3f
sham <albasha.sq@gmail.com>
2025-12-22 13:13:16 +0300

    add filter home query

--- app/Filament/Resources/IdentityResource.php
+++ app/Filament/Resources/IdentityResource.php
@@ -48,8 +48,8 @@ class IdentityResource extends Resource
                     ->getOptionLabelUsing(fn ($value): ?string =>
                     User::find($value)?->name
                     ),
-                Forms\Components\SpatieMediaLibraryFileUpload::make('front')->label('الوجه الأمامي')->required(),
-                Forms\Components\SpatieMediaLibraryFileUpload::make('back')->label('الوجه الخلفي')->required(),
+                Forms\Components\SpatieMediaLibraryFileUpload::make('front')->label('الوجه الأمامي')->required()->openable(),
+                Forms\Components\SpatieMediaLibraryFileUpload::make('back')->label('الوجه الخلفي')->required()->openable(),
                 Forms\Components\Select::make('status')->options([
                     OrderStatusEnum::PENDING->value=>OrderStatusEnum::PENDING->getLabel(),
                     OrderStatusEnum::COMPLETE->value=>OrderStatusEnum::COMPLETE->getLabel(),
