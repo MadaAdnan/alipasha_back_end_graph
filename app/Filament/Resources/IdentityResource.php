60d96be22bbb52bbc50b6a81de323d3ebadf90e3
sham <albasha.sq@gmail.com>
2025-12-23 11:08:46 +0300

    add graph identity

--- app/Filament/Resources/IdentityResource.php
+++ app/Filament/Resources/IdentityResource.php
@@ -70,6 +70,8 @@ class IdentityResource extends Resource
                 Tables\Columns\TextColumn::make('status')->formatStateUsing(fn($state) => OrderStatusEnum::tryFrom($state)->getLabel())->label('الحالة'),
                 Tables\Columns\SpatieMediaLibraryImageColumn::make('front')->collection('front')->conversion('web')->url(fn($record) => $record->getFirstMediaUrl('front', 'web'), true),
                 Tables\Columns\SpatieMediaLibraryImageColumn::make('back')->collection('back')->conversion('web')->url(fn($record) => $record->getFirstMediaUrl('back', 'web'), true),
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('passport')->collection('passport')->conversion('web')->url(fn($record) => $record->getFirstMediaUrl('back', 'web'), true),
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('record')->collection('record')->conversion('web')->url(fn($record) => $record->getFirstMediaUrl('back', 'web'), true),
                 Tables\Columns\TextColumn::make('id')->label('#'),
             ])
             ->filters([
--- app/GraphQL/Mutations/VerifyIdentity.php
+++ app/GraphQL/Mutations/VerifyIdentity.php
@@ -17,6 +17,12 @@ final  class VerifyIdentity
         ])->exists()) {
             throw new GraphQLExceptionHandler('لديك طلب توثيق سابق وهو قيد المراجعة');
         }
+
+        if (empty($input['passport']) && empty($input['record']) &&
+            (empty($input['imageFront']) || empty($input['imageBack']))) {
+            throw new GraphQLExceptionHandler('الرجاء إدخال صور');
+        }
+
         try{
 
             \DB::beginTransaction();
@@ -24,8 +30,18 @@ final  class VerifyIdentity
                 'user_id' => auth()->id(),
                 'status' => 'pending'
             ]);
-            $identity->addMedia($input['imageFront'])->toMediaCollection('front');
-            $identity->addMedia($input['imageBack'])->toMediaCollection('back');
+            if(!empty($input['imageFront'])){
+                $identity->addMedia($input['imageFront'])->toMediaCollection('front');
+                $identity->addMedia($input['imageBack'])->toMediaCollection('back');
+            }
+            if(!empty($input['record'])){
+                $identity->addMedia($input['record'])->toMediaCollection('record');
+            }
+            if(!empty($input['passport'])){
+                $identity->addMedia($input['passport'])->toMediaCollection('passport');
+            }
+
+
             \DB::commit();
             return [
                 'user'=>auth()->user(),
