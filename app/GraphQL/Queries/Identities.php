32d458c9f7b5418ec240a93888bd5539ad9120e9
sham <albasha.sq@gmail.com>
2025-12-23 08:59:22 +0300

    add graph identity

--- app/GraphQL/Queries/Identities.php
+++ app/GraphQL/Queries/Identities.php
@@ -10,7 +10,7 @@ final  class Identities
     /** @param array{} $args */
     public function __invoke($_, array $args)
     {
-        $identity = Identity::where('user_id', auth()->id)->where(function ($query) {
+        $identity = Identity::where('user_id', auth()->id())->where(function ($query) {
             $query->where('status', 'pending');
             $query->orWhere('status', OrderStatusEnum::COMPLETE->value);
         })->first();
