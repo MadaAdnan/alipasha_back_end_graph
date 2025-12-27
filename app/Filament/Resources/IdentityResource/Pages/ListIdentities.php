f6c67b89f577e5c30b769b5b47ad66da5d18e209
Adnan <mh.shamey@gmail.com>
2025-12-27 12:51:38 +0300

    add taps identity

--- app/Filament/Resources/IdentityResource/Pages/ListIdentities.php
+++ app/Filament/Resources/IdentityResource/Pages/ListIdentities.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources\IdentityResource\Pages;
 
+use App\Enums\IdentityEnum;
+use App\Enums\OrderStatusEnum;
 use App\Filament\Resources\IdentityResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListIdentities extends ListRecords
@@ -14,6 +17,17 @@ class ListIdentities extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+
+            Tab::make( IdentityEnum::PENDING->value)->modifyQueryUsing(fn($query)=>$query->where('status',IdentityEnum::PENDING->value)->with(['user']))->label(IdentityEnum::PENDING->getLabel()),
+            Tab::make( IdentityEnum::COMPLETE->value)->modifyQueryUsing(fn($query)=>$query->where('status',IdentityEnum::COMPLETE->value)->with(['user']))->label(IdentityEnum::COMPLETE->getLabel()),
+            Tab::make( IdentityEnum::CANCELE->value)->modifyQueryUsing(fn($query)=>$query->where('status',IdentityEnum::CANCELE->value)->with(['user']))->label(IdentityEnum::CANCELE->getLabel()),
+
+
         ];
     }
 }
