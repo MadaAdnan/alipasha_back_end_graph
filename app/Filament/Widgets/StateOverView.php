57b6f661d2bddb103f1dba4f099af809a3da9f72
Adnan <mh.shamey@gmail.com>
2025-12-27 13:11:19 +0300

    add taps identity1

--- app/Filament/Widgets/StateOverView.php
+++ app/Filament/Widgets/StateOverView.php
@@ -24,8 +24,9 @@ class StateOverView extends BaseWidget
         $users=User::select(['id','is_seller','email_verified_at'])/*->whereBetween('created_at',[$start,$end])*/->get();
         $products=Product::select(['id','type'])->where('active',ProductActiveEnum::ACTIVE->value)/*->whereBetween('created_at',[$start,$end])*/->get();
         return [
-            Stat::make('عدد المستخدمين الموثقين', $users->where('is_seller','=',0)->whereNotNull('email_verified_at')->count()),
-            Stat::make('عدد المستخدمين غير الموثقين', $users->where('is_seller','=',0)->whereNull('email_verified_at')->count()),
+            Stat::make('عدد المستخدمين الموثقين', $users->where('is_verified','=',1)->count()),
+            Stat::make('عدد المستخدمين المؤكدين', $users->where('is_seller','=',0)->whereNotNull('email_verified_at')->count()),
+            Stat::make('عدد المستخدمين غير المؤكدين', $users->where('is_seller','=',0)->whereNull('email_verified_at')->count()),
             Stat::make('عدد المتاجر ', $users->where('is_seller','=',1)->count()),
             Stat::make('عدد المنتجات ', $products->whereIn('type',[
                 CategoryTypeEnum::RESTAURANT->value,
