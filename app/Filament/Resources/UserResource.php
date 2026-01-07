7837792d5d6a60765f2fe247aae0f68a956a304b
Adnan <mh.shamey@gmail.com>
2026-01-07 11:30:20 +0300

    add graph identity

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -323,7 +323,7 @@ class UserResource extends Resource
                     /* send msg chat */
                     Tables\Actions\Action::make('send_msg_chat')->form([
                         Forms\Components\Textarea::make('msg')->label('الرسالة')->required(),
-                    ])->record(fn($record)=>User::find($record->id))
+                    ])
                         ->action(function ($record, $data,$livewire) {
                             $user=auth()->user();
 
