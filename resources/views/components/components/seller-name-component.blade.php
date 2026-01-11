3148e0bccc09adf432c5ede4b574fa4f1efb24df
Adnan <mh.shamey@gmail.com>
2026-01-11 12:01:49 +0300

    add auth page

--- resources/views/components/components/seller-name-component.blade.php
+++ resources/views/components/components/seller-name-component.blade.php
@@ -2,7 +2,7 @@
     'seller'=>null
 ])
 @if($seller)
-<a class="cursor-pointer d-flex  align-items-center gap-1" href="{{route('seller.profile',$seller->id)}}">
+<a class="cursor-pointer d-flex justify-content-start  align-items-center gap-1" href="{{route('seller.profile',$seller->id)}}">
     <i class="fa fa-market"></i>
   <span class="seller-name d-inline-block fs-6">  {{$seller->seller_name ?? $seller->name}}</span>
     @if($seller->is_verified==true)
