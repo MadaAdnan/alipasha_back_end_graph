aacd02f33a41ca58c13865aea738268f34a2f6d1
Adnan <mh.shamey@gmail.com>
2026-01-11 16:58:46 +0300

    add auth page

--- resources/views/components/components/paginator-component.blade.php
+++ resources/views/components/components/paginator-component.blade.php
@@ -17,7 +17,7 @@
 
                 {{-- الأرقام --}}
                 @foreach ($paginator->links()->elements[0] ?? [] as $page => $url)
-                    <span class="mx-1 rounded py-1 px-2 border border-secondary {{ $paginator->currentPage() == $page ? 'bg-gold' : 'bg-white' }}">
+                    <span class="mx-1 btn rounded py-1 px-2 border border-secondary {{ $paginator->currentPage() == $page ? 'btn-red-accent' : 'bg-white' }}">
                         <a class="" href="{{ $url }}">
                             {{ $page }}
                         </a>
