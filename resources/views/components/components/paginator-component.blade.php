843b3095c35d576bd7a99bb59aa0656d20d58f94
Adnan <mh.shamey@gmail.com>
2026-01-11 16:40:49 +0300

    add auth page

--- resources/views/components/components/paginator-component.blade.php
+++ resources/views/components/components/paginator-component.blade.php
@@ -5,34 +5,34 @@
 
     <nav class="bg-white rounded p-2">
 
-            <ul class=" justify-content-center">
+            <div class=" justify-content-center">
 
                 {{-- السابق --}}
-                <li class=" {{ $paginator->onFirstPage() ? 'disabled' : '' }}">
+                <span class=" {{ $paginator->onFirstPage() ? 'disabled' : '' }}">
                     <a class=""
                        href="{{ $paginator->previousPageUrl() ?? '#' }}">
                         <i class="fa fa-angle-left"></i>
                     </a>
-                </li>
+                </span>
 
                 {{-- الأرقام --}}
                 @foreach ($paginator->links()->elements[0] ?? [] as $page => $url)
-                    <li class=" {{ $paginator->currentPage() == $page ? 'active' : '' }}">
+                    <span class=" {{ $paginator->currentPage() == $page ? 'active' : '' }}">
                         <a class="" href="{{ $url }}">
                             {{ $page }}
                         </a>
-                    </li>
+                    </span>
                 @endforeach
 
                 {{-- التالي --}}
-                <li class=" {{ $paginator->hasMorePages() ? '' : 'disabled' }}">
+                <span class=" {{ $paginator->hasMorePages() ? '' : 'disabled' }}">
                     <a class=""
                        href="{{ $paginator->nextPageUrl() ?? '#' }}">
                         <i class="fa fa-angle-right"></i>
                     </a>
-                </li>
+                </span>
 
-            </ul>
+            </div>
     </nav>
 
 @endif
