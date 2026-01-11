245b5c141135d760901d4be99cb9714c9bb7afc9
Adnan <mh.shamey@gmail.com>
2026-01-11 12:59:14 +0300

    add auth page

--- resources/views/theme2/index.blade.php
+++ resources/views/theme2/index.blade.php
@@ -3,8 +3,9 @@
     <div class="container my-2">
         <div class="row">
             <div class="col-md-12">
+                <x-components.side-bar-category-component :categories="[]"/>
             </div>
-            <x-components.side-bar-category-component :categories="[]"/>
+
             <div class="col-md-9">
 
             @if($categoriesWithProducts)
