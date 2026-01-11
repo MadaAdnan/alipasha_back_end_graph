740d01a3d6cfd52cbf758c98532417edf58ed087
Adnan <mh.shamey@gmail.com>
2026-01-11 16:19:29 +0300

    add auth page

--- resources/views/components/components/paginator-component.blade.php
+++ resources/views/components/components/paginator-component.blade.php
@@ -1,7 +1,7 @@
 @php use Illuminate\Pagination\UrlWindow; @endphp
 @props(['paginator'])
 
-@if ($paginator && $paginator->hasPages())
+@if ($paginator && ($paginator instanceof \Illuminate\Pagination\Paginator || $paginator instanceof \Illuminate\Pagination\LengthAwarePaginator)&& $paginator->hasPages())
     @php
 
         $elements = UrlWindow::make($paginator)->elements;
