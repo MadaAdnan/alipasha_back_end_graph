63304453a015cc7e5450b1fe6232802919273516
Adnan <mh.shamey@gmail.com>
2026-01-11 10:08:45 +0300

    add auth page

--- resources/views/components/components/side-bar-category-item-component.blade.php
+++ resources/views/components/components/side-bar-category-item-component.blade.php
@@ -4,7 +4,7 @@
 @php
 $dataCategory=$category;
  @endphp
-<a class="cursor-pointer ">
+<a class="cursor-pointer " href="{{route('category.show',$dataCategory?->id)}}">
 <div class="d-flex justify-content-between py-1">
     <div class="side-bar-category-item-icon">
         <i class="fa-regular fa-circle-dot"></i>
