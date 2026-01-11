d637f929747e82bd80aea7bc5c0755d1b02ea06a
Adnan <mh.shamey@gmail.com>
2026-01-11 16:10:17 +0300

    add auth page

---
+++ resources/views/components/components/paginator-component.blade.php
@@ -0,0 +1,35 @@
+@props([
+    'paginator'=>null,
+])
+@if ($paginator?->hasPages())
+    <nav>
+        <ul class="pagination justify-content-center">
+
+            {{-- السابق --}}
+            <li class="page-item {{ $paginator->onFirstPage() ? 'disabled' : '' }}">
+                <a class="page-link"
+                   href="{{ $paginator->previousPageUrl() ?? '#' }}">
+                    السابق
+                </a>
+            </li>
+
+            {{-- الأرقام --}}
+            @foreach ($paginator->links()->elements[0] ?? [] as $page => $url)
+                <li class="page-item {{ $paginator->currentPage() == $page ? 'active' : '' }}">
+                    <a class="page-link" href="{{ $url }}">
+                        {{ $page }}
+                    </a>
+                </li>
+            @endforeach
+
+            {{-- التالي --}}
+            <li class="page-item {{ $paginator->hasMorePages() ? '' : 'disabled' }}">
+                <a class="page-link"
+                   href="{{ $paginator->nextPageUrl() ?? '#' }}">
+                    التالي
+                </a>
+            </li>
+
+        </ul>
+    </nav>
+@endif
