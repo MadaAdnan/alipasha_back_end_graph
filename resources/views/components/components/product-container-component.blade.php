ab2cffbb90bbe231b0058d79f722ee9077cbd9f9
Adnan <mh.shamey@gmail.com>
2026-01-11 12:00:47 +0300

    add auth page

--- resources/views/components/components/product-container-component.blade.php
+++ resources/views/components/components/product-container-component.blade.php
@@ -21,7 +21,7 @@
     @if($products)
         <div class="row justify-content-center">
             @foreach($dataProducts as $product)
-                <div class="col-md-4">
+                <div class="col-md-3">
                 <x-components.product-container-item-component :product="$product"/>
                 </div>
             @endforeach
--- resources/views/components/components/seller-name-component.blade.php
+++ resources/views/components/components/seller-name-component.blade.php
@@ -3,8 +3,8 @@
 ])
 @if($seller)
 <a class="cursor-pointer d-flex  align-items-center gap-1" href="{{route('seller.profile',$seller->id)}}">
-    <img src="{{$seller->getImage('logo')}}" class="rounded-circle" style="width: 30px; height: 30px;">
-  <span class="seller-name d-inline-block">  {{$seller->seller_name ?? $seller->name}}</span>
+    <i class="fa fa-market"></i>
+  <span class="seller-name d-inline-block fs-6">  {{$seller->seller_name ?? $seller->name}}</span>
     @if($seller->is_verified==true)
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--blue-accent)" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
             <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
