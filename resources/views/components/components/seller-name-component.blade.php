94799e1b1db9f8682f361bab67fc0cc8aa0daea2
Adnan <mh.shamey@gmail.com>
2026-01-11 12:33:07 +0300

    add auth page

--- resources/views/components/components/seller-name-component.blade.php
+++ resources/views/components/components/seller-name-component.blade.php
@@ -1,10 +1,11 @@
 @props([
-    'seller'=>null
+    'seller'=>null,
+    'class'=>''
 ])
 @if($seller)
 <a class="cursor-pointer d-flex justify-content-start  align-items-center gap-1" href="{{route('seller.profile',$seller->id)}}">
     <i class="fa fa-shop"></i>
-  <span class="seller-name d-inline-block fs-6 flex-grow-1">  {{$seller->seller_name ?? $seller->name}}</span>
+  <span class="seller-name d-inline-block  flex-grow-1 {{$class??'fs-6'}}">  {{$seller->seller_name ?? $seller->name}}</span>
     @if($seller->is_verified==true)
         <i class="bi bi-patch-check-fill text-blue-accent"></i>
     @endif
