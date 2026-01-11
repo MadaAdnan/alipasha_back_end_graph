7e6e7f0faa8de351a2548090bac491fc00367dd9
Adnan <mh.shamey@gmail.com>
2026-01-11 16:13:25 +0300

    add auth page

--- resources/views/components/components/paginator-component.blade.php
+++ resources/views/components/components/paginator-component.blade.php
@@ -1,31 +1,45 @@
-@props([
-    'paginator'=>null,
-])
-@if ($paginator!=null && $paginator->hasPages())
+@props(['paginator'])
+
+@if ($paginator && $paginator->hasPages())
+    @php
+        use Illuminate\Pagination\UrlWindow;
+        $elements = UrlWindow::make($paginator)->elements;
+    @endphp
+
     <nav>
         <ul class="pagination justify-content-center">
 
             {{-- السابق --}}
             <li class="page-item {{ $paginator->onFirstPage() ? 'disabled' : '' }}">
-                <a class="page-link"
-                   href="{{ $paginator->previousPageUrl() ?? '#' }}">
+                <a class="page-link" href="{{ $paginator->previousPageUrl() ?? '#' }}">
                     السابق
                 </a>
             </li>
 
             {{-- الأرقام --}}
-            @foreach ($paginator->links()->elements[0] ?? [] as $page => $url)
-                <li class="page-item {{ $paginator->currentPage() == $page ? 'active' : '' }}">
-                    <a class="page-link" href="{{ $url }}">
-                        {{ $page }}
-                    </a>
-                </li>
+            @foreach ($elements as $element)
+                {{-- ثلاث نقاط --}}
+                @if (is_string($element))
+                    <li class="page-item disabled">
+                        <span class="page-link">{{ $element }}</span>
+                    </li>
+                @endif
+
+                {{-- روابط --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        <li class="page-item {{ $paginator->currentPage() == $page ? 'active' : '' }}">
+                            <a class="page-link" href="{{ $url }}">
+                                {{ $page }}
+                            </a>
+                        </li>
+                    @endforeach
+                @endif
             @endforeach
 
             {{-- التالي --}}
             <li class="page-item {{ $paginator->hasMorePages() ? '' : 'disabled' }}">
-                <a class="page-link"
-                   href="{{ $paginator->nextPageUrl() ?? '#' }}">
+                <a class="page-link" href="{{ $paginator->nextPageUrl() ?? '#' }}">
                     التالي
                 </a>
             </li>
