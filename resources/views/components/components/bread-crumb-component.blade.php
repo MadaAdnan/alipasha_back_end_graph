038555eb37d0d15ae2f3747caaf1b24a73524551
Adnan <mh.shamey@gmail.com>
2026-01-11 12:58:13 +0300

    add auth page

--- resources/views/components/components/bread-crumb-component.blade.php
+++ resources/views/components/components/bread-crumb-component.blade.php
@@ -10,7 +10,7 @@
         @foreach($categories as $category)
             <a href="{{$category['url']??'#'}}">@if(isset($category['icon'])) <i class="{{$category['icon']}}"></i> @endif {{$category['name']}}</a>
         @endforeach
-       
+
 
     </div>
 </div>
