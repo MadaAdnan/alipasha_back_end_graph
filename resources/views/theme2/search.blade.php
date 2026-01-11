087b3f0ce34be16500ba9ff992121142389b36a9
Adnan <mh.shamey@gmail.com>
2026-01-11 15:36:27 +0300

    add auth page

--- resources/views/theme2/search.blade.php
+++ resources/views/theme2/search.blade.php
@@ -15,7 +15,7 @@
             <div class="col-md-9 my-2">
                 @if($products->count()>0)
                     @foreach($products as $product)
-                        <x-components.estate-card-component :product="$product" class="d-none d-md-block"/>
+                        <x-components.estate-card-component :product="$product" class="d-none d-md-flex"/>
                     @endforeach
                 @else
 <div class="bg-white p-2 rounded">لا يوجد اى نتائج</div>
