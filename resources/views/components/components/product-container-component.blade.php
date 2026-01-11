db671a6833fe48034acfb911ecd4d47965b9c18a
Adnan <mh.shamey@gmail.com>
2026-01-11 10:01:09 +0300

    add auth page

--- resources/views/components/components/product-container-component.blade.php
+++ resources/views/components/components/product-container-component.blade.php
@@ -7,23 +7,23 @@
     $categoryName=$category?->name;
     $categoryId=$category?->id;
 @endphp
-<div class="col-md-9">
-    <div class="card card-body">
-        @if($categoryName!=null)
-            <div class="d-flex justify-content-between">
-                <h3 class="fs-6">{{$categoryName}}</h3>
-                @if($categoryId!=null)
-                    <a href="{{route('category.show',['id'=>$categoryId])}}" class="fs-6">مشاهدة المزيد</a>
-                @endif
-            </div>
-        @endif
-       @if($products)
-                <div class="row justify-content-center">
-                    @foreach($dataProducts as $product)
-                        <x-components.product-container-item-component :product="$product"/>
-                    @endforeach
 
-                </div>
-           @endif
-    </div>
+<div class="card card-body">
+    @if($categoryName!=null)
+        <div class="d-flex justify-content-between">
+            <h3 class="fs-6">{{$categoryName}}</h3>
+            @if($categoryId!=null)
+                <a href="{{route('category.show',['id'=>$categoryId])}}" class="fs-6">مشاهدة المزيد</a>
+            @endif
+        </div>
+    @endif
+    @if($products)
+        <div class="row justify-content-center">
+            @foreach($dataProducts as $product)
+                <x-components.product-container-item-component :product="$product"/>
+            @endforeach
+
+        </div>
+    @endif
 </div>
+
--- resources/views/theme2/index.blade.php
+++ resources/views/theme2/index.blade.php
@@ -5,12 +5,14 @@
             <div class="col-md-12">
             </div>
             <x-components.side-bar-category-component :categories="$categories"/>
+            <div class="col-md-9">
             @if($categoriesWithProducts)
                 @foreach($categoriesWithProducts as $category)
                     <x-components.product-container-component :products="$category->products" :category="$category"/>
                 @endforeach
 
                 @endif
+            </div>
         </div>
     </div>
 
