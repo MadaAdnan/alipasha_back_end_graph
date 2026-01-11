a82df240b6f687e1cc629df942228a7c1ddbff93
Adnan <mh.shamey@gmail.com>
2026-01-11 12:45:35 +0300

    add auth page

--- resources/views/components/components/price-component.blade.php
+++ resources/views/components/components/price-component.blade.php
@@ -6,8 +6,8 @@
 <div class="d-flex gap-1">
     @if($isDiscount)
         <del class="text-gray fs-7">{{$price}} $</del>
-        <span class="text-red-accent fw-bold fs-7">{{$discount}} $</span>
+        <span class="text-red-accent fw-bold fs-6">{{$discount}} $</span>
     @else
-        <span class="text-red-accent fw-bold fs-7">{{$price}} $</span>
+        <span class="text-red-accent fw-bold fs-6">{{$price}} $</span>
     @endif
 </div>
