5931a5b3139c9d62faf5e434e631d892c39effc3
Adnan <mh.shamey@gmail.com>
2026-01-13 12:31:08 +0300

    add auth page

--- resources/views/components/components/seller-name-component.blade.php
+++ resources/views/components/components/seller-name-component.blade.php
@@ -8,7 +8,7 @@
     <a class="cursor-pointer d-flex justify-content-start  align-items-center gap-1"
        href="{{route('seller.profile',$seller->id)}}">
         @if($image)
-            <img src="{{$image}}" class="rounded-circle img-fluid" alt="">
+            <img src="{{$image}}" class="rounded-circle img-seller" alt="">
         @else
             <i class="fa fa-shop {{$iconSize}}"></i>
         @endif
