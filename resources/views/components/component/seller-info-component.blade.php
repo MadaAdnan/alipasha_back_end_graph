d3ca562a43b578338a8a12c7e99d0535f5e23247
Adnan <mh.shamey@gmail.com>
2026-01-13 12:38:19 +0300

    add auth page

--- resources/views/components/component/seller-info-component.blade.php
+++ resources/views/components/component/seller-info-component.blade.php
@@ -5,5 +5,5 @@
    <h6 class="text-gray fw-bold my-1">معلومات المعلن</h6>
     <x-components.seller-name-component :seller="$seller" :image="$seller->getImage()"/>
     <span class="small text-muted"><i class="fa fa-location-dot"></i> {{$seller->address}}</span>
-    <div class="divider"></div>
+    <div class="divider my-1 "></div>
 </div>
