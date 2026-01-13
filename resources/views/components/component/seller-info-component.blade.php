1ea23e70182029f79493e86a84eed3e2b9f764ce
Adnan <mh.shamey@gmail.com>
2026-01-13 12:29:15 +0300

    add auth page

--- resources/views/components/component/seller-info-component.blade.php
+++ resources/views/components/component/seller-info-component.blade.php
@@ -1,4 +1,7 @@
+@props([
+    'seller'=>null
+])
 <div class="card card-body">
    <h4>معلومات المعلن</h4>
-    <x-components.seller-name-component/>
+    <x-components.seller-name-component :seller="$seller" :img="$seller->getImage()"/>
 </div>
--- resources/views/components/components/seller-name-component.blade.php
+++ resources/views/components/components/seller-name-component.blade.php
@@ -1,16 +1,22 @@
 @props([
     'seller'=>null,
     'class'=>'',
-    'iconSize'=>''
+    'iconSize'=>'',
+    'image'=>null
 ])
 @if($seller)
-<a class="cursor-pointer d-flex justify-content-start  align-items-center gap-1" href="{{route('seller.profile',$seller->id)}}">
-    <i class="fa fa-shop {{$iconSize}}"></i>
-  <span class="seller-name d-inline-block  {{$class??'fs-6'}}">  {{$seller->seller_name ?? $seller->name}}</span>
-    @if($seller->is_verified==true)
-        <i class="bi bi-patch-check-fill text-blue-accent {{$iconSize}}"></i>
-    @endif
-</a>
+    <a class="cursor-pointer d-flex justify-content-start  align-items-center gap-1"
+       href="{{route('seller.profile',$seller->id)}}">
+        @if($image)
+            <img src="{{$image}}" class="rounded-circle" alt="">
+        @else
+            <i class="fa fa-shop {{$iconSize}}"></i>
+        @endif
+        <span class="seller-name d-inline-block  {{$class??'fs-6'}}">  {{$seller->seller_name ?? $seller->name}}</span>
+        @if($seller->is_verified==true)
+            <i class="bi bi-patch-check-fill text-blue-accent {{$iconSize}}"></i>
+        @endif
+    </a>
 @else
-<span></span>
+    <span></span>
 @endif
