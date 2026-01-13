b505201f185e6b113c352aef8c658669dd634760
Adnan <mh.shamey@gmail.com>
2026-01-13 08:00:11 +0300

    add auth page

--- resources/views/components/components/product-container-component.blade.php
+++ resources/views/components/components/product-container-component.blade.php
@@ -14,7 +14,7 @@
         <div class="d-flex justify-content-between">
             <h3 class="fs-4 text-gray">{{$categoryName}} @if($count>0) ({{$count}}) @endif</h3>
             @if($categoryId!=null)
-                <a href="{{route('category.show',['id'=>$categoryId])}}" class="fs-4 text-gray">مشاهدة المزيد</a>
+                <a href="{{route('category.show',['id'=>$categoryId])}}" class="fs-4 text-green">مشاهدة المزيد</a>
             @endif
         </div>
 
