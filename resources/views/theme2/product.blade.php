2310a8fce2fefe089f3d7ef1bae9ef88f8f1b0aa
Adnan <mh.shamey@gmail.com>
2026-01-13 12:32:58 +0300

    add auth page

--- resources/views/theme2/product.blade.php
+++ resources/views/theme2/product.blade.php
@@ -1,7 +1,7 @@
 @extends('theme2.layouts.master')
 
 @section('content')
-    <div class="container mt-2">
+    <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-9">
                 <x-component.slider-component :items="$post->getImages('images')"/>
