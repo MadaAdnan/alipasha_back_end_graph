e1f07cf926a84176c93cc544f6f83e67a1e8ec8e
Adnan <mh.shamey@gmail.com>
2026-01-13 12:26:44 +0300

    add auth page

---
+++ resources/views/components/component/seller-info-component.blade.php
@@ -0,0 +1,4 @@
+<div class="card card-body">
+   <h4>معلومات المعلن</h4>
+    <x-components.seller-name-component/>
+</div>
--- resources/views/theme2/product.blade.php
+++ resources/views/theme2/product.blade.php
@@ -1,13 +1,15 @@
 @extends('theme2.layouts.master')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-2">
         <div class="row justify-content-center">
             <div class="col-md-9">
                 <x-component.slider-component :items="$post->getImages('images')"/>
 
             </div>
-            <div class="col-md-3"></div>
+            <div class="col-md-3">
+                <x-component.seller-info-component :seller="$post->user"/>
+            </div>
         </div>
     </div>
 
