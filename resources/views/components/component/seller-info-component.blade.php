cdd7bb0ebbaafa89fe42c3974332fe102e6b3b41
Adnan <mh.shamey@gmail.com>
2026-01-13 12:42:44 +0300

    add auth page

--- resources/views/components/component/seller-info-component.blade.php
+++ resources/views/components/component/seller-info-component.blade.php
@@ -6,4 +6,24 @@
     <x-components.seller-name-component :seller="$seller" :image="$seller->getImage()"/>
     <span class="small text-muted"><i class="fa fa-location-dot"></i> {{$seller->address}}</span>
     <div class="divider my-1 "></div>
+    <div class="d-flex justify-content-center">
+        <form action="{{route('communities.store')}}">
+            @csrf
+            <button class="btn btn-sm btn-outline-primary">
+                <i class="fa fa-chat"></i>
+                <span class="small d-none d-md-inline-block mx-1">
+                   تحدث معه
+                </span>
+            </button>
+        </form>
+        <form action="{{route('/')}}">
+            @csrf
+            <button class="btn btn-sm btn-outline-primary">
+                <i class="fa-brands fa-whatsapp"></i>
+                <span class="small d-none d-md-inline-block mx-1">
+                   واتس آب
+                </span>
+            </button>
+        </form>
+    </div>
 </div>
