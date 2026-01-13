0fe85fcdb62d730cd361ad3874bfedf3e667039c
Adnan <mh.shamey@gmail.com>
2026-01-13 12:56:00 +0300

    add auth page

--- resources/views/theme2/product.blade.php
+++ resources/views/theme2/product.blade.php
@@ -14,13 +14,16 @@
         <div class="row justify-content-center">
             <div class="col-md-9">
                 <h2 class="fw-bold">{{$post->name??$post->expert}}</h2>
-                <x-components.bread-crumb-component :first="$post->category?->name" urlFirst=" "
-                                                    iconFirst=" " :categories="[
+               <div class="d-flex">
+                   <x-components.bread-crumb-component :first="$post->category?->name" urlFirst=" "
+                                                       iconFirst=" " :categories="[
     ['name'=>$post->sub1?->name],
     ['name'=>$post->sub2?->name],
     ['name'=>$post->sub3?->name],
     ['name'=>$post->sub4?->name],
 ]"/>
+                   <x-components.price-component :price="$post->price" :discount="$post->discount" :isDiscount="$post->is_discount"/>
+               </div>
             </div>
             <div class="col-md-3"></div>
         </div>
