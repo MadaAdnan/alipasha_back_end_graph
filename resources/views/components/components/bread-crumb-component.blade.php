3f0a561cc4cc7e0bb729db479340a8e45b29eeff
Adnan <mh.shamey@gmail.com>
2026-01-13 12:54:22 +0300

    add auth page

--- resources/views/components/components/bread-crumb-component.blade.php
+++ resources/views/components/components/bread-crumb-component.blade.php
@@ -8,6 +8,9 @@
         <ol class="breadcrumb">
             <li class="breadcrumb-item"> <a href="{{$urlFirst}}"><i class="{{$iconFirst}}"></i> {{$first}}</a></li>
             @foreach($categories as $category)
+                @if(!isset($category['name']) || (isset($category['name']) && $category['name']==''))
+                    @continue
+                @endif
                 <li class="breadcrumb-item {{$category['class']??''}}" @if(isset($category['class']) && $category['class']=='active') aria-current="page" @endif><a href="{{$category['url']??'#'}}">@if(isset($category['icon'])) <i class="{{$category['icon']}}"></i> @endif {{$category['name']}}</a></li>
             @endforeach
         </ol>
