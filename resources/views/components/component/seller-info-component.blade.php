7deb6ae0d9d56fb9c141c9414c60444de15eda86
Adnan <mh.shamey@gmail.com>
2026-01-13 12:34:05 +0300

    add auth page

--- resources/views/components/component/seller-info-component.blade.php
+++ resources/views/components/component/seller-info-component.blade.php
@@ -2,6 +2,6 @@
     'seller'=>null
 ])
 <div class="card card-body">
-   <h6 class="text-gray">معلومات المعلن</h6>
+   <h6 class="text-gray fw-bold my-1">معلومات المعلن</h6>
     <x-components.seller-name-component :seller="$seller" :image="$seller->getImage()"/>
 </div>
