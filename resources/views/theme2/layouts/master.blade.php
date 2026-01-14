81c490505decaeeecdc5fdcb976c5ce50ed9a301
Adnan <mh.shamey@gmail.com>
2026-01-14 16:29:04 +0300

    add auth page

--- resources/views/theme2/layouts/master.blade.php
+++ resources/views/theme2/layouts/master.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="{{asset('theeme2/css/style.css?v='.rand(0,100))}}">
     <link rel="stylesheet" href="{{asset('theeme2/css/style-sm.css?v='.rand(0,100))}}">
     <title>علي باشا</title>
-    @yield('css')
+    @stack('css')
 </head>
 <body class="bg-gray">
 <x-layouts.nav-bar-component/>
@@ -24,6 +24,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
-@yield('js')
+@stack('js')
 </body>
 </html>
