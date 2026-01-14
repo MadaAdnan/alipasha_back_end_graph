919b46e9ef0685b50a875dce4108b49fbbc9ce12
Adnan <mh.shamey@gmail.com>
2026-01-14 15:44:23 +0300

    add auth page

--- resources/views/theme2/category.blade.php
+++ resources/views/theme2/category.blade.php
@@ -2,15 +2,19 @@
 @section('content')
     <div class="container my-2">
         <div class="row">
-            <div class="col-md-12">
-                <x-components.bread-crumb-component :categories="[]"/>
-            </div>
+
             <div class="col-md-3">
                 <x-components.side-bar-category-component :categories="$categories" :categoryId="$category->id"/>
             </div>
 
             <div class="col-md-9">
-                <x-component.top-add-post-component />
+                <div class="d-flex align-items-center">
+                    <x-components.bread-crumb-component :categories="[
+    ['name'=>$category->name]
+]"/>
+                    <x-component.top-add-post-component />
+                </div>
+
                         <x-components.product-container-component :products="$products->items()" :category="$category" :showMore="false"/>
 
             <x-components.paginator-component :paginator="$cloneProducts"/>
