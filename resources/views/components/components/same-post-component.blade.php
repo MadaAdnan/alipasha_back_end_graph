5fb1d027eb14619a40ba26be4f5794a4367ac7ec
Adnan <mh.shamey@gmail.com>
2026-01-15 11:07:38 +0300

    add auth page

--- resources/views/components/components/same-post-component.blade.php
+++ resources/views/components/components/same-post-component.blade.php
@@ -1,7 +1,7 @@
 <div class="mt-2">
     <div class="categories-header">
         <i class="fas fa-list"></i>
-        <span>منتجات مختارة</span>
+        <span>منتجات ذات صلة</span>
     </div>
 @foreach($products as $product)
     <x-components.product-container-item-component class="my-1" :product="$product"/>
