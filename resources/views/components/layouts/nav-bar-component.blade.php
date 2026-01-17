56380a852e8008104d6ef68843cc163547180eac
Adnan <mh.shamey@gmail.com>
2026-01-17 10:28:32 +0300

    add auth page

--- resources/views/components/layouts/nav-bar-component.blade.php
+++ resources/views/components/layouts/nav-bar-component.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-white">
     <div class="container">
-        <a class="navbar-brand" href="#"><img class="logo" src="{{$setting?->getFirstMediaUrl('logo')}}" alt=""></a>
+        <a class="navbar-brand" href="{{route('index')}}"><img class="logo" src="{{$setting?->getFirstMediaUrl('logo')}}" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
