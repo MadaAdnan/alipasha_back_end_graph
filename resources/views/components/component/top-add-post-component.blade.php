51adf26544e165310edfa83ae6deae29f601273d
Adnan <mh.shamey@gmail.com>
2026-01-17 10:40:02 +0300

    add auth page

--- resources/views/components/component/top-add-post-component.blade.php
+++ resources/views/components/component/top-add-post-component.blade.php
@@ -3,7 +3,7 @@
     <button class="btn-contact-us" data-bs-target="#exampleModalToggle" data-bs-toggle="modal"><i
             class="fa fa-comments"></i> <span>تواصل معنا</span>
     </button>
-    <a class="btn-add-post" href=""> <i class="fa fa-plus"></i> أضف إعلانك</a>
+    <a class="btn-add-post" href="{{url('/seller')}}"> <i class="fa fa-plus"></i> أضف إعلانك</a>
 
 
     {{--    Modal--}}
@@ -32,25 +32,6 @@
                         <button type="button" class="btn-red-accent rounded px-2 py-1" data-bs-dismiss="modal" ><i class="fa fa-close"></i> <span class="d-none d-md-inline">إغلاق</span></button>
 
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
-         tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modal 2</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    Hide this modal and show the first with the button below.
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to
-                        first
-                    </button>
                 </div>
             </div>
         </div>
