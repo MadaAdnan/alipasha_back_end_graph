5d311b1ca67b74eaaf53bb6e668d71027b297d20
Adnan <mh.shamey@gmail.com>
2026-01-18 09:56:16 +0300

    add auth page

--- resources/views/components/components/paginator-component.blade.php
+++ resources/views/components/components/paginator-component.blade.php
@@ -10,13 +10,13 @@
                 {{-- السابق --}}
                 <span class=" {{ $paginator->onFirstPage() ? 'disabled' : 'text-red-accent' }}">
                     <a class=""
-                       href="{{ $paginator->previousPageUrl() ?? '#' }}">
+                       href="{{ $paginator->withQueryString()->previousPageUrl() ?? '#' }}">
                         <i class=" {{ $paginator->onFirstPage() ? 'disabled' : 'text-red-accent' }} fa fa-angle-right"></i>
                     </a>
                 </span>
 
                 {{-- الأرقام --}}
-                @foreach ($paginator->links()->elements[0] ?? [] as $page => $url)
+                @foreach ($paginator->withQueryString()->links()->elements[0] ?? [] as $page => $url)
                     <span class="mx-1 btn rounded py-1 px-2  {{ $paginator->currentPage() == $page ? 'btn-red-accent' : 'bg-white' }}">
                         <a class="" href="{{ $url }}">
                             {{ $page }}
@@ -27,7 +27,7 @@
                 {{-- التالي --}}
                 <span class=" {{ $paginator->hasMorePages() ? 'text-red-accent' : 'disabled' }}">
                     <a class=""
-                       href="{{ $paginator->nextPageUrl() ?? '#' }}">
+                       href="{{ $paginator->withQueryString()->nextPageUrl() ?? '#' }}">
 
                           <i class=" {{ $paginator->hasMorePages() ? 'text-red-accent' : 'disabled' }} fa fa-angle-left"></i>
                     </a>
