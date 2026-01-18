9e3df3d763f94433531a2298732d11efa026e1e9
Adnan <mh.shamey@gmail.com>
2026-01-18 10:17:32 +0300

    add auth page

--- resources/views/components/components/product-container-component.blade.php
+++ resources/views/components/components/product-container-component.blade.php
@@ -3,22 +3,30 @@
     'category'=>null,
     'count'=>0,
     'showMore'=>true,
+    'categoryName'=>''
 ])
 @php
     $dataProducts=$products;
-    $categoryName=$category?->name;
+    if($category!=null){
+         $categoryName=$category?->name;
+    }
+
     $categoryId=$category?->id;
 @endphp
 
 <div class="card card-body my-2 ">
-    @if($categoryName!=null)
+    @if($category!=null)
         <div class="d-flex justify-content-between">
             <h3 class="fs-4 text-gray">{{$categoryName}} @if($count>0) <span class="fs-6 text-muted">({{$count}})</span> @endif</h3>
             @if($showMore)
                 <a href="{{route('category.show',['id'=>$categoryId])}}" class="fs-6 text-green">مشاهدة المزيد</a>
             @endif
         </div>
+@else
+        <div class="d-flex justify-content-between">
+            <h3 class="fs-4 text-gray">{{$categoryName}} @if($count>0) <span class="fs-6 text-muted">({{$count}})</span> @endif</h3>
 
+        </div>
     @endif
     @if($dataProducts)
         <div class="row g-2">
