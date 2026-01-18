f543c5510af8d29f08f3abd405c23640c5840ff6
Adnan <mh.shamey@gmail.com>
2026-01-18 09:59:25 +0300

    add auth page

--- resources/views/theme2/market.blade.php
+++ resources/views/theme2/market.blade.php
@@ -5,11 +5,11 @@
     <div class="row">
         <div class="col-md-3">
 
-            <x-components.side-bar-category-market-component :categories="$categories" :store="$store"/>
+            <x-components.side-bar-category-market-component :categories="$categories" :store="$store" />
         </div>
         <div class="col-md-9">
             <x-components.product-container-component :products="$products->items()"
-                                                     />
+                                                     :categoryName="($categories->where('id',request()->get('category_id'))->first()?->name)?? 'جميع المنتجات'"/>
 
             <x-components.paginator-component :paginator="$products"/>
         </div>
