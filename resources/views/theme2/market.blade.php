8387173db6759c9cc5f53ae5aa222528cb9705e6
Adnan <mh.shamey@gmail.com>
2026-01-18 09:44:49 +0300

    add auth page

--- resources/views/theme2/market.blade.php
+++ resources/views/theme2/market.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-3">
 
-            <x-components.side-bar-category-market-component :categories="{{$categories}}" :store="{{$store}}"/>
+            <x-components.side-bar-category-market-component :categories="{{$categories??[]}}" :store="{{$store}}"/>
         </div>
         <div class="col-md-9">
             <x-components.product-container-component :products="$products"/>
