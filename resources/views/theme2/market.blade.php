097a5c9018dc719bc1595e060aa843f9b4ac59e5
Adnan <mh.shamey@gmail.com>
2026-01-18 10:24:43 +0300

    add auth page

--- resources/views/theme2/market.blade.php
+++ resources/views/theme2/market.blade.php
@@ -1,25 +1,33 @@
 @extends('theme2.layouts.master')
 
 @section('content')
-<div class="container my-5">
-    <div class="row justify-content-center">
-<div class="col-md-2">
-    <x-components.small-widget-product-detail-component class="bg-white border rounded p-2" title="متابعين" icon="fa-regular fa-thumbs-up" :info="$store->followers_count"/>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-md-2">
+                <x-components.small-widget-product-detail-component class="bg-white border rounded p-2" title="متابعين"
+                                                                    icon="fa-regular fa-thumbs-up"
+                                                                    :info="$store->followers_count"/>
 
-</div>
-    </div>
-    <div class="row">
-        <div class="col-md-3">
+            </div>
+            <div class="col-md-2">
+                <x-components.small-widget-product-detail-component class="bg-white border rounded p-2" title="المنتجات"
+                                                                    icon="fa-solid fa-boxes-stacked"
+                                                                    :info="$store->products_count"/>
 
-            <x-components.side-bar-category-market-component :categories="$categories" :store="$store" />
+            </div>
         </div>
-        <div class="col-md-9">
-            <x-components.product-container-component :products="$products->items()"
-                                                     :categoryName="($categories->where('id',request()->get('category_id'))->first()?->name)?? 'جميع المنتجات'"/>
+        <div class="row">
+            <div class="col-md-3">
 
-            <x-components.paginator-component :paginator="$products"/>
+                <x-components.side-bar-category-market-component :categories="$categories" :store="$store"/>
+            </div>
+            <div class="col-md-9">
+                <x-components.product-container-component :products="$products->items()"
+                                                          :categoryName="($categories->where('id',request()->get('category_id'))->first()?->name)?? 'جميع المنتجات'"/>
+
+                <x-components.paginator-component :paginator="$products"/>
+            </div>
         </div>
     </div>
-</div>
 
 @endsection
