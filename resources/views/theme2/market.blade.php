723426ad998527fd701f600845001b25ea11b328
Adnan <mh.shamey@gmail.com>
2026-01-18 10:27:23 +0300

    add auth page

--- resources/views/theme2/market.blade.php
+++ resources/views/theme2/market.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container my-5">
-        <div class="row justify-content-between">
+        <div class="row justify-content-between align-items-center">
             <div class="col-md-9">
                 <div class="row justify-content-center">
                     <div class="col-md-2">
