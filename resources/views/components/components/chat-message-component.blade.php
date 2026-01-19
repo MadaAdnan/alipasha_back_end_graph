9f22a8ff51529294f5592e51156a88f3422167f6
Adnan <mh.shamey@gmail.com>
2026-01-19 11:07:05 +0300

    add auth page

--- resources/views/components/components/chat-message-component.blade.php
+++ resources/views/components/components/chat-message-component.blade.php
@@ -7,16 +7,21 @@
 @endphp
 
 @if($user->id==auth()->id())
-    <div class="message d-flex gap-1">
+    <div class="d-flex flex-column gap-1">
+    <div class="message d-flex flex-row-reverse gap-1">
         <img src="{{$user->getImage()}}" alt="{{$user->name}}">
-        <p class="bg-white rounded text-wrap">{{$message->body}}</p>
+        <p class="bg-white rounded text-wrap p-2">{{$message->body}}</p>
+
+    </div>
         <span>{{$message->created_at->diffForHumans()}}</span>
     </div>
 @else
-    <div class="message d-flex gap-1">
+    <div class="d-flex flex-column gap-1">
+        <div class="message d-flex  gap-1">
+            <img src="{{$user->getImage()}}" alt="{{$user->name}}">
+            <p class="bg-white rounded text-wrap p-2">{{$message->body}}</p>
 
-        <p class="bg-white rounded text-wrap">{{$message->body}}</p>
+        </div>
         <span>{{$message->created_at->diffForHumans()}}</span>
-        <img src="{{$user->getImage()}}" alt="{{$user->name}}">
     </div>
 @endif
