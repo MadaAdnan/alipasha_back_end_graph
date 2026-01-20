0bad0e66b78a989108611d3b00487c1de6cc51fb
Adnan <mh.shamey@gmail.com>
2026-01-20 11:52:59 +0300

    add auth page

--- resources/views/theme2/product.blade.php
+++ resources/views/theme2/product.blade.php
@@ -9,7 +9,7 @@
             </div>
             <div class="col-md-3">
                 <x-component.seller-info-component :seller="$post->user"/>
-                <x-components.social-seller-component/>
+                <x-components.social-seller-component :store="$post->user"/>
             </div>
         </div>
         <div class="row justify-content-center">
