cddf0229f2763c155339cf4933638d87663e685d
Adnan <mh.shamey@gmail.com>
2026-01-20 10:34:46 +0300

    add auth page

--- resources/views/components/components/chat-message-component.blade.php
+++ resources/views/components/components/chat-message-component.blade.php
@@ -8,7 +8,7 @@
 
 @if($user->id==auth()->id())
     <div class="d-flex flex-column gap-1  justify-content-start align-items-end ">
-    <div class="message d-flex flex-row-reverse gap-1 ">
+    <div class="message d-flex  gap-1 ">
         <img src="{{$user->getImage()}}" alt="{{$user->name}}">
         <p class=" rounded text-wrap p-2">{{$message->body}}</p>
 
