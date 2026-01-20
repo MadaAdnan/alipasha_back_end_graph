bb2fb362a8a74dcc503116c3661a8c5732214adc
Adnan <mh.shamey@gmail.com>
2026-01-20 11:44:25 +0300

    add auth page

--- resources/views/components/components/social-seller-component.blade.php
+++ resources/views/components/components/social-seller-component.blade.php
@@ -1,7 +1,7 @@
 @props([
     'store'=>null
 ])
-{{dd($store->social)}}
+
 <div class="social-media-section text-center py-4">
     <h5 class="mb-3">تابعنا على</h5>
     <div class="social-links d-flex justify-content-center gap-3 flex-wrap">
@@ -42,7 +42,7 @@
         @endif
     </div>
     <p class="text-muted mt-3 mb-0">
-
+{{$store->slogan}}
     </p>
 </div>
 
