d118b51ff9306ba86f8e090953b1fa686aab1e60
Adnan <mh.shamey@gmail.com>
2026-01-21 09:31:02 +0300

    add auth page

--- resources/views/theme2/product.blade.php
+++ resources/views/theme2/product.blade.php
@@ -9,63 +9,71 @@
             </div>
             <div class="col-md-3">
                 <x-component.seller-info-component :seller="$post->user"/>
-                  @if($post->user->plans()->whereNot('duration','free')->exists())
-                <x-components.social-seller-component :store="$post->user"/>
-                   @endif
+                @if($post->user->plans()->whereNot('duration','free')->exists())
+                    <x-components.social-seller-component :store="$post->user"/>
+                @endif
             </div>
         </div>
         <div class="row justify-content-center">
             <div class="col-md-9">
                 <h2 class="fw-bold">{{$post->name??$post->expert}}</h2>
-               <div class="d-flex  align-items-center px-1 py-1">
-                   <x-components.bread-crumb-component class="flex-grow-1 pt-3 px-2" :first="$post->category?->name" urlFirst=" "
-                                                       iconFirst=" " :categories="[
+                <div class="d-flex  align-items-center px-1 py-1">
+                    <x-components.bread-crumb-component class="flex-grow-1 pt-3 px-2" :first="$post->category?->name"
+                                                        urlFirst=" "
+                                                        iconFirst=" " :categories="[
     ['name'=>$post->sub1?->name],
     ['name'=>$post->sub2?->name],
     ['name'=>$post->sub3?->name],
     ['name'=>$post->sub4?->name],
 ]"/>
-                   <x-components.price-component class="bg-transparent " :price="$post->price" :discount="$post->discount" :isDiscount="$post->is_discount"/>
-               </div>
+                    <x-components.price-component class="bg-transparent " :price="$post->price"
+                                                  :discount="$post->discount" :isDiscount="$post->is_discount"/>
+                </div>
                 <div class="row justify-content-center align-items-center my-1">
                     @if($post->video!=null && Str::startsWith($post->video ,"https://"))
 
-                    <div class="col-md-2">
-                        <x-components.play-video-component :product="$post"/>
+                        <div class="col-md-2">
+                            <x-components.play-video-component :product="$post"/>
+                        </div>
+
+                    @endif
+                    <div class="col-md-2 col-6">
+                        <x-components.small-widget-product-detail-component class="border rounded p-2 bg-white"
+                                                                            title="تاريخ النشر"
+                                                                            icon="fas fa-calendar d-block fs-4 text-gray"
+                                                                            info="{{$post->created_at?->format('Y-m-d')}}"/>
                     </div>
-
-
-                @endif
-                        <div class="col-md-2 col-6">
-                            <x-components.small-widget-product-detail-component class="border rounded p-2 bg-white" title="تاريخ النشر" icon="fas fa-calendar d-block fs-4 text-gray" info="{{$post->created_at?->format('Y-m-d')}}"/>
-                        </div>
-                        <div class="col-md-2 col-6">
-                            <x-components.small-widget-product-detail-component class="border rounded p-2 bg-white" title="عدد المشاهدات" icon="fa fa-eye d-block fs-4 text-gray" info="{{$post->views_count}}"/>
-                        </div>
+                    <div class="col-md-2 col-6">
+                        <x-components.small-widget-product-detail-component class="border rounded p-2 bg-white"
+                                                                            title="عدد المشاهدات"
+                                                                            icon="fa fa-eye d-block fs-4 text-gray"
+                                                                            info="{{$post->views_count}}"/>
+                    </div>
                 </div>
                 <div class="bg-white p-3">
-                   <div class="d-flex justify-content-between">
-                       <h4 class="text-black fw-bold">التفاصيل</h4>
-                       <div class="">
-                           @if(auth()->check())
-                           <x-components.add-to-cart-component :post="$post"/>
-                               <x-components.like-btn-component class="mx-2" :post="$post"/>
-                           @endif
+                    <div class="d-flex justify-content-between">
+                        <h4 class="text-black fw-bold">التفاصيل</h4>
+                        <div class="">
+                            @if(auth()->check())
+                                <x-components.add-to-cart-component :post="$post"/>
+                                <x-components.like-btn-component class="mx-2" :post="$post"/>
+                            @endif
 
-                        <x-components.share-btn-component :url="route('posts.show', $post->id)"/>
+                            <x-components.share-btn-component :url="route('posts.show', $post->id)"/>
 
-                       </div>
-                   </div>
+                        </div>
+                    </div>
                     <p class="lead text-justify">{!! $post->info !!}</p>
                     @if($post->colors->count()>0)
-                    <div class="d-flex justify-content-between info-data">
-                        <span><i class="fas fa-palette"></i> الألوان المتوفرة</span>
-                       <div class="d-flex">
-                           @foreach($post->colors as $color)
-                               <span class="p-2 rounded-circle color" style="background-color: {{$color->code}}"></span>
-                           @endforeach
-                       </div>
-                    </div>
+                        <div class="d-flex justify-content-between info-data">
+                            <span><i class="fas fa-palette"></i> الألوان المتوفرة</span>
+                            <div class="d-flex">
+                                @foreach($post->colors as $color)
+                                    <span class="p-2 rounded-circle color"
+                                          style="background-color: {{$color->code}}"></span>
+                                @endforeach
+                            </div>
+                        </div>
                     @endif
                     <div class="d-flex justify-content-between info-data">
                         <span><i class="fas fa-map-marker-alt"></i> المحافظة</span>
@@ -81,6 +89,11 @@
                     </div>
                 </div>
             </div>
+            <div class="col-md-3">
+                <x-components.social-seller-component :seller="$post->user"/>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-md-9">
 
                 <x-components.same-post-component :category="$post->sub1_id"/>
