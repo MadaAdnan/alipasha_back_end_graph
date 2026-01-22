7abb6733f28e125cad6566122a0caa3bfb9df6e0
Adnan <mh.shamey@gmail.com>
2026-01-22 11:33:22 +0300

    add auth page

--- resources/views/theme2/service.blade.php
+++ resources/views/theme2/service.blade.php
@@ -17,7 +17,7 @@
 
 
                     <h3 class="fw-bold mb-3">
-                        {{$servic->name}}
+                        {{$service->name}}
                     </h3>
 
                     <p class="service-description">
