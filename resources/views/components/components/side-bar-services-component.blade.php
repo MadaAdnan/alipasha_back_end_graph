b4e6f916cbb346d76c93e0a8c000820be39c2ab3
Adnan <mh.shamey@gmail.com>
2026-01-22 10:21:13 +0300

    add auth page

--- resources/views/components/components/side-bar-services-component.blade.php
+++ resources/views/components/components/side-bar-services-component.blade.php
@@ -10,11 +10,13 @@
         <span>التصنيفات</span>
     </div>
     <ul class="categories-list">
+        @if($services)
         @foreach($services as $service)
             <li class="category-item-wrapper">
                 <x-components.side-bar-services-item-component :service="$service"/>
             </li>
         @endforeach
+        @endif
     </ul>
 </div>
 
