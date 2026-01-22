5438493e466203f00b0d80b5f4d6b2485ba6da16
Adnan <mh.shamey@gmail.com>
2026-01-22 11:01:17 +0300

    add auth page

--- resources/views/theme2/services.blade.php
+++ resources/views/theme2/services.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <div class="container mb-2 mt-5">
         <div class="row justify-content-center">
+            <div class="col-md-3"></div>
         <div class="col-md-9">
             <div class="row justify-content-center">
                 <div class="col-md-4 col-6">
