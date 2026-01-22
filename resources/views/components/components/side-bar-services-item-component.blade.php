7588c105f58328af8c000cb8ab751049b5ad00e1
Adnan <mh.shamey@gmail.com>
2026-01-22 11:16:26 +0300

    add auth page

--- resources/views/components/components/side-bar-services-item-component.blade.php
+++ resources/views/components/components/side-bar-services-item-component.blade.php
@@ -6,10 +6,10 @@
 
 
     $isActive = $serviceId != null && $service?->id == $serviceId;
-    dd($serviceId,$service?->id)
+
 @endphp
 
-<a class="category-item-link {{ $isActive ? 'active' : '' }}" href="{{route('services.index',['category'=>$service?->id])}}">
+<a class="category-item-link {{ $isActive ? 'active' : '' }}" href="{{route('services.index',['category_id'=>$service?->id])}}">
     <div class="category-item-icon">
         <i class="fas fa-tag"></i>
         <span>{{ $service?->name }}</span>
