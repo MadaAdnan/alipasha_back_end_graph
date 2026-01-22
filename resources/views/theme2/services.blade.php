3530e131ab1d891eb3d612714d20fd30b8b340f9
Adnan <mh.shamey@gmail.com>
2026-01-22 10:22:13 +0300

    add auth page

--- resources/views/theme2/services.blade.php
+++ resources/views/theme2/services.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
 
             <div class="col-md-3  my-2 sticky-col">
-<x-components.side-bar-services-component :services="$services"/>
+<x-components.side-bar-services-component :services="$categories"/>
             </div>
 
 @endsection
