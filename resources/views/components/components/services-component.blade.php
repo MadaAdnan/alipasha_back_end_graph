ecd826d8113d4b6fe8332c76cec7f63c532f142e
Adnan <mh.shamey@gmail.com>
2026-01-22 10:43:10 +0300

    add auth page

--- resources/views/components/components/services-component.blade.php
+++ resources/views/components/components/services-component.blade.php
@@ -21,7 +21,7 @@
                 </div>
             </div>
 
-            <a href="#" class="btn bg-gradient btn-sm">
+            <a href="#" class="btn bg-gradient text-white btn-sm">
                 استعراض <i class="fa-solid fa-arrow-left"></i>
             </a>
         </div>
