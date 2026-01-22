933b284e3671176bd98cc34ac911f742bd94d8d1
Adnan <mh.shamey@gmail.com>
2026-01-22 13:18:00 +0300

    add auth page

--- lang/vendor/filament-shield/ar/filament-shield.php
+++ lang/vendor/filament-shield/ar/filament-shield.php
@@ -77,6 +77,6 @@ return [
         'restore_any' => 'استرجاع الكل',
         'replicate' => 'استنساخ',
         'add_balance' => 'إضافة وسحب الرصيد',
-        'SeeProductChart'=>'عرض المخطط البياني للمنتجات'
+        'see_product_chart'=>'عرض المخطط البياني للمنتجات'
     ],
 ];
