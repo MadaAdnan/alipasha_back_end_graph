42502013c7a0723f1cd41c7ad7976a25c9b16d6d
Adnan <mh.shamey@gmail.com>
2026-01-22 10:56:48 +0300

    add auth page

--- resources/views/theme2/services.blade.php
+++ resources/views/theme2/services.blade.php
@@ -1,15 +1,19 @@
 @extends('theme2.layouts.master')
 @section('content')
     <div class="container mb-2 mt-5">
-        <div class="row">
-            <div class="col-md-2 col-6">
-                <x-components.small-widget-product-detail-component title="الخدمات المنشورة" icon="" info="{{$services_count}}"/>
-            </div>
-            <div class="col-md-2 col-6">
-                <x-components.small-widget-product-detail-component title="عدد المشاهدات" icon="" info="{{$views}}"/>
-            </div>
-            <div class="col-md-2 col-6">
-                <x-components.small-widget-product-detail-component title="المزودين بالمعلومات" icon="" info="{{$sellers}}"/>
+        <div class="row justify-content-center">
+            <div class="col-md-9">
+                <div class="row justify-content-center">
+                    <div class="col-md-2 col-6">
+                        <x-components.small-widget-product-detail-component title="الخدمات المنشورة" icon="" info="{{$services_count}}"/>
+                    </div>
+                    <div class="col-md-2 col-6">
+                        <x-components.small-widget-product-detail-component title="عدد المشاهدات" icon="" info="{{$views}}"/>
+                    </div>
+                    <div class="col-md-2 col-6">
+                        <x-components.small-widget-product-detail-component title="المزودين بالمعلومات" icon="" info="{{$sellers}}"/>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
