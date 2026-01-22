284e785260fa888367df4b330d4d1c0552e3e93a
Adnan <mh.shamey@gmail.com>
2026-01-22 11:38:41 +0300

    add auth page

--- resources/views/theme2/service.blade.php
+++ resources/views/theme2/service.blade.php
@@ -4,8 +4,7 @@
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-md-9">
-                <x-components.bread-crumb-component class="rounded px-3 py-2 rounded bg-white my-2"  :categoris="[
-
+                <x-components.bread-crumb-component class="rounded px-3 py-2 rounded bg-white my-2"  :categories="[
                         ['name' => 'الخدمات', 'url' => route('services.index')],
                         ['name' => $service->category?->name, 'url' => route('services.index', ['category_id'=>$service->category?->id])],
                         ['name' => $service->name, 'url' => null],
