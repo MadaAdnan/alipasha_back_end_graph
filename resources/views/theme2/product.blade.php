836aa4a9d8f62c26b937f13cf38055dcc6dbef2c
Adnan <mh.shamey@gmail.com>
2026-01-22 09:50:29 +0300

    add auth page

--- resources/views/theme2/product.blade.php
+++ resources/views/theme2/product.blade.php
@@ -51,7 +51,7 @@
                     </div>
                 </div>
                 <div class="categories-header">
-                    <i class="fas fa-comments"></i>
+                    <i class="fas fa-info"></i>
                     <span>التفاصيل</span>
                 </div>
                 <div class="bg-white p-3">
