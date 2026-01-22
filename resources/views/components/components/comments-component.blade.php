75efe50461a615d05944f57a467af7dddfa870ee
Adnan <mh.shamey@gmail.com>
2026-01-22 09:03:26 +0300

    add auth page

--- resources/views/components/components/comments-component.blade.php
+++ resources/views/components/components/comments-component.blade.php
@@ -20,7 +20,7 @@
                 {{--<input type="text"
                        class="form-control flex-grow-1"
                        placeholder="اكتب تعليقًا...">--}}
-                <x-form.input-component name="comment" wrapperClass="flex-grow-1" placeholder="اكتب تعليقًا..." required/>
+                <x-form.input-component name="comment" wrapperClass="flex-grow-1 pt-1" placeholder="اكتب تعليقًا..." required/>
                 <button type="submit" class="btn btn-red-accent rounded-5 px-4 ">
                     <i class="fas fa-paper-plane"></i>
                 </button>
