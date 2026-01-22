8cdabe3edcffa034b753fed817e0b7a8163b1ea2
Adnan <mh.shamey@gmail.com>
2026-01-22 12:12:45 +0300

    add auth page

--- resources/views/components/layouts/nav-bar-component.blade.php
+++ resources/views/components/layouts/nav-bar-component.blade.php
@@ -14,19 +14,19 @@
                 @endif
             @endif
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        {{--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        </button>--}}
+        <div class="collapse navbar-collapse" id="navbarSupportedContent1">
 
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li>
+                {{--<li>
                     <form class="d-flex position-relative" role="search">
                         <input class="form-control me-2" type="search" placeholder="بحث" aria-label="Search"/>
 
                         <button class="transparent position-absolute search-icon-navbar"><i class="fa fa-search"></i></button>
                     </form>
-                </li>
+                </li>--}}
 @guest()
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('login')}}"><i class="fa fa-user"></i> تسجيل الدخول </a>
