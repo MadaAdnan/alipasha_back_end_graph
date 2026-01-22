3ed833a28665cff4191072b42ded4530e12f8dd4
Adnan <mh.shamey@gmail.com>
2026-01-22 11:10:08 +0300

    add auth page

--- resources/views/theme2/services.blade.php
+++ resources/views/theme2/services.blade.php
@@ -34,7 +34,7 @@
                 <x-components.services-component :services="$services->items()"/>
                 <x-components.paginator-component :paginator="$services"/>
                 @else
-                    <div class="border border-1 rounded w-100 px-2 py-1 mt-1 bg-white">
+                    <div class="border border-1 rounded w-100 px-2 py-1 mt-1 bg-white fs-4">
                         لا يوجد خدمات
                     </div>
                 @endif
