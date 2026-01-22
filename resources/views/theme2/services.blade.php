ac2921d90ef84de606e00e200c4f07ef23878357
Adnan <mh.shamey@gmail.com>
2026-01-22 11:03:17 +0300

    add auth page

--- resources/views/theme2/services.blade.php
+++ resources/views/theme2/services.blade.php
@@ -6,7 +6,9 @@
         <div class="col-md-9">
             <div class="row justify-content-center">
                 <div class="col-md-4 col-6">
-                    <x-components.small-widget-product-detail-component title="الخدمات المنشورة" icon="fa-solid fa-file-signature" info="{{$services_count}}"/>
+                    <x-components.small-widget-product-detail-component classTitle=""
+                                                                        classInfo=""
+                                                                        class="bg-white rounded my-1" title="الخدمات المنشورة" icon="fa-solid fa-file-signature" info="{{$services_count}}"/>
                 </div>
                 <div class="col-md-4 col-6">
                     <x-components.small-widget-product-detail-component title="عدد المشاهدات" icon="fa fa-eye" info="{{$views}}"/>
