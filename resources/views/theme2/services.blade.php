14010fad4b0a291aa029e1da73f69b5b109b9443
Adnan <mh.shamey@gmail.com>
2026-01-22 11:04:53 +0300

    add auth page

--- resources/views/theme2/services.blade.php
+++ resources/views/theme2/services.blade.php
@@ -7,14 +7,18 @@
             <div class="row justify-content-center">
                 <div class="col-md-4 col-6">
                     <x-components.small-widget-product-detail-component classTitle="fs-4"
-                                                                        classInfo="lead"
+                                                                        classInfo="fs-4"
                                                                         class="bg-white rounded my-1 py-2" title="الخدمات المنشورة" icon="fa-solid fa-file-signature fs-4" info="{{$services_count}}"/>
                 </div>
                 <div class="col-md-4 col-6">
-                    <x-components.small-widget-product-detail-component title="عدد المشاهدات" icon="fa fa-eye" info="{{$views}}"/>
+                    <x-components.small-widget-product-detail-component title="عدد المشاهدات" icon="fa fa-eye" info="{{$views}}" classTitle="fs-4"
+                                                                        classInfo="fs-4"
+                                                                        class="bg-white rounded my-1 py-2"/>
                 </div>
                 <div class="col-md-4 col-6">
-                    <x-components.small-widget-product-detail-component title="المزودين بالمعلومات" icon="" info="{{$sellers}}"/>
+                    <x-components.small-widget-product-detail-component title="المزودين بالمعلومات" icon="" info="{{$sellers}}" classTitle="fs-4"
+                                                                        classInfo="fs-4"
+                                                                        class="bg-white rounded my-1 py-2"/>
                 </div>
             </div>
         </div>
