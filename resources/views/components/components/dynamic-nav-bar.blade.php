65c9f8b9459bb89f097b13aa0ccd7a65a2c43298
Adnan <mh.shamey@gmail.com>
2026-01-24 12:13:20 +0300

    add auth page

--- resources/views/components/components/dynamic-nav-bar.blade.php
+++ resources/views/components/components/dynamic-nav-bar.blade.php
@@ -1,10 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-white border-top">
     <div class="container">
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 @foreach($navs as $nav)
