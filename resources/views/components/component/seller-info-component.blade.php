623c5431145395d066365bf4fc94c38bbe33d136
Adnan <mh.shamey@gmail.com>
2026-01-24 10:08:39 +0300

    add auth page

--- resources/views/components/component/seller-info-component.blade.php
+++ resources/views/components/component/seller-info-component.blade.php
@@ -25,6 +25,18 @@
                 </span>
             </button>
         </form>
+        @else
+            <button class="btn-follow rounded" onclick="showToast('يرجى تسجيل الدخول اولاً')">
+                <i class="fa fa-bell"></i>
+                <span class="small d-none d-md-inline-block mx-1 ">
+                    @if($isFollowing) تتابعه
+                    @else
+                        متابعة
+                    @endif
+
+
+                </span>
+            </button>
             @endauth
     </div>
     <x-components.seller-name-component :seller="$seller" :image="$seller->getImage()"/>
