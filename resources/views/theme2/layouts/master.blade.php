973a3f33680a7c717320b5820bf2f1a91f903aef
Adnan <mh.shamey@gmail.com>
2026-01-24 09:41:51 +0300

    add auth page

--- resources/views/theme2/layouts/master.blade.php
+++ resources/views/theme2/layouts/master.blade.php
@@ -8,8 +8,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+          integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{asset('theeme2/css/style.css?v='.rand(0,100))}}">
     <link rel="stylesheet" href="{{asset('theeme2/css/style-sm.css?v='.rand(0,100))}}">
@@ -24,7 +27,8 @@
 </head>
 <body class="bg-gray">
 <x-layouts.nav-bar-component/>
-</body><x-components.dynamic-nav-bar/>
+
+<x-components.dynamic-nav-bar/>
 @yield('content')
 
 
@@ -32,12 +36,16 @@
 
 @auth
     <x-components.link-chat-component/>
-<script>
-    const token=localStorage.getItem('token')
-    if(token === '' || token == null){
-        localStorage.setItem('token',`{{auth()->user()->createToken('user')->plainTextToken}}`)
-    }
-</script>
+
+@endauth
+</body>
+@auth
+    <script>
+        const token = localStorage.getItem('token')
+        if (token === '' || token == null) {
+            localStorage.setItem('token', `{{auth()->user()->createToken('user')->plainTextToken}}`)
+        }
+    </script>
 @endauth
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
         integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y"
@@ -53,5 +61,5 @@
 
 @stack('js')
 
-</body>
+
 </html>
