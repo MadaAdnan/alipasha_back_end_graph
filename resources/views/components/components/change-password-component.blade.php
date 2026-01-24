7cac3efab801186b5fc9f270ed1fb48436641cef
Adnan <mh.shamey@gmail.com>
2026-01-24 10:55:23 +0300

    add auth page

--- resources/views/components/components/change-password-component.blade.php
+++ resources/views/components/components/change-password-component.blade.php
@@ -3,9 +3,13 @@
         <i class="fas fa-sliders-h"></i>
         <span>تغيير كلمة المرور</span>
     </div>
-
-       <x-form.input-password-component name="old_password" required label="كلمة المرور القديمة" :isRounded="true" id="old_password"/>
-       <x-form.input-password-component name="new_password" required label="كلمة المرور الجديدة" :isRounded="true" id="new_password"/>
-       <x-form.input-password-component name="confirm_password" required label="تأكيد كلمة المرور " :isRounded="true" id="confirm_password"/>
-
+    <form action="{{ route('change-password') }}" method="POST">
+        <x-form.input-password-component name="old_password" required label="كلمة المرور القديمة"
+                                         placeholder="كلمة المرور القديمة" :isRounded="true" id="old_password"/>
+        <x-form.input-password-component name="new_password" required label="كلمة المرور الجديدة"
+                                         placeholder="كلمة المرور الجديدة" :isRounded="true" id="new_password"/>
+        <x-form.input-password-component name="confirm_password" required label="تأكيد كلمة المرور "
+                                         placeholder="تأكيد كلمة المرور " :isRounded="true" id="confirm_password"/>
+        <button class="btn btn-red-accent">تغيير</button>
+    </form>
 </div>
