3c6804c2952f32dccaeafbeee54f14fd3ec1d033
Adnan <mh.shamey@gmail.com>
2026-01-24 12:13:58 +0300

    add auth page

--- resources/views/components/layouts/nav-bar-component.blade.php
+++ resources/views/components/layouts/nav-bar-component.blade.php
@@ -14,10 +14,7 @@
                 @endif
             @endif
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+
         {{--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>--}}
