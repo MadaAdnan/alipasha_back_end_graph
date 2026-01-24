2841a0b5b01740c0f630df2b057a18e8365cab6d
Adnan <mh.shamey@gmail.com>
2026-01-24 11:45:08 +0300

    add auth page

--- resources/views/components/components/profile-component.blade.php
+++ resources/views/components/components/profile-component.blade.php
@@ -1,4 +1,8 @@
 <div class="card card-body my-2 ">
+    <div class="d-flex justify-content-between">
+        <h3 class="fs-4 text-gray">تعديل الملف الشخصي</h3>
+
+    </div>
     <form action="">
         <x-form.input-component name="name" :value="old('name')??auth()->user()->name"/>
         <x-form.input-component name="email" :value="old('name')??auth()->user()->email"/>
