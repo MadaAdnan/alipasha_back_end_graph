f570b72f4a3dd82c47bf90e989f82c3faa8bf0d7
Adnan <mh.shamey@gmail.com>
2026-01-24 11:50:39 +0300

    add auth page

--- resources/views/components/components/profile-component.blade.php
+++ resources/views/components/components/profile-component.blade.php
@@ -6,7 +6,7 @@
     <form action="">
         <x-form.input-component name="name" :value="old('name')??auth()->user()->name" label="الاسم" placeholder="الاسم" required/>
         <x-form.input-component name="email" :value="old('name')??auth()->user()->email" label="البريد الإلكتروني" disabled placeholder="البريد الإلكتروني" required/>
-        <x-form.input-phone-component placeholder="9XXXXXXXX" :phoneValue="{{auth()->user()->phone}}" :countryValue="auth()->user()->country_code" label="رقم الهاتف" id="registerPhone"
+        <x-form.input-phone-component placeholder="9XXXXXXXX" :phoneValue="auth()->user()->phone" :countryValue="auth()->user()->country_code" label="رقم الهاتف" id="registerPhone"
                                       name="phone"
                                       countryName="phone_code" required/>
         <x-form.input-component name="address" :value="old('name')??auth()->user()->address" label="العنوان" placeholder="العنوان" required/>
