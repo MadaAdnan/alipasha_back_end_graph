4d996924ab5f334dcbf44b6f6d2f73b3db37d325
Adnan <mh.shamey@gmail.com>
2026-01-24 08:52:51 +0300

    add auth page

--- resources/views/components/components/seller-name-component.blade.php
+++ resources/views/components/components/seller-name-component.blade.php
@@ -12,7 +12,7 @@
         @else
             <i class="fa fa-shop {{$iconSize}}"></i>
         @endif
-        <span class="seller-name d-inline-block  {{$class??'fs-6'}}">  {{$seller->seller_name ?? $seller->name}}</span>
+        <span class="seller-name d-inline-block flex-grow-1 overflow-hidden  {{$class??'fs-6'}}">  {{$seller->seller_name ?? $seller->name}}</span>
         @if($seller->is_verified==true)
             <i class="bi bi-patch-check-fill text-blue-accent {{$iconSize}}"></i>
         @endif
