d49af673b17a35e2c2dc81c32521ea9765f8e1ff
Adnan <mh.shamey@gmail.com>
2026-01-25 09:20:18 +0300

    add auth page

--- resources/views/components/components/filter-component.blade.php
+++ resources/views/components/components/filter-component.blade.php
@@ -19,7 +19,7 @@
             <label class="filter-label" for="Search">بحث</label>
             <input class="filter-input" id="Search" name="q" placeholder="ابحث عن... "/>
             <div id="suggestions"  class="position-absolute bg-white w-100 z-3 rounded shadow-sm">
-                <ul id="suggestionsUl" class="text-start">
+                <ul id="suggestionsUl" class="w-100">
 
                 </ul>
             </div>
