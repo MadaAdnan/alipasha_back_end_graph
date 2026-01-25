ecafa3083d1b6dd31adc1e6eb3f402fc73908f0b
Adnan <mh.shamey@gmail.com>
2026-01-25 09:37:36 +0300

    add auth page

--- resources/views/components/component/seller-info-component.blade.php
+++ resources/views/components/component/seller-info-component.blade.php
@@ -4,7 +4,7 @@
 ])
 <div class="card card-body seller-data">
     <div class="d-flex justify-content-between align-items-center">
-        <h6 class="text-gray fw-bold my-1">معلومات المعلن</h6>
+        <h6 class="text-gray fw-bold my-1">معلومات المعلن @if($seller->id==auth()->id()) <a href="{{route('profile.index')}}"><i class="fa fa-edit"></i></a> @endif</h6>
         @auth
             @php
                 $following=auth()->user()->followers()->pluck('seller_id')->toArray();
