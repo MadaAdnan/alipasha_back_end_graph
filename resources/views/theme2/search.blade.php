ae6d33513cbf9c71b3a888f99e5c3bdc7d1a9fa3
Adnan <mh.shamey@gmail.com>
2026-01-25 09:30:50 +0300

    add auth page

--- resources/views/theme2/search.blade.php
+++ resources/views/theme2/search.blade.php
@@ -2,18 +2,16 @@
 @section('content')
     <div class="container my-2">
         <div class="row">
-            <div class="col-md-12">
-                <x-components.bread-crumb-component :categories="[
-    ['id'=>null,'name'=>'الفلتر','class'=>'active']
-]" :firstUrl="route('index')"/>
-            </div>
+
             <div class="col-md-3  my-2">
                 <x-components.side-bar-category-component :categories="$categories"/>
                 <x-components.filter-component/>
             </div>
 
             <div class="col-md-9 my-2">
-
+                <x-components.bread-crumb-component :categories="[
+    ['id'=>null,'name'=>'الفلتر','class'=>'active']
+]" :firstUrl="route('index')"/>
                 @if($products->count()>0)
                     @foreach($products as $product)
                         <x-components.estate-card-component :product="$product" class="d-none d-md-flex mt-2"/>
