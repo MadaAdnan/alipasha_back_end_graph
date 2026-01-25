cd36ed4a481d795d7001e72ca746226056e001c5
Adnan <mh.shamey@gmail.com>
2026-01-25 08:52:23 +0300

    add auth page

--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,7 @@ Route::get('suggestions', function () {
    $q=\request()->get('q');
    return \App\Models\Product::active()->product()->where(fn($query)=>$query->where('name', 'like', "%{$q}%")
  /*  ->orWhere('expert', 'like', "%{$q}%")*/
-   )->limit(7)->pluck('name')->toArray();
+   )->select('name')->distinct()->limit(7)->pluck('name')->toArray();
 
 });
 Route::middleware('auth:sanctum')->group(function () {
