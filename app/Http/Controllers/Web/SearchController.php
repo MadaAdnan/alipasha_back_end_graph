d1def5c672a5648e9f7f02d163f935401b834318
Adnan <mh.shamey@gmail.com>
2026-01-25 08:01:10 +0300

    add auth page

--- app/Http/Controllers/Web/SearchController.php
+++ app/Http/Controllers/Web/SearchController.php
@@ -26,65 +26,39 @@ class SearchController extends Controller
         $seller = \request()->get('seller_id');
 
         $priceFrom = \request()->filled('price_from') ?\request()->get('price_from'):0;
-        $priceTo = \request()->get('price_to');
+        $priceTo = \request()->filled('price_to') ?\request()->get('price_to'):1000000;
 
-        $query = Product::query()
-            ->where('active', ProductActiveEnum::ACTIVE->value);
-
-        if (request()->filled('type')) {
-            $type = request()->type;
-
-            if (
-                $type == CategoryTypeEnum::SEARCH_JOB->value ||
-                $type == CategoryTypeEnum::JOB->value
-            ) {
-                $query->whereIn('type', [
+        $products = Product::query()->where('active', ProductActiveEnum::ACTIVE->value);
+        if(!empty($type)){
+            if ($type == CategoryTypeEnum::SEARCH_JOB->value || $type == CategoryTypeEnum::JOB->value) {
+                $products->whereIn('type', [
                     CategoryTypeEnum::SEARCH_JOB->value,
                     CategoryTypeEnum::JOB->value,
                 ]);
             } else {
-                $query->where('type', $type);
+                $products->where('type', $type);
             }
         }
-
-        if (request()->filled('q')) {
-            $query->where(function ($q) {
-                $q->where('name', 'like', '%' . request()->q . '%')
-                    ->orWhere('info', 'like', '%' . request()->q . '%');
-            });
+        if(!empty($text)){
+            $products->where(fn($q) => $q->where('name', 'like', "%{$text}%")->orWhere('info', 'like', "%{$text}%"));
         }
-
-        if (request()->filled('city_id')) {
-            $query->where('city_id', request()->city_id);
+        if(!empty($city)){
+            $products->where('city_id', $city);
         }
-
-        if (request()->filled('area_id')) {
-            $query->whereHas('user', fn ($q) =>
-            $q->where('area_id', request()->area_id)
-            );
+        if(!empty($area)){
+            $products->whereHas('user', fn($query) => $query->where('area_id', $area));
         }
+           if(!empty($category)){
+               $products->where(fn($query)=>$query->where('category_id', $category)->orWhere('sub1_id',$category)
+                   ->orWhere('sub2_id',$category)
+                   ->orWhere('sub3_id',$category)
+                   ->orWhere('sub4_id',$category));
 
-        if (request()->filled('category_id')) {
-            $query->where(function ($q) {
-                $q->where('category_id', request()->category_id)
-                    ->orWhere('sub1_id', request()->category_id)
-                    ->orWhere('sub2_id', request()->category_id)
-                    ->orWhere('sub3_id', request()->category_id)
-                    ->orWhere('sub4_id', request()->category_id);
-            });
-        }
+           }
+            $products  ->whereBetween('price', [$priceFrom,$priceTo]);
 
-        $query->when(
-            request()->filled('price_from'),
-            fn ($q) => $q->where('price', '>=', request()->price_from)
-        );
 
-        $query->when(
-            request()->filled('price_to'),
-            fn ($q) => $q->where('price', '<=', request()->price_to)
-        );
-
-        $products = $query->latest()->paginate();
+          $products=  $products->latest()->paginate();
         $productsForPagination = clone $products;
         $categories = Category::where('is_active', true)
             ->where(['is_active' => true, 'is_main' => true])
