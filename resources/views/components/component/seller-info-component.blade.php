3afbc597f6b1c82c955f63b82a03f7ef8857b2cb
Adnan <mh.shamey@gmail.com>
2026-01-27 09:49:28 +0300

    css

--- resources/views/components/component/seller-info-component.blade.php
+++ resources/views/components/component/seller-info-component.blade.php
@@ -65,7 +65,7 @@
         @auth
         <form action="{{route('communities.store')}}">
             @csrf
-            <input type="hidden" name="sellerId" value="{{$seller->id}}">
+            <input type="hidden" name="sellerId" value="{{ $seller?->id ??$post->user_id }}">
             <button class="btn-green rounded bg-transparent d-flex justify-content-center align-items-center">
                 <i class="fa fa-comments text-black"></i>
                 <span class="small d-none d-md-inline-block  text-black">
