9b047e09753b5be42da6b4ada6e51a944d1507dd
Adnan <mh.shamey@gmail.com>
2026-01-27 10:34:04 +0300

    css

--- resources/views/components/component/slider-component.blade.php
+++ resources/views/components/component/slider-component.blade.php
@@ -1,8 +1,8 @@
 @props([ 'id' => 'main-slider', 'items' => [], ])
-<div class="slider-container d-flex">
+<div class="slider-container d-flex flex-sm-col-reverse">
     <!-- Thumbnails -->
     @if(count($items) > 1)
-        <div class="d-flex flex-column justify-content-center mt-2 thumbnails-wrapper">
+        <div class="d-flex flex-column flex-sm-row justify-content-center mt-2 thumbnails-wrapper">
             @foreach($items as $index => $item)
                 <div class="thumbnail-item mx-1 @if($loop->first) active @endif"
                      data-carousel-id="carousel-{{ $id }}"
