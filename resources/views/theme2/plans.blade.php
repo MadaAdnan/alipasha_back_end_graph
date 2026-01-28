399d56aef3b9cda6943671d881fc9ce410ee97cd
Adnan <mh.shamey@gmail.com>
2026-01-28 10:35:49 +0300

    css

--- resources/views/theme2/plans.blade.php
+++ resources/views/theme2/plans.blade.php
@@ -33,7 +33,7 @@
                             </p>
 
                         <div class="d-flex flex-column">
-                            <ul class="list-unstyled w-100 mb-3 ps-0 pe-0 flex-grow-1">
+                            <ul class="list-unstyled w-100 mb-3 ps-0 pe-0 flex-grow-1 flex-fill">
                                 @foreach($plan->items as $item)
                                     <li class="d-flex align-items-center mb-1 border border-1 rounded p-1">
                                         @if($item['active'])
