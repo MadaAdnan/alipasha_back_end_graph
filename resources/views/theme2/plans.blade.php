ffb70a657836e88e2c8e13df08f3a147793ea9dd
Adnan <mh.shamey@gmail.com>
2026-01-28 10:37:07 +0300

    css

--- resources/views/theme2/plans.blade.php
+++ resources/views/theme2/plans.blade.php
@@ -33,7 +33,7 @@
                             </p>
 
                         <div class="d-flex flex-column">
-                            <ul class="list-unstyled w-100 mb-3 ps-0 pe-0 flex-grow-1 ">
+                            <ul class="list-unstyled w-100 mb-3 ps-0 pe-0 ">
                                 @foreach($plan->items as $item)
                                     <li class="d-flex align-items-center mb-1 border border-1 rounded p-1">
                                         @if($item['active'])
@@ -47,7 +47,7 @@
                                     </li>
                                 @endforeach
                             </ul>
-                            <div class="flex-fill"></div>
+                            <div class="flex-fill  flex-grow-1"></div>
                             @php
                                 $plansId=auth()->user()->plans->pluck('id')->toArray();
                                 $isActive=in_array($plan->id,$plansId);
