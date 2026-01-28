1e93396391a35f2153d46e1c85602733a01f795d
Adnan <mh.shamey@gmail.com>
2026-01-28 10:26:54 +0300

    css

--- resources/views/theme2/plans.blade.php
+++ resources/views/theme2/plans.blade.php
@@ -42,7 +42,7 @@
 
                                           <i class="fa-solid fa-circle-xmark text-danger fs-5 me-1"></i>
                                       @endif
-                                      <span class="flex-grow-1 text-start  text-dark" style="display: contents">{{ $item['item'] }}</span>
+                                      <span class="flex-grow-1 text-start  text-dark" >{{ $item['item'] }}</span>
                                   </li>
                               @endforeach
                           </ul>
