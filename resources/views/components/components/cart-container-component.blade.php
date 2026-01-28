b777fc3fa0fe8abb8f56da062b4007e41f349d63
Adnan <mh.shamey@gmail.com>
2026-01-28 11:27:28 +0300

    css

--- resources/views/components/components/cart-container-component.blade.php
+++ resources/views/components/components/cart-container-component.blade.php
@@ -26,26 +26,37 @@
             </div>
             <div class="col-12">
                 @php
-                    $message='';
-                    $sumTotal=0;
-                    if($items->count()>0){
-                        $message="السلام عليكم ورحمة الله وبركاته \n";
-                        foreach ($items as $item){
-                            $product=$item->product;
-                             $message.="معرف المنتج : ".$product->id."\n";
-                             $message.="المنتج : ".$product->name."\n";
-                              $price=$product->is_discount?$product->discount:$product->price;
-                              $message.="السعر : ".$price."\n";
-                              $message.="الكمية : ".$item->qty."\n";
-                              $total=$price * $item->qty;
-                              $sumTotal+=$total;
-                              $message.="الإجمالي : ".$total."\n";
-                              $message.="______________________________________\n";
-                               }
-                    }
-                      $message.="قيمة الطلب : ".$sumTotal;
+                    $dataForApi=[];
+                        $message='';
+                        $sumTotal=0;
+                        if($items->count()>0){
+                            $message="السلام عليكم ورحمة الله وبركاته \n";
+                            foreach ($items as $item){
+
+                                $product=$item->product;
+                                 $message.="معرف المنتج : ".$product->id."\n";
+                                 $message.="المنتج : ".$product->name."\n";
+                                  $price=$product->is_discount?$product->discount:$product->price;
+                                  $message.="السعر : ".$price."\n";
+                                  $message.="الكمية : ".$item->qty."\n";
+                                  $total=$price * $item->qty;
+                                  $sumTotal+=$total;
+                                  $message.="الإجمالي : ".$total."\n";
+                                  $message.="______________________________________\n";
+                                   $dataForApi[]=[
+                                    "product_id"=>$item->product_id,
+                                    'qty'=>$item->qty,
+                                    'price'=>$price,
+                                    'total'=>$total
+                                    ];
+                                   }
+                        }
+                          $message.="قيمة الطلب : ".$sumTotal;
                 @endphp
-                <button id="submit" type="button" data-href="https://wa.me/{{$seller?->full_phone}}?text={{urlencode($message)}}" class="btn btn-green"><i class="fa-brand fa-whatsapp"></i> طلب من خلال واتس آب</button>
+                <button id="submit" type="button"
+                        data-href="https://wa.me/{{$seller?->full_phone}}?text={{urlencode($message)}}"
+                        class="btn btn-green"><i class="fa-brand fa-whatsapp"></i> طلب من خلال واتس آب
+                </button>
             </div>
         @else
             <div class="col-12">
@@ -53,14 +64,45 @@
             </div>
         @endif
 
-        </div>
+    </div>
 
 </div>
 <script>
-  let submit=document.getElementById('submit');
-  submit.addEventListener('click',function (){
-     console.log(this.dataset.href)
-  })
+    let submit = document.getElementById('submit');
+    submit.addEventListener('click', function () {
+        console.log(this.dataset.href)
+        fetch(`/api/orders`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+                ...(localStorage.getItem('token') && {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                })
+            },
+            body: JSON.stringify({
+                data: {{$dataForApi}}
+            })
+        })
+            .then(response => {
+
+                if (response) {
+
+                    return response.json(); // نستخدم json() لأن الاستجابة الآن تكون ككائن JSON
+                }
+                throw new Error('Network response was not ok');
+            })
+            .then(data => {
+                console.log(data)
+
+
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                showToast(error, 'error');
+            });
+    })
 
 </script>
 
