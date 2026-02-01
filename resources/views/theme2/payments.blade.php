db38afcb0279d6f3ba5e0c1b3907ab9fa66e64bf
Adnan <mh.shamey@gmail.com>
2026-02-01 14:11:29 +0300

    add payment page

--- resources/views/theme2/payments.blade.php
+++ resources/views/theme2/payments.blade.php
@@ -10,9 +10,9 @@
                         <span>طريقة شحن الحساب</span>
                     </div>
                     <div class="bg-white">
-                        <img src="{{asset('images/payment/payment1.jpg')}}" alt="Payment Method 1">
-                        <img src="{{asset('images/payment/payment2.jpg')}}" alt="Payment Method 2">
-                        <img src="{{asset('images/payment/payment3.jpg')}}" alt="Payment Method 3">
+                        <img class="payment-img" src="{{asset('images/payment/payment1.jpg')}}" alt="Payment Method 1">
+                        <img class="payment-img" src="{{asset('images/payment/payment2.jpg')}}" alt="Payment Method 2">
+                        <img class="payment-img" src="{{asset('images/payment/payment3.jpg')}}" alt="Payment Method 3">
                     </div>
                 </div>
             </div>
