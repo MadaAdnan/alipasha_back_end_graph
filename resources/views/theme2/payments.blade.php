fffb63760fede8cad550cdb0107fd694adde1f82
Adnan <mh.shamey@gmail.com>
2026-02-01 14:29:47 +0300

    add payment page

--- resources/views/theme2/payments.blade.php
+++ resources/views/theme2/payments.blade.php
@@ -16,7 +16,7 @@
                         <p class="lead "><span>المحفظة:</span> <span  class="d-inline-block border border-1 p-2 fw-bolder rounded">{{$wallet}}</span><i class="fa fa-copy" onclick="copyTextToClipboard('{{$wallet}}')"></i></p>
                         <p class="lead"><span>معرفك الشخصي:</span> <span class="d-inline-block border border-1 p-2 fw-bolder rounded">{{auth()->id()}}</span><i class="fa fa-copy" onclick="copyTextToClipboard('{{auth()->id()}}')"></i></p>
 
-                        <div class=" d-flex flex-column gap-3 align-items-center justify-content-center">
+                        <div class=" d-flex flex-column gap-3 align-items-center justify-content-center w-100">
                             <img class="payment-img" src="{{asset('images/payment/payment1.jpg')}}" alt="Payment Method 1">
                             <img class="payment-img" src="{{asset('images/payment/payment2.jpg')}}" alt="Payment Method 2">
                             <img class="payment-img" src="{{asset('images/payment/payment3.jpg')}}" alt="Payment Method 3">
