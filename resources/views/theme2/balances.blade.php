5e4b3c51ee5bcff7f1ce5ac0a7512797c6f7ed9f
Adnan <mh.shamey@gmail.com>
2026-02-04 11:07:32 +0300

    add payment page

--- resources/views/theme2/balances.blade.php
+++ resources/views/theme2/balances.blade.php
@@ -9,52 +9,52 @@
                 <div class="filter-container">
                     <div class="filter-header">
                         <i class="fas fa-wallet"></i>
-                        <span>طريقة شحن الحساب</span>
+                        <span>الرصيد</span>
                     </div>
-            <div class="col-12 ">
-                <div class="table-responsive" style="margin-top: 100px">
-                    <table class="table table-striped" dir="rtl">
-                        <thead>
-                        <tr>
-                            <th>سحب</th>
-                            <th>إيداع</th>
-                            <th>الرصيد</th>
-                            <th>البيان</th>
-                            <th>
-                                <a href="{{route('balances.index',['sort'=>request()->get('sort')=='desc'?'asc':'desc'])}}">التاريخ
-                                    @if(request()->get('sort')=='desc')
-                                        <i class="bi bi-sort-down-alt"></i>
 
-                                    @else
-                                        <i class="bi bi-sort-up"></i>
-                                    @endif
-                                </a></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($balances as $balance)
+                    <div class="table-responsive" style="margin-top: 100px">
+                        <table class="table table-striped" dir="rtl">
+                            <thead>
                             <tr>
-                                <td>{{$balance->debit}}</td>
-                                <td>{{$balance->credit}}</td>
-                                <td>{{$balance->total}}</td>
-                                <td>{{$balance->info}}</td>
-                                <td>{{$balance->created_at->format('Y-m-d H:i')}}</td>
+                                <th>سحب</th>
+                                <th>إيداع</th>
+                                <th>الرصيد</th>
+                                <th>البيان</th>
+                                <th>
+                                    <a href="{{route('balances.index',['sort'=>request()->get('sort')=='desc'?'asc':'desc'])}}">التاريخ
+                                        @if(request()->get('sort')=='desc')
+                                            <i class="bi bi-sort-down-alt"></i>
 
+                                        @else
+                                            <i class="bi bi-sort-up"></i>
+                                        @endif
+                                    </a></th>
                             </tr>
-                        @endforeach
+                            </thead>
+                            <tbody>
+                            @foreach($balances as $balance)
+                                <tr>
+                                    <td>{{$balance->debit}}</td>
+                                    <td>{{$balance->credit}}</td>
+                                    <td>{{$balance->total}}</td>
+                                    <td>{{$balance->info}}</td>
+                                    <td>{{$balance->created_at->format('Y-m-d H:i')}}</td>
 
-                        </tbody>
+                                </tr>
+                            @endforeach
 
-                    </table>
+                            </tbody>
 
-                    <div class="d-flex justify-content-center w-100">
-<x-components.paginator-component :paginator="$balances"/>
+                        </table>
+
+                        <div class="d-flex justify-content-center w-100">
+                            <x-components.paginator-component :paginator="$balances"/>
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
-    </div>
+
         </div>
     </div>
 @endsection
