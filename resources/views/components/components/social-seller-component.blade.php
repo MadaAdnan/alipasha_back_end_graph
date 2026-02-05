bab911587e28fea673060ff71fd9499ed9e13bb1
Adnan <mh.shamey@gmail.com>
2026-02-05 11:05:52 +0300

    add payment page

--- resources/views/components/components/social-seller-component.blade.php
+++ resources/views/components/components/social-seller-component.blade.php
@@ -3,7 +3,7 @@
 ])
 @if($store->plans()->whereNot('duration','free')->exists())
 <div class="social-media-section text-center py-4">
-    <h5 class="mb-3">تابعنا على</h5>
+    {{--<h5 class="mb-3">تابعنا على</h5>--}}
     <div class="social-links d-flex justify-content-center gap-3 flex-wrap">
         @if(Str::startsWith('https://',$store->social['face']))
             <a href="https://facebook.com/yourpage" target="_blank"
