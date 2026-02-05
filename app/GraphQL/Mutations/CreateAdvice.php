8bdca691e349ed4b34b913b0a02feb282f60c082
Adnan <mh.shamey@gmail.com>
2026-02-05 12:36:34 +0300

    add payment page

--- app/GraphQL/Mutations/CreateAdvice.php
+++ app/GraphQL/Mutations/CreateAdvice.php
@@ -41,8 +41,8 @@ final class CreateAdvice
             $currentPlan = $planUser->plan;
             break;
         }
-        throw new GraphQLExceptionHandler($currentPlan->name.":{".$currentPlan?->ads_count."}:(".$myAdvices.")");
-        if (now()->greaterThan($expiredDate) || $currentPlan?->ads_count >= $myAdvices) {
+//        throw new GraphQLExceptionHandler($currentPlan->name.":{".$currentPlan?->ads_count."}:(".$myAdvices.")");
+        if (now()->lessThan($expiredDate) || ($currentPlan?->ads_count >= $myAdvices)) {
             $data=[
                 'title'=>'تنبيه',
                 'body'=>'وصلت لحد النشر المسموح لك شهريا انتظر للشهر القادم او قم بترقية حسابك لتحصل على النشر المفتوح'
