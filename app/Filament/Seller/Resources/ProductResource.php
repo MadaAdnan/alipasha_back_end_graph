4bb149e9a3d9a583669648ac1bbda1bab50a5e80
Adnan <mh.shamey@gmail.com>
2026-02-05 12:49:12 +0300

    add payment page

--- app/Filament/Seller/Resources/ProductResource.php
+++ app/Filament/Seller/Resources/ProductResource.php
@@ -129,6 +129,7 @@ class ProductResource extends Resource implements HasShieldPermissions
                 Forms\Components\SpatieMediaLibraryFileUpload::make('images')
                     ->conversion('webp')
                     ->collection('images')
+                    ->maxFiles(3)
                     ->image()->multiple()->imageEditor()->imageCropAspectRatio('1:1')
             ->label('صور '),
                              Forms\Components\TextInput::make('video')->label('رابط الفيديو إن وجد'),
