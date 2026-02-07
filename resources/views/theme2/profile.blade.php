e42912d32190cdd54322dc68c82a2ec69c48e460
Adnan <mh.shamey@gmail.com>
2026-02-07 12:07:39 +0300

    add payment page

--- resources/views/theme2/profile.blade.php
+++ resources/views/theme2/profile.blade.php
@@ -4,6 +4,7 @@
     <div class="container mb-2 mt-5">
         <div class="row">
             <div class="col-md-3">
+                <x-components.change-avatar/>
                 <x-components.change-password-component/>
             </div>
             <div class="col-md-9">
