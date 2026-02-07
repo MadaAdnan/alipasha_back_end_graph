eeb583afeda806da39973e671b93bdd5be339480
Adnan <mh.shamey@gmail.com>
2026-02-07 12:17:31 +0300

    add payment page

--- resources/views/components/components/change-avatar.blade.php
+++ resources/views/components/components/change-avatar.blade.php
@@ -6,7 +6,8 @@
     <form action="{{ route('rechange-password') }}" method="POST" class="d-flex flex-column justify-content-center align-items-center gap-2">
         @csrf
         <label for="avatar" class="position-relative">
-            <i class="fa fa-image position-absolute" style="top:45%;left:40%"></i>
+
+            <i class="fa-regular fa-image position-absolute text-muted" style="z-index: 10;top:45%;left:40%"></i>
             <img style="width: 100px;aspect-ratio: 1;border-radius: 50%;border:1px solid red" src="{{auth()->user()->getImage()}}" alt="Avatar" id="IMG_AVATAR">
         </label>
             <input type="file" class="d-none" name="avatar" id="avatar">
