d4df03d84df78856a1d2750b6ef5bb4b3f6d6f4d
Adnan <mh.shamey@gmail.com>
2026-02-07 08:24:52 +0300

    add payment page

--- resources/views/components/components/profile-component.blade.php
+++ resources/views/components/components/profile-component.blade.php
@@ -6,7 +6,7 @@
     <form action="{{route('profile.store')}}" method="POST">
         @csrf
         <x-form.input-component name="name" :value="old('name')??auth()->user()->name" label="الاسم" placeholder="الاسم"
-                                :disabled="auth()->user()->is_verified"
+                                @if (auth()->user()->is_verified ) disabled="" @endif
                                 :helpText="auth()->user()->is_verified?'حسابك موثق لا يمكن تغيير الاسم':''" required/>
         <x-form.input-component name="seller_name" :disabled="!auth()->user()->is_verified"
                                 :value="old('seller_name')??auth()->user()->seller_name" label="اسم المتجر"
