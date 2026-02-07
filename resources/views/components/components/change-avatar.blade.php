fdba4357b2826bd55951c361d8f6303b105140a6
Adnan <mh.shamey@gmail.com>
2026-02-07 12:14:39 +0300

    add payment page

--- resources/views/components/components/change-avatar.blade.php
+++ resources/views/components/components/change-avatar.blade.php
@@ -3,13 +3,13 @@
         <i class="fa-solid fa-user-secret"></i>
         <span>تغيير الصورة الشخصية</span>
     </div>
-    <form action="{{ route('rechange-password') }}" method="POST">
+    <form action="{{ route('rechange-password') }}" method="POST" class="d-flex flex-column justify-content-center align-items-center">
         @csrf
         <label for="avatar">
             <img style="width: 100px;aspect-ratio: 1;border-radius: 50%;border:1px solid red" src="{{auth()->user()->getImage()}}" alt="Avatar" id="IMG_AVATAR">
         </label>
             <input type="file" class="d-none" name="avatar" id="avatar">
 
-        <button class="btn btn-red-accent">تغيير</button>
+        <button class="btn btn-red-accent">حفظ</button>
     </form>
 </div>
