81f9e5bb8ee181cbf5299cea11e126731585f02b
Adnan <mh.shamey@gmail.com>
2026-02-07 11:52:32 +0300

    add payment page

--- resources/views/theme2/confirm_email.blade.php
+++ resources/views/theme2/confirm_email.blade.php
@@ -39,7 +39,8 @@
                method:'POST',
                headers:{
                    'Content-Type':'application/json',
-                   'ACCEPT':'application/json'
+                   'ACCEPT':'application/json',
+                   'X-CSRF-TOKEN':'{{csrf_token()}}'
                }
            }) .then(res => res.json())
                .then(data=>{
