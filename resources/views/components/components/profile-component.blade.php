8392b727967740e97274b725e9f7bc29653425be
Adnan <mh.shamey@gmail.com>
2026-02-07 08:26:53 +0300

    add payment page

--- resources/views/components/components/profile-component.blade.php
+++ resources/views/components/components/profile-component.blade.php
@@ -12,7 +12,7 @@
                                 :value="old('seller_name')??auth()->user()->seller_name" label="اسم المتجر"
                                 placeholder="اسم المتجر" required/>
         <x-form.input-component name="email" :value="old('email')??auth()->user()->email" label="البريد الإلكتروني"
-                                disabled placeholder="البريد الإلكتروني" required/>
+                                :disabled="true" placeholder="البريد الإلكتروني" required/>
         <x-form.input-phone-component placeholder="9XXXXXXXX" :value="auth()->user()->phone"
                                       :countryValue="auth()->user()->phone_code" label="رقم الهاتف" id="registerPhone"
                                       name="phone"
