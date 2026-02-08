98372049e72f4976482fce9de8914ba25ebde1dc
Adnan <mh.shamey@gmail.com>
2026-02-08 08:48:58 +0300

    add payment page

--- app/Filament/Seller/Resources/ProductResource.php
+++ app/Filament/Seller/Resources/ProductResource.php
@@ -131,7 +131,6 @@ class ProductResource extends Resource implements HasShieldPermissions
                     ->collection('images')
                     ->maxFiles(4)
                     ->minFiles(1)
-                   ->acceptedFileTypes(['image/png','image/jpeg','image/jpg','image/webp'])
 
                     ->image()->multiple()->imageEditor()->imageCropAspectRatio('1:1') ->helperText('يمكنك رفع 4 صور كحد أقصى (صورة واحدة في كل مرة)')
             ->label('صور '),
